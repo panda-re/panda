qemu/qemu

commit 977b6b91cee1132f8c7b12d22f4b273091598e44

ppc440_bamboo: Add 0.12 and 0.13 machine types for backward compat
Add a 0.12 machine type for compatibility with older versions. Mark the
default one as 0.13.

Signed-off-by: Amit Shah <amit.shah@redhat.com>
Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

hw/ppc440_bamboo.c
@@ -173,7 +173,15 @@ static void bamboo_init(ram_addr_t ram_size,
 }
 
 static QEMUMachine bamboo_machine = {
-    .name = "bamboo",
+    .name = "bamboo-0.13",
+    .alias = "bamboo",
+    .desc = "bamboo",
+    .init = bamboo_init,
+    .is_default = 1,
+};
+
+static QEMUMachine bamboo_machine_v0_12 = {
+    .name = "bamboo-0.12",
     .desc = "bamboo",
     .init = bamboo_init,
 };
@@ -181,6 +189,7 @@ static QEMUMachine bamboo_machine = {
 static void bamboo_machine_init(void)
 {
     qemu_register_machine(&bamboo_machine);
+    qemu_register_machine(&bamboo_machine_v0_12);
 }
 
 machine_init(bamboo_machine_init);

panda-re/panda

commit 1f09d130aa4df7d5ef00b04da87899d441ea2e11

ppc: don't define bamboo-0.13 as the default machine
It has been broken by commit 977b6b91cee1132f8c7b12d22f4b273091598e44.

Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

hw/ppc440_bamboo.c
@@ -177,7 +177,6 @@ static QEMUMachine bamboo_machine = {
     .alias = "bamboo",
     .desc = "bamboo",
     .init = bamboo_init,
-    .is_default = 1,
 };
 
 static QEMUMachine bamboo_machine_v0_12 = {