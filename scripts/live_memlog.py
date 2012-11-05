47e20351a4d8045863ca9bae87ec157798b08178
Brendan Dolan-Gavitt <brendandg@gatech.edu>
2012-11-04 23:01:05 -0500

    Script now emulates tail -f itself, eliminating buffer between tail and python, and allowing truly live updates.

--- scripts/live_memlog.py
+++ scripts/live_memlog.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
 import sys
-import fileinput
+import time
 
-for line in sys.stdin:
-    caller, pc, cr3, addr, n, val = line.strip().split()
-    caller = int(caller, 16)
-    pc = int(pc, 16)
-    cr3 = int(cr3, 16)
-    addr = int(addr, 16)
-    val = val.decode('hex')
-    sys.stdout.write(val)
-    sys.stdout.flush()
+file = open(sys.argv[1])
+
+while 1:
+    where = file.tell()
+    line = file.readline()
+    if not line:
+        time.sleep(.1)
+        file.seek(where)
+    else:
+        caller, pc, cr3, addr, n, val = line.strip().split()
+        caller = int(caller, 16)
+        pc = int(pc, 16)
+        cr3 = int(cr3, 16)
+        addr = int(addr, 16)
+        val = val.decode('hex')
+        sys.stdout.write(val)
+        #sys.stdout.flush()
