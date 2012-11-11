016fd612a57df1ec427ee4d8e31702c0738fe52f
Brendan Dolan-Gavitt <brendandg@gatech.edu>
2012-11-11 12:44:51 -0500

    Add plugin to create a memory dump the first time a tap point is reached (useful if you want to use volatility etc. or get symbol information)

---
+++ qemu/panda_plugins/memsnap/memsnap.cpp
@@ -0,0 +1,134 @@
+// This needs to be defined before anything is included in order to get
+// the PRIx64 macro
+#define __STDC_FORMAT_MACROS
+
+extern "C" {
+
+#include "config.h"
+#include "qemu-common.h"
+#include "monitor.h"
+#include "cpu.h"
+#include "disas.h"
+
+#include "panda_plugin.h"
+
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <set>
+#include <iostream>
+#include <fstream>
+
+// These need to be extern "C" so that the ABI is compatible with
+// QEMU/PANDA, which is written in C
+extern "C" {
+
+bool init_plugin(void *);
+void uninit_plugin(void *);
+int mem_read_callback(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size, void *buf);
+
+}
+
+struct prog_point {
+    target_ulong caller;
+    target_ulong pc;
+    target_ulong cr3;
+    bool operator <(const prog_point &p) const {
+        return (this->pc < p.pc) || \
+               (this->pc == p.pc && this->caller < p.caller) || \
+               (this->pc == p.pc && this->caller == p.caller && this->cr3 < p.cr3);
+    }
+};
+
+
+std::set<prog_point> tap_points;
+
+void save_mem(FILE *f) {
+#ifdef CONFIG_SOFTMMU
+    uint8_t mem_buf[TARGET_PAGE_SIZE];
+    uint8_t zero_buf[TARGET_PAGE_SIZE];
+    memset(zero_buf, 0, TARGET_PAGE_SIZE);
+    int res;
+    for (ram_addr_t addr = 0; addr < ram_size; addr += TARGET_PAGE_SIZE) {
+        res = panda_physical_memory_rw(addr, mem_buf, TARGET_PAGE_SIZE, 0);
+        if (res == -1) { // I/O. Just fill page with zeroes.
+            fwrite(zero_buf, TARGET_PAGE_SIZE, 1, f);
+        }
+        else {
+            fwrite(mem_buf, TARGET_PAGE_SIZE, 1, f);
+        }
+    }
+#endif
+}
+
+bool done = false;
+
+int mem_write_callback(CPUState *env, target_ulong pc, target_ulong addr,
+                       target_ulong size, void *buf) {
+    if(done) return 1;
+
+    prog_point p = {};
+#ifdef TARGET_I386
+    panda_virtual_memory_rw(env, env->regs[R_EBP]+4, (uint8_t *)&p.caller, 4, 0);
+    if((env->hflags & HF_CPL_MASK) != 0) // Lump all kernel-mode CR3s together
+        p.cr3 = env->cr[3];
+#endif
+    p.pc = pc;
+
+    // XXX disable CR3 check
+    //p.cr3 = 0;
+
+    if (tap_points.find(p) != tap_points.end()) {
+        tap_points.erase(p);
+        char path[256];
+        sprintf(path, TARGET_FMT_lx "." TARGET_FMT_lx "." TARGET_FMT_lx ".mem",
+            p.caller, p.pc, p.cr3);
+        FILE *f = fopen(path, "wb");
+        save_mem(f);
+        fclose(f);
+    }
+    
+    if (tap_points.empty()) done = true;
+
+    return 1;
+}
+
+bool init_plugin(void *self) {
+    panda_cb pcb;
+
+    printf("Initializing plugin memsnap\n");
+    
+    panda_enable_precise_pc();
+    panda_enable_memcb();    
+    pcb.mem_write = mem_write_callback;
+    panda_register_callback(self, PANDA_CB_MEM_WRITE, pcb);
+
+    std::ifstream taps("tap_points.txt");
+    if (!taps) {
+        printf("Couldn't open tap_points.txt; no tap points defined. Exiting.\n");
+        return false;
+    }
+
+    prog_point p = {};
+    while (taps >> std::hex >> p.caller) {
+        taps >> std::hex >> p.pc;
+        taps >> std::hex >> p.cr3;
+
+        // XXX disable CR3 check
+        //p.cr3 = 0;
+
+        printf("Adding tap point (" TARGET_FMT_lx "," TARGET_FMT_lx "," TARGET_FMT_lx ")\n",
+               p.caller, p.pc, p.cr3);
+        tap_points.insert(p);
+    }
+    taps.close();
+
+    return true;
+}
+
+void uninit_plugin(void *self) {
+
+}
