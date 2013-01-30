31dc5db5a15ca6761898f1709ad65827fe4f1dc6
Ryan Whelan <ryan.whelan@ll.mit.edu>
2013-01-30 16:05:17 -0500

    More work on call graph tool.

--- qemu/panda_plugins/bitcode_callgraph/bitcode_callgraph.cpp
+++ qemu/panda_plugins/bitcode_callgraph/bitcode_callgraph.cpp
@@ -19,12 +19,21 @@
 #include "llvm/Support/IRReader.h"
 #include "llvm/Support/raw_ostream.h"
 
+//#define PRINTCFG
+
 using namespace llvm;
 
 class PandaCallGraphPass : public ModulePass {
+    void visitNode(CallGraphNode *node);
+    int curdepth;
+    int maxdepth;
+    Function *currentFunc;
+    Function *deepestFunc;
+    std::set<Function*> externalFuncs;
+    std::set<Function*> recursiveFuncs;
 public:
     static char ID;
-    PandaCallGraphPass() : ModulePass(ID) {
+    PandaCallGraphPass() : ModulePass(ID), curdepth(0), maxdepth(0) {
         PassRegistry &Reg = *PassRegistry::getPassRegistry();
         initializeIPA(Reg); // Initialize call graph analysis
     }
@@ -39,6 +48,59 @@ public:
 char PandaCallGraphPass::ID = 0;
 static RegisterPass<PandaCallGraphPass> X("PCGP", "Panda Call Graph Pass");
 
+void PandaCallGraphPass::visitNode(CallGraphNode *node){
+    curdepth++;
+    if (curdepth > maxdepth){
+        maxdepth++;
+        deepestFunc = currentFunc;
+    }
+
+    std::set<Function*> funcset; // Edges for this node
+    for (CallGraphNode::iterator i = node->begin(); i != node->end(); i++){
+        Function *f = i->second->getFunction();
+        if (f){
+            if (funcset.find(f) != funcset.end()){
+                // Only visit a called function once
+                continue;
+            }
+            funcset.insert(f);
+#ifdef PRINTCFG
+            for (int j = 0; j < curdepth; j++) printf(" ");
+            printf("%s ", f->getName().str().c_str());
+#endif
+            if (i->second->getFunction()->isDeclaration()){
+                externalFuncs.insert(f);
+#ifdef PRINTCFG
+                printf("-- EXTERNAL\n");
+#endif
+            }
+            else {
+                if (f == node->getFunction()){
+                    recursiveFuncs.insert(f);
+#ifdef PRINTCFG
+                    printf("-- RECURSIVE\n");
+#endif
+                }
+                else {
+#ifdef PRINTCFG
+                    printf("\n");
+#endif
+                    visitNode(i->second);
+                }
+            }
+        }
+        else {
+            // Call to an LLVM value or inline ASM (yes, you can inline ASM
+            // in the IR)
+#ifdef PRINTCFG
+            for (int j = 0; j < curdepth; j++) printf(" ");
+            printf("NULL FUNCTION\n");
+#endif
+        }
+    }
+    curdepth--;
+}
+
 bool PandaCallGraphPass::runOnModule(Module &M){
     CallGraph &CG = getAnalysis<CallGraph>();
     
@@ -51,36 +113,40 @@ bool PandaCallGraphPass::runOnModule(Module &M){
     }
     printf("\n\n");
 
-    std::set<Function*> funcset;
-
+    // Look at call graph for each function in module
     for (Module::iterator i = M.begin(); i != M.end(); i++){
-
         if (i->isDeclaration()){
             // If it's defined somewhere else that's not op_helper.c 
             continue;
         }
 
+#ifdef PRINTCFG
         printf("=== %s ===\n", i->getName().str().c_str());
-        funcset.clear();
-        for (CallGraphNode::iterator j = CG[i]->begin(); j != CG[i]->end();
-                j++){
-            Function *f = j->second->getFunction();
-            if (f){
-                if (funcset.find(f) == funcset.end()){
-                    // Only print a called function once
-                    printf("%s\n", f->getName().str().c_str());
-                    funcset.insert(f);
-                }
-            }
-            else {
-                // Call to an LLVM value or inline ASM (yes, you can inline ASM
-                // in the IR)
-                printf("NULL FUNCTION\n");
-            }
-        }
-        printf("\n\n");
+#endif
+        currentFunc = i;
+        visitNode(CG[i]);
 
+#ifdef PRINTCFG
+        printf("\n\n");
+#endif
     }
+
+    printf("=== External functions: ===\n");
+    for (std::set<Function*>::iterator i = externalFuncs.begin();
+            i != externalFuncs.end(); i++){
+        printf("%s\n", (*i)->getName().str().c_str());
+    }
+    printf("\n\n");
+
+    printf("=== Recursive functions: ===\n");
+    for (std::set<Function*>::iterator i = recursiveFuncs.begin();
+            i != recursiveFuncs.end(); i++){
+        printf("%s\n", (*i)->getName().str().c_str());
+    }
+    printf("\n\n");
+    
+    printf("=== Maximum call depth: ===\n%d, %s\n\n", maxdepth,
+        deepestFunc->getName().str().c_str());
 
     return false; // doesn't modify
 }
