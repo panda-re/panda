0c43395475187466154df39cab92ff91b86d2a7d
Ryan Whelan <rwhelan@ll.mit.edu>
2013-04-19 11:00:32 -0400

    Some small changes to build scripts, and some compile documentation.  Building QEMU and LLVM with same compiler (g++-4.7.2) fixes problems encountered when LLVM was built with Clang and QEMU was built with g++-4.7.2.

--- qemu/build.sh
+++ qemu/build.sh
@@ -2,10 +2,11 @@
 
 ./configure --target-list=x86_64-softmmu,x86_64-linux-user,i386-linux-user,\
 arm-linux-user,arm-softmmu \
+--cxx=g++-4.7 \
 --prefix=`pwd`/install \
 --disable-pie \
 --enable-llvm \
---with-llvm=../llvm-3.0/Release+Debug+Asserts \
+--with-llvm=../llvm-3.0/Debug+Asserts \
 --extra-cflags="-O2" \
 --extra-cxxflags="-O2" \
 && make -j8
