bf913215d62d615d4aec4eec512a20e8801f914b
Ryan Whelan <rwhelan@ll.mit.edu>
2013-05-24 14:48:24 -0400

    Specify CC in build scripts also, and fix old LLVM code so it compiles when clang isn't installed.

--- qemu/build.sh
+++ qemu/build.sh
@@ -2,6 +2,7 @@
 
 ./configure --target-list=x86_64-softmmu,x86_64-linux-user,i386-linux-user,\
 arm-linux-user,arm-softmmu \
+--cc=gcc-4.7 \
 --cxx=g++-4.7 \
 --prefix=`pwd`/install \
 --disable-pie \
