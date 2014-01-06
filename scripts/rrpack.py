c694299208a67228be9b79d0a9e99856d8d6bef2
Brendan Dolan-Gavitt <brendandg@gatech.edu>
2014-01-06 15:18:33 -0500

    Add scripts that pack/unpack record/replay logs.

---
+++ scripts/rrpack.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import sys, os
+import subprocess
+import struct
+import hashlib
+
+RRPACK_MAGIC = "PANDA_RR"
+
+# PANDA Packed RR file format (all integers are little-endian):
+# 0x00: magic "PANDA_RR"
+# 0x08: uint64_t num_instructions
+# 0x10: MD5 (16 bytes) of remaining data
+# 0x20: archive data in .tar.xz format
+
+if len(sys.argv) != 2:
+    print >>sys.stderr, "usage: %s <rr_basename>" % sys.argv[0]
+    sys.exit(1)
+
+base = sys.argv[1]
+outfname = base + '.rr'
+
+if os.path.exists(outfname):
+    print >>sys.stderr, "%s already exists; will not overwrite. Aborting." % outfname
+    sys.exit(1)
+
+# Get number of instructions
+try:
+    with open(base + '-rr-nondet.log', 'rb') as f:
+        # num_guest_insns is 64-bit int at offset 16
+        f.seek(16)
+        num_guest_insns = struct.unpack("<Q", f.read(8))[0]
+except EnvironmentError:
+    print >>sys.stderr, "Failed to open", base + '-rr-nondet.log. Aborting.'
+    sys.exit(1)
+
+print "Packing RR log %s with %d instructions..." % (base, num_guest_insns)
+outf = open(outfname, 'wb')
+outf.write(RRPACK_MAGIC)
+outf.write(struct.pack("<Q", num_guest_insns))
+outf.write("\0" * 16) # Placeholder for checksum
+outf.flush()
+subprocess.check_call(['tar', 'cJf', '-', base + '-rr-snp', base + '-rr-nondet.log'], stdout=outf)
+outf.close()
+
+print "Calculating checksum...",
+outf = open(outfname, 'r+b')
+outf.seek(0x20)
+m = hashlib.md5()
+while True:
+    data = outf.read(4096)
+    if not data: break
+    m.update(data)
+digest = m.digest()
+outf.seek(0x10)
+outf.write(digest)
+outf.close()
+print "Done."
---
+++ scripts/rrunpack.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import sys, os
+import subprocess
+import struct
+import hashlib
+
+RRPACK_MAGIC = "PANDA_RR"
+
+# PANDA Packed RR file format (all integers are little-endian):
+# 0x00: magic "PANDA_RR"
+# 0x08: uint64_t num_instructions
+# 0x10: MD5 (16 bytes) of remaining data
+# 0x20: archive data in .tar.xz format
+
+if len(sys.argv) != 2:
+    print >>sys.stderr, "usage: %s <filename.rr>" % sys.argv[0]
+    sys.exit(1)
+
+infname = sys.argv[1]
+
+# Get file info
+try:
+    with open(infname, 'rb') as f:
+        magic, num_guest_insns, file_digest = struct.unpack("<8sQ16s", f.read(0x20))
+        if magic != RRPACK_MAGIC:
+            print >>sys.stderr, infname, "is not in PANDA Record/Replay format"
+            sys.exit(1)
+        print "Verifying checksum...",
+        m = hashlib.md5()
+        while True:
+            data = f.read(4096)
+            if not data: break
+            m.update(data)
+        digest = m.digest()
+        if digest != file_digest:
+            print "FAILED. Aborting."
+            sys.exit(1)
+        else:
+            print "Success."
+        f.seek(0x20)
+        print "Unacking RR log %s with %d instructions..." % (infname, num_guest_insns),
+        subprocess.check_call(['tar', 'xJvf', '-'], stdin=f)
+        print "Done."
+except EnvironmentError:
+    print >>sys.stderr, "Failed to open", infname
+    sys.exit(1)
