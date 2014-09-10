bff19db14438f022f122625fdd7dc2d6e44076fd
Patrick Hulin <patrick.hulin@gmail.com>
2014-09-10 16:43:22 -0400

    Change defaults to those for bug2

--- qemu/panda_plugins/useafterfree/useafterfree.cpp
+++ qemu/panda_plugins/useafterfree/useafterfree.cpp
@@ -474,11 +474,11 @@ bool init_plugin(void *self) {
     panda_arg_list *args = panda_get_args("useafterfree");
 
     // Addresses for alloc/free/realloc
-    alloc_guest_addr = panda_parse_ulong(args, "alloc", 0x77D72DD6);
-    free_guest_addr = panda_parse_ulong(args, "free", 0x77D72C6A);
-    realloc_guest_addr = panda_parse_ulong(args, "realloc", 0x77D8FF51);
+    alloc_guest_addr = panda_parse_ulong(args, "alloc", 0x7787209D);
+    free_guest_addr = panda_parse_ulong(args, "free", 0x77871F31);
+    realloc_guest_addr = panda_parse_ulong(args, "realloc", 0x77877E54);
     // CR3 to watch.
-    right_cr3 = panda_parse_ulong(args, "cr3", 0x7F893460);
+    right_cr3 = panda_parse_ulong(args, "cr3", 0x3F98B320);
     // Size of words on target OS.
     word_size = panda_parse_uint64(args, "word", 4);
 
