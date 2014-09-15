550aedf88f9ab80623a9bca6346c383a79e05592
Ryan Whelan <rwhelan@ll.mit.edu>
2014-09-15 16:05:55 -0400

    Update and check in remainder of ARM hypercall stuff that was hiding in a git stash.

--- qemu/panda_plugins/taint/tests/include/gcc/panda_mark.h
+++ qemu/panda_plugins/taint/tests/include/gcc/panda_mark.h
@@ -1,12 +1,17 @@
 #ifndef __PANDA_MARK_H__
 #define __PANDA_MARK_H__
 
+#if !defined(TARGET_I386) && !defined(TARGET_ARM)
+#error "Define your architecture (TARGET_I386 or TARGET_ARM) with -D"
+#endif
+
 const int LABEL_BUFFER = 7;
 const int LABEL_BUFFER_POS = 8;
 const int QUERY_BUFFER = 9;
 
+#ifdef TARGET_I386
 inline
-void cpu_id(unsigned long buf, unsigned long len, unsigned long off, int action) {
+void hypercall(unsigned long buf, unsigned long len, unsigned long off, int action) {
   unsigned long rax = action;
   unsigned long rbx = buf;
   unsigned long rcx = len;
@@ -34,14 +39,40 @@ void cpu_id(unsigned long buf, unsigned long len, unsigned long off, int action)
   return;
 }
 
+#endif // TARGET_I386
+
+#ifdef TARGET_ARM
+inline
+void hypercall(unsigned long buf, unsigned long len, unsigned long off, int action) {
+    unsigned long r0 = action;
+    unsigned long r1 = buf;
+    unsigned long r2 = len;
+    unsigned long r3 = off;
+
+    asm __volatile__
+      ("push {%%r0-%%r3} \t\n\
+        mov %%r0, %0 \t\n\
+        mov %%r1, %1 \t\n\
+        mov %%r2, %2 \t\n\
+        mov %%r3, %3 \t\n\
+        mcr p7, 0, r0, c0, c0, 0 \t\n\
+        pop {%%r0-%%r3} \t\n"
+      
+      : /* no output registers */
+      : "r" (r0), "r" (r1), "r" (r2), "r" (r3) /* input operands */
+      : "r0", "r1", "r2", "r3" /* clobbered registers */
+      );
+    return;
+}
+#endif // TARGET_ARM
+
 /* buf is the address of the buffer to be labeled
  * len is the length of the buffer to be labeled */
 inline
 void label_buffer(unsigned long buf, unsigned long len) {
   printf("Address to be labeled: 0x%lx\n", buf);
   printf("Size in bytes: %lu\n", len);
-  //Ignore offset for now
-  cpu_id(buf, len, 0, LABEL_BUFFER);
+  hypercall(buf, len, 0, LABEL_BUFFER);
   return;
 }
 
@@ -51,8 +82,7 @@ inline
 void query_buffer(unsigned long buf, unsigned long len) {
   printf("Address to be queried: 0x%lx\n", buf);
   printf("Size in bytes: %lu\n", len);
-  //Ignore offset for now
-  cpu_id(buf, len, 0, QUERY_BUFFER);
+  hypercall(buf, len, 0, QUERY_BUFFER);
   return;
 }
 
