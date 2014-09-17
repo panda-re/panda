3c85d7a1e32d746c370bb2ac9e2f1e6a9f44511b
Patrick Hulin <patrick.hulin@gmail.com>
2014-09-17 16:06:35 -0400

    add initialmem plugin

---
+++ qemu/panda_plugins/initialmem/initialmem.cpp
@@ -0,0 +1,57 @@
+/* PANDABEGINCOMMENT
+ * 
+ * Authors:
+ *  Tim Leek               tleek@ll.mit.edu
+ *  Ryan Whelan            rwhelan@ll.mit.edu
+ *  Joshua Hodosh          josh.hodosh@ll.mit.edu
+ *  Michael Zhivich        mzhivich@ll.mit.edu
+ *  Brendan Dolan-Gavitt   brendandg@gatech.edu
+ * 
+ * This work is licensed under the terms of the GNU GPL, version 2. 
+ * See the COPYING file in the top-level directory. 
+ * 
+PANDAENDCOMMENT */
+// This needs to be defined before anything is included in order to get
+// the PRIx64 macro
+#define __STDC_FORMAT_MACROS
+
+extern "C" {
+
+#include "config.h"
+#include "qemu-common.h"
+#include "rr_log.h"
+
+#include "panda_plugin.h"
+
+}
+
+#include <stdio.h>
+
+// These need to be extern "C" so that the ABI is compatible with
+// QEMU/PANDA, which is written in C
+extern "C" {
+
+bool init_plugin(void *);
+void uninit_plugin(void *);
+
+int before_block_exec(CPUState *env, TranslationBlock *tb);
+
+}
+
+int before_block_exec(CPUState *env, TranslationBlock *tb) {
+    printf("Saving memory to initial.raw.\n");
+    panda_memsavep(fopen("initial.raw", "wb"));
+    rr_do_end_replay(0);
+    return 0;
+}
+
+bool init_plugin(void *self) {
+    panda_cb pcb = { .before_block_exec = before_block_exec };
+    panda_register_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC, pcb);
+
+    return true;
+}
+
+void uninit_plugin(void *self) {
+
+}
