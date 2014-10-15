6409d9a681981ba9ca7254b8062c5f76b8609c6d
Tim Leek <tleek@ll.mit.edu>
2014-10-15 11:42:42 -0400

    tralign now works by trying to align subset of trace for a cr3 and also knows how to match up cr3s between two traces

--- qemu/panda_plugins/tralign/tralign.cpp
+++ qemu/panda_plugins/tralign/tralign.cpp
@@ -44,9 +44,6 @@ extern "C" {
 #ifdef CONFIG_SOFTMMU
 
 
-// alignment will be with respect to this many blocks.  
-// 0 means every bb is indexed individually
-uint32_t numblocks = 0;
 
 // n grams
 uint32_t min_n = 1;
@@ -57,22 +54,45 @@ uint64_t total_instr = 0;
 char *traceind_pfx = NULL;
 
 // current block
-uint32_t block_num = 0;
+uint64_t block_num = 0;
 
 uint8_t *buffer = NULL;
 uint32_t buffer_len = 0;
 uint32_t buffer_max = 0;
 
-void *ind;
-void *indc;
+// one index per cr3
+std::map < uint64_t, void * > ind;
+std::map < uint64_t, void * > indc;
 
-uint32_t bb_counter = 0;
 
 uint64_t instr_this_block = 0;
 uint64_t instr_this_block_indexed = 0;
 uint64_t instr_per_block = 0;
 
 
+
+// write physmem to a file
+// and invoke volatility on it
+void vol(char *outfilename) {
+    printf ("vol %s\n", outfilename);
+    std::string pmf = (std::string(outfilename)) + ".pmem";
+    FILE *fp = fopen((char *) pmf.c_str(), "w");
+    panda_memsavep(fp);
+    fclose(fp);
+    std::string cmd = "/usr/bin/volatility -f " + pmf + " --profile=Linux_Debian_Wheezy_3_2_0-4-686-pae_x86 linux_pslist";
+    fp = popen(cmd.c_str(), "r");
+    FILE *fp2 = fopen(outfilename, "w");
+    char line[4096];
+    while (fgets(line, sizeof(line)-1, fp) != NULL) {
+        fprintf(fp2, "%s", line);
+    }
+    fclose(fp2);
+    printf ("...done\n");
+}
+ 
+    
+    
+    
         
 bool pdice (float prob_yes) {
     if ((((float) (rand ())) / RAND_MAX) < prob_yes)
@@ -82,14 +102,32 @@ bool pdice (float prob_yes) {
 }
 
 
-float pincludebb = 0.01;
+
 
 
 uint64_t total_bb = 0;
 
+
+std::map < uint64_t, uint64_t > trace_bb_num_to_guest_instr_count;
+
+
 int tralign_before_block_exec(CPUState *env, TranslationBlock *tb) {
+    uint64_t cr3 = panda_current_asid(env);
+
+    if (indc.count(cr3) == 0) {
+        std::string pfx = std::string(traceind_pfx) + "-" + std::to_string(cr3);
+        indc[cr3] = new_index_common_c((char *) pfx.c_str(), min_n, max_n, 100);    
+        ind[cr3] = new_index_c();
+    }
     if (tb->size > 16) {
         total_bb ++;
+        /*
+        if ((total_bb % 100000) == 0) {
+            std::string fn = "/data/laredo/tleek/vo-" + (std::to_string(rr_get_guest_instr_count()));
+            vol((char *) fn.c_str());
+        }
+        return 0;
+        */
         if (!rr_in_replay()) {
             return 0;
         }
@@ -103,10 +141,14 @@ int tralign_before_block_exec(CPUState *env, TranslationBlock *tb) {
         }
         panda_virtual_memory_rw(env, tb->pc, buffer, tb->size, 0);
         // index it        
-        index_this_passage_c(indc, ind, buffer, tb->size, block_num);
+        index_this_passage_c(indc[cr3], ind[cr3], buffer, tb->size, block_num);
+        // maintain mapping from trace basic block number back to where in 
+        trace_bb_num_to_guest_instr_count [block_num] = rr_get_guest_instr_count();
+        block_num ++;
     }
-    block_num ++;
-  
+    //    if ((block_num % 100000) == 0) {
+    //        printf ("block_num = %lu\n", block_num);
+    //    }
     return 0;
 }
 
@@ -123,10 +165,6 @@ bool init_plugin(void *self) {
     if (args != NULL) {
         int i;
         for (i = 0; i < args->nargs; i++) {
-            if (0 == strncmp(args->list[i].key, "numblocks", 9)) {
-                numblocks = atoi(args->list[i].value);
-                printf ("numblocks = %d\n", numblocks);
-            }
             if (0 == strncmp(args->list[i].key, "min_n", 5)) {
                 min_n = atoi(args->list[i].value);
             }
@@ -136,24 +174,19 @@ bool init_plugin(void *self) {
             if (0 == strncmp(args->list[i].key, "traceind_pfx", 13)) {
                 traceind_pfx = args->list[i].value;
             }
-            if (0 == strncmp(args->list[i].key, "pincludebb", 10)) {
-                pincludebb = atof(args->list[i].value);
-                printf ("prob of incl a bb in trinv is %.5f\n", pincludebb);
-            }
         }
     }
     if (traceind_pfx == NULL) {
         traceind_pfx = strdup("/tmp/trinv");
     }
     printf ("tralign n=%d,%d traceind_pfx=%s\n", min_n, max_n, traceind_pfx);
-    printf ("numblocks = %d pincludebb=%.6f\n", numblocks, pincludebb);
     panda_cb pcb;
     pcb.before_block_exec = tralign_before_block_exec;
     panda_register_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC, pcb);
     buffer_max = 1024;
     buffer = (uint8_t *) malloc(buffer_max);
-    indc = new_index_common_c(traceind_pfx, min_n, max_n, 100);    
-    ind = new_index_c();
+    //    indc = new_index_common_c(traceind_pfx, min_n, max_n, 100);    
+    //    ind = new_index_c();
     return true;
 
 #endif
@@ -163,17 +196,45 @@ bool init_plugin(void *self) {
 
 
 
+
+void marshall_uint64_uint64_map(std::string filename, std::map < uint64_t, uint64_t > &uumap) {
+    FILE *fp = fopen ((char *) filename.c_str(), "w");
+    uint64_t occ = uumap.size();
+    WU(occ);
+    for ( auto &kvp : uumap ) {
+        WU(kvp.first);
+        WU(kvp.second);
+    }
+    fclose(fp);
+}
+
+
+
+
 void uninit_plugin(void *self) {
 #ifdef CONFIG_SOFTMMU
-    printf ("total bb = %lu\n", total_bb);
-    printf ("marshalling index common\n");
-    marshall_index_common_c(indc);
-    printf ("marshalling index\n");
-    marshall_index_c(indc, ind, traceind_pfx);
-    printf ("inverting\n");
-    void *inv = invert_c(indc, ind);
-    printf ("marshalling inv index\n");  
-    marshall_invindex_c(indc, inv, traceind_pfx);
-
+    uint32_t n = ind.size();
+    printf ("found %d cr3s\n", ind.size());
+    std::string fn = std::string(traceind_pfx) + ".cr3";
+    FILE *fp = fopen(fn.c_str(), "w");
+    fwrite ((void *) &n, sizeof(n), 1, fp);
+    for ( auto &kvp : ind ) {
+        uint64_t cr3 = kvp.first;
+        fwrite((void *) &cr3, sizeof(cr3), 1, fp);
+        printf ("cr3=0x%lx\n", cr3);
+        Index *i = reinterpret_cast<Index *> (kvp.second);
+        IndexCommon *ic = reinterpret_cast<IndexCommon *> (indc[cr3]);
+        printf ("marshalling index common\n");
+        marshall_index_common_c(ic);
+        printf ("marshalling index\n");
+        marshall_index_c(ic, i, traceind_pfx);
+        printf ("inverting\n");
+        void *inv = invert_c(ic, i);
+        printf ("marshalling inv index\n");  
+        marshall_invindex_c(ic, inv, traceind_pfx);
+    }
+    fclose(fp);
+    std::string filename = (std::string(traceind_pfx)) + ".tr2rr";
+    marshall_uint64_uint64_map(filename, trace_bb_num_to_guest_instr_count);
 #endif
 }
--- qemu/panda_plugins/tralign/traligner.cpp
+++ qemu/panda_plugins/tralign/traligner.cpp
@@ -18,16 +18,52 @@ extern "C"{
 using namespace std;
 
 
+std::vector < uint64_t > read_cr3s(std::string tr_pfx) {
+    std::vector < uint64_t > cr3s;
+    std::string fn = tr_pfx + ".cr3";
+    FILE *fp = fopen((char *) fn.c_str(), "r");
+    uint32_t n;
+    fread((void *) &n, sizeof(n), 1, fp);
+    for (uint32_t i=0; i<n; i++) {
+        uint64_t cr3;
+        fread((void *) &cr3, sizeof(cr3), 1, fp);
+        cr3s.push_back(cr3);
+    }
+    return cr3s;
+}
+
+
+bool mysf (std::pair < uint64_t, uint32_t > p1, std::pair < uint64_t, uint32_t > p2 ) {
+    return (p1.second > p2.second);
+}
+
+
 int main (int argc, char **argv) {
     
-    if (argc != 3) {
-        printf ("traligner trace_inv_index_pfx_1 trace_inv_index_pfx_2\n");
+    if (argc != 6) {
+        printf ("traligner trace_inv_index_pfx_1 cr3_1 trace_inv_index_pfx_2 cr3_2 threshold\n");
         exit (1);
     }
 
     std::string tr1_pfx = std::string(argv[1]);
-    std::string tr2_pfx = std::string(argv[2]);
-    
+    uint64_t cr3_1 = strtoul(argv[2], 0, 16);
+    std::string tr2_pfx = std::string(argv[3]);
+    uint64_t cr3_2 = strtoul(argv[4], 0, 16);
+    float threshold = atof(argv[5]);
+
+    printf ("tr1_pfx = [%s]\n", tr1_pfx.c_str());
+    printf ("tr2_pfx = [%s]\n", tr2_pfx.c_str());
+    printf ("cr3_1=0x%lx cr3_2=0x%lx\n", cr3_1, cr3_2);
+    printf ("threshold = %.4f\n", threshold);
+
+    printf ("unmarshalling tr2rr for tr1\n");
+    std::map < uint64_t, uint64_t > bbn1_to_tr1 = unmarshall_uint64_uint64_map(tr1_pfx + ".tr2rr");
+    printf ("unmarshalling tr2rr for tr2\n");
+    std::map < uint64_t, uint64_t > bbn2_to_tr2 = unmarshall_uint64_uint64_map(tr2_pfx + ".tr2rr");
+
+    tr1_pfx = tr1_pfx + "-" + (std::to_string(cr3_1));
+    tr2_pfx = tr2_pfx + "-" + (std::to_string(cr3_2));                         
+
     printf ("tr1 is %s\n", tr1_pfx.c_str());
     printf ("tr2 is %s\n", tr2_pfx.c_str());
     printf ("unmarshalling commons\n");
@@ -39,7 +75,9 @@ int main (int argc, char **argv) {
     Index *ind2 = unmarshall_index(tr2_pfx, indc2, true);
     indc1->num_uind = ind1->binary_to_uind.size();
     printf ("unmarshalling pp scores for tr2\n");
-    PpScores *pps2 = unmarshall_preprocessed_scores(std::string(tr2_pfx));
+    PpScores *pps2 = unmarshall_preprocessed_scores(tr2_pfx);
+    
+        
     printf ("looking for corresponding bb in both traces\n");
 
     printf ("total of %d bb in tr1\n", indc1->num_passages);
@@ -48,61 +86,120 @@ int main (int argc, char **argv) {
     uint32_t i=0;
     uint32_t m=0;
     uint32_t mode = 0;
-    uint32_t last_p1, last_p2;
-    std::set < std::pair < uint32_t, uint32_t > > edge;
-    std::set < uint32_t > ps;
+    uint64_t last_p1, last_p2, mp1, mp2;
+    mp1=mp2=0;
+    std::set < std::pair < uint64_t, uint64_t > > edge;
+    std::set < uint64_t > ps1;
+    std::set < uint64_t > ps2;
+    std::map < uint64_t, uint64_t > match;
     for ( auto &kvp : ind1->uind_to_passage ) {
-        if ((i%1000) == 0) {
-            printf ("%d uinds. %d have matches\n", i, m);
-        }
         uint32_t uind1 = kvp.first;
-        if (indc1->uind_to_psgs[uind1].size() > 1) {
-            continue;
-        }
         Passage *passage1 = (Passage *) (&kvp.second);
         uint32_t uind2;
         float score;
         // find unique passage in tr2 that corresponds to passage1
         query_with_passage(indc2, passage1, pps2, &uind2, &score);
         if (score > 2.5) {
+            match[uind1] = uind2;                
+        }
+
+        printf ("score = %.3f\n", score);
+
+        if (indc1->uind_to_psgs[uind1].size() > 1) {
+            continue;
+        }
+
+        if (score > 2.5) {
             if (indc2->uind_to_psgs[uind2].size() == 1) {
-                uint32_t p1 = *(indc1->uind_to_psgs[uind1].begin());
-                uint32_t p2 = *(indc2->uind_to_psgs[uind2].begin());
-                std::pair <uint32_t, uint32_t> e = std::make_pair(p1, p2);
-                ps.insert(p1);
+                uint64_t p1 = *(indc1->uind_to_psgs[uind1].begin());
+                uint64_t p2 = *(indc2->uind_to_psgs[uind2].begin());
+                std::pair <uint64_t, uint64_t> e = std::make_pair(p1, p2);
+                ps1.insert(p1);
+                ps2.insert(p2);
                 edge.insert(e);
-                printf ("edge %d 1 %d 2\n", p1, p2);
+                printf ("singlteon match %d 1 %d 2\n", p1, p2);
             }
         }
+
         i ++;
     }     
+
+    printf ("%d uniq psgs in tr 1.  %d have matches\n", ind1->uind_to_passage.size(), match.size());
+
     printf ("found %d singletons -- expanding\n", (int) edge.size());
+    FILE *fp = fopen("passages1u2", "w");
+    for (uint32_t p=0; p<indc1->num_passages; p++) {
+        uint32_t uind1 = ind1->passages[p];        
+        if (match.count(uind1) != 0) {
+            fprintf (fp, "%d\n", match[uind1]);
+        }
+    }
+    fclose(fp);
+    fp = fopen("passages2u2", "w");
+    for (uint32_t p=0; p<indc2->num_passages; p++) {
+        fprintf(fp, "%d\n", ind2->passages[p]);
+    }
+    fclose(fp);
+    
     for ( auto &el : edge ) {
         for (int s=-1; s<=1; s+=2) {
-            uint32_t p1 = el.first;
-            uint32_t p2 = el.second;
-            printf ("considering edge %d %d\n", p1, p2);
-            for (uint32_t i=0; i<3; i++) {
-                p1 += s;
-                p2 += s;
-                printf ("%d %d\n", p1, p2);
-                if (ps.count(p1) != 0) {                    
-                    break;
+            uint64_t start1 = el.first;
+            uint64_t start2 = el.second;           
+            uint64_t p1 = start1;
+            uint64_t p2 = start2;
+            printf ("considering %d %d\n", p1, p2);
+            uint64_t i = 0;
+            while (true) {
+                if (s == -1) {
+                    assert (ind1->passages.lower_bound(p1 - 1) != ind1->passages.end());
+                    assert (ind2->passages.lower_bound(p1 - 1) != ind2->passages.end());
+                    p1 = ind1->passages.lower_bound(p1 - 1)->first;
+                    p2 = ind1->passages.lower_bound(p2 - 1)->first;
                 }
+                else {
+                    assert (ind1->passages.upper_bound(p1 - 1) != ind1->passages.end());
+                    assert (ind2->passages.upper_bound(p1 - 1) != ind2->passages.end());
+                    p1 = ind1->passages.upper_bound(p1)->first;
+                    p2 = ind1->passages.upper_bound(p2)->first;
+                }
+                printf("p1=%d p2=%d\n", p1, p2);
                 uint32_t uind1 = ind1->passages[p1];
                 uint32_t uind2 = ind2->passages[p2];
-                uint32_t best_uind2;
-                float score;
-                Passage *passage = &(ind1->uind_to_passage[uind1]);
-                query_with_passage(indc2, passage, pps2, &best_uind2, &score);
-                if (score > 2.5) {
-                    if (best_uind2 == uind2) {
-                        printf ("edge %d 1 %d 2\n", p1, p2);
-                        continue;
-                    }
+                printf ("uind1=%d uind2=%d\n", uind1, uind2);
+                if (match.count(uind1) == 0) {
+                    // we dont have a match for this uind in trace 2
+                    printf ("no match at %d\n", p1);
+                    break;
+                } 
+                uint32_t best_uind2 = match[uind1];
+                printf ("best uind2 match = %d\n", best_uind2);
+                if (best_uind2 == uind2) {
+                    printf ("continuing...\n");
+                    // ok p1 & p2 correspond -- keep going
+                    //                    printf ("adding %lu to ps\n", p1);
+                    ps1.insert(p1);
+                    ps2.insert(p2);
+                    i++;
+                    continue;                    
                 }
                 break;
             }
+            printf ("i=%d\n", i);
+            if (i>0) {
+                if (p1 > indc1->num_passages) { p1 = indc1->num_passages; }
+                if (p2 > indc2->num_passages) { p2 = indc2->num_passages; }
+                // num of replay instructions in this extent
+                uint64_t len = bbn1_to_tr1[p1] - bbn1_to_tr1[start1];
+                if (p1 < start1) {
+                    std::swap(p1, start1);
+                    len = -len;
+                }                
+                assert (bbn1_to_tr1.count(start1) != 0);
+                assert (bbn2_to_tr2.count(start2) != 0);
+                printf ( "edge %lu,%lu,%lu,%lu,%lu\n", start1, start2, bbn1_to_tr1[start1], bbn2_to_tr2[start2], len);
+            }
+            printf ("%d in ps1 %d in ps2\n", ps1.size(), ps2.size());
         }
     }        
+
 }
