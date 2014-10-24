b95150fe6ec8f5655a6873afcc9bfe736e2bb689
Brendan Dolan-Gavitt <brendandg@gatech.edu>
2014-10-23 22:03:52 -0400

    Add C++ guards so .h files can be used in C too

--- qemu/panda/panda_common.h
+++ qemu/panda/panda_common.h
@@ -7,13 +7,11 @@ extern "C" {
 
 #include "cpu.h"
 
+target_ulong panda_current_pc(CPUState *env);
+target_ulong panda_current_asid(CPUState *env);
+
 #ifdef __cplusplus
 }
 #endif
-
-extern "C" {
-target_ulong panda_current_pc(CPUState *env);
-target_ulong panda_current_asid(CPUState *env);
-}
 
 #endif
