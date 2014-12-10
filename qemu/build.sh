5f80c7c6979493c89bdba170af26e652fdb2543c
Manolis Stamatogiannakis <manolis.stamatogiannakis@vu.nl>
2014-12-10 01:17:32 +0100

    build.sh respects CC/CXX envirnonment variables. Linker flags for stuff in /usr/local. LLVM version removed from flag. LLVM 3.3 is still the suggested version.

--- qemu/build.sh
+++ qemu/build.sh
@@ -3,14 +3,15 @@
 python ../scripts/apigen.py
 
 ./configure --target-list=x86_64-softmmu,i386-softmmu,arm-softmmu \
---cc=gcc-4.7 \
---cxx=g++-4.7 \
+--cc=${CC:=gcc-4.7} \
+--cxx=${CXX:=g++-4.7} \
 --prefix=`pwd`/install \
 --disable-pie \
 --disable-xen \
 --disable-libiscsi \
 --enable-llvm \
---with-llvm=../llvm-3.3/Debug+Asserts \
+--with-llvm=../llvm/Debug+Asserts \
 --extra-cflags="-O2" \
 --extra-cxxflags="-O2" \
+--extra-ldflags="-L/usr/local/lib -L/usr/local/lib64" \
 && make -j $(nproc)
