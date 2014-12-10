bc2a924885b8c7db12432bfb52ba2216a87490bb
Brendan Dolan-Gavitt <brendan@panda.gtisc.gatech.edu>
2014-12-10 06:38:27 +0000

    Turns out ffmpeg understands ppm just fine.
    Accordingly, removing the png->ppm conversion

--- qemu/panda_plugins/replaymovie/movie.sh
+++ qemu/panda_plugins/replaymovie/movie.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 #
-# NOTE: you may need to apt-get install all of the following:
-# parallel
-# graphicsmagick
+# NOTE: you may need to apt-get install the following:
 # ffmpeg
 #
 
-ls *.ppm | parallel -m gm mogrify -format png -colorspace RGB
-rm replay_movie*.ppm
-ffmpeg -y -threads 0 -r 20 -i replay_movie_%03d.png replay.mp4 -qscale 5 -b 9600
+ffmpeg -y -threads 0 -r 20 -i replay_movie_%03d.ppm replay.mp4 -qscale 5 -b 9600
