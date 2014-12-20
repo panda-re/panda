c66025a1d7497df5e230e2d1ec6efeef2e64f947
Brendan Dolan-Gavitt <brendandg@gatech.edu>
2014-12-20 14:36:20 -0500

    Add unicode string support to memstrings

--- qemu/panda_plugins/memstrings/memstrings.cpp
+++ qemu/panda_plugins/memstrings/memstrings.cpp
@@ -27,6 +27,7 @@ extern "C" {
 #include "rr_log.h"
 }
 
+#include <wctype.h>
 #include <zlib.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -52,18 +53,26 @@ struct string_pos {
     uint8_t ch[MAX_STRLEN];
 };
 
+struct ustring_pos {
+    int nch;
+    uint16_t ch[MAX_STRLEN];
+};
+
 std::map<target_ulong,string_pos> read_text_tracker;
 std::map<target_ulong,string_pos> write_text_tracker;
+std::map<target_ulong,ustring_pos> read_utext_tracker;
+std::map<target_ulong,ustring_pos> write_utext_tracker;
 
 gzFile mem_report = NULL;
 int min_strlen;
 
 int mem_callback(CPUState *env, target_ulong pc, target_ulong addr,
-                       target_ulong size, void *buf, bool is_write,
-                       std::map<target_ulong,string_pos> &text_tracker) {
+                       target_ulong size, void *buf, bool is_write) {
 
-    string_pos &sp = text_tracker[pc];
+    string_pos &sp = is_write ? write_text_tracker[pc] : read_text_tracker[pc];
+    ustring_pos &usp = is_write ? write_utext_tracker[pc] : read_utext_tracker[pc];
 
+    // ASCII
     for (unsigned int i = 0; i < size; i++) {
         uint8_t val = ((uint8_t *)buf)[i];
         if (isprint(val)) {
@@ -82,19 +91,54 @@ int mem_callback(CPUState *env, target_ulong pc, target_ulong addr,
             sp.nch = 0;
         }
     }
- 
+
+    // Don't consider one-byte reads/writes for UTF-16
+    if (size < 2) {
+        return 1;
+    }
+
+    // UTF-16-LE
+    for (unsigned int i = 0; i < size; i+=2) {
+        uint8_t vall = ((uint8_t *)buf)[i];
+        uint8_t valh = ((uint8_t *)buf)[i+1];
+        uint16_t val = (valh << 8) | vall;
+        if (iswprint(val)) {
+            usp.ch[usp.nch++] = val;
+            // If we max out the string, chop it
+            if (usp.nch == MAX_STRLEN - 1) {
+                gsize bytes_written = 0;
+                gchar *out_str = g_convert((gchar *)usp.ch, usp.nch*2,
+                    "UTF-8", "UTF-16LE", NULL, &bytes_written, NULL);
+                gzprintf(mem_report, "%s\n", out_str);
+                g_free(out_str);
+                usp.nch = 0;
+            }
+        }
+        else {
+            // Don't bother with strings shorter than min
+            if (usp.nch >= min_strlen) {
+                gsize bytes_written = 0;
+                gchar *out_str = g_convert((gchar *)usp.ch, usp.nch*2,
+                    "UTF-8", "UTF-16LE", NULL, &bytes_written, NULL);
+                gzprintf(mem_report, "%s\n", out_str);
+                g_free(out_str);
+            }
+            usp.nch = 0;
+        }
+    }
+
     return 1;
 }
 
 int mem_read_callback(CPUState *env, target_ulong pc, target_ulong addr,
                        target_ulong size, void *buf) {
-    return mem_callback(env, pc, addr, size, buf, false, read_text_tracker);
+    return mem_callback(env, pc, addr, size, buf, false);
 
 }
 
 int mem_write_callback(CPUState *env, target_ulong pc, target_ulong addr,
                        target_ulong size, void *buf) {
-    return mem_callback(env, pc, addr, size, buf, true, write_text_tracker);
+    return mem_callback(env, pc, addr, size, buf, true);
 }
 
 bool init_plugin(void *self) {
@@ -131,11 +175,34 @@ bool init_plugin(void *self) {
 
 void uninit_plugin(void *self) {
     // Save any that we haven't flushed yet
-    for (auto &kvp : read_text_tracker)
-        if (kvp.second.nch > min_strlen)
+    for (auto &kvp : read_text_tracker) {
+        if (kvp.second.nch > min_strlen) {
             gzprintf(mem_report, "%.*s\n", kvp.second.nch, kvp.second.ch);
-    for (auto &kvp : write_text_tracker)
-        if (kvp.second.nch > min_strlen)
+        }
+    }
+    for (auto &kvp : write_text_tracker) {
+        if (kvp.second.nch > min_strlen) {
             gzprintf(mem_report, "%.*s\n", kvp.second.nch, kvp.second.ch);
+        }
+    }
+    for (auto &kvp : read_utext_tracker) {
+        if (kvp.second.nch > min_strlen) {
+            gsize bytes_written = 0;
+            gchar *out_str = g_convert((gchar *)kvp.second.ch, kvp.second.nch*2,
+                "UTF-8", "UTF-16LE", NULL, &bytes_written, NULL);
+            gzprintf(mem_report, "%s\n", out_str);
+            g_free(out_str);
+        }
+    }
+    for (auto &kvp : write_utext_tracker) {
+        if (kvp.second.nch > min_strlen) {
+            gsize bytes_written = 0;
+            gchar *out_str = g_convert((gchar *)kvp.second.ch, kvp.second.nch*2,
+                "UTF-8", "UTF-16LE", NULL, &bytes_written, NULL);
+            gzprintf(mem_report, "%s\n", out_str);
+            g_free(out_str);
+        }
+    }
+
     gzclose(mem_report);
 }
