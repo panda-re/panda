533b3b26ff59c683033023efa91acc4435423e08
Patrick Hulin <patrick.hulin@gmail.com>
2015-03-23 17:17:54 -0400

    asidstory: Even prettire.

--- qemu/panda_plugins/asidstory/asidstory.cpp
+++ qemu/panda_plugins/asidstory/asidstory.cpp
@@ -71,6 +71,8 @@ bool pid_ok(int pid) {
 #define SAMPLE_CUTOFF 10    
 #define SAMPLE_RATE 1
 #define MILLION 1000000
+#define NAMELEN 10
+#define NAMELENS "10"
 
 uint64_t a_counter = 0;
 uint64_t b_counter = 0;
@@ -97,6 +99,7 @@ struct NamePid {
 };
 
 struct ProcessData {
+    std::string shortname;
     std::map<Cell, Count> cells;
     Count count;
     Instr first;
@@ -109,7 +112,7 @@ std::map<NamePid, ProcessData> process_datas;
 typedef std::pair<NamePid, ProcessData> ProcessKV;
 
 static unsigned digits(uint64_t num) {
-    return num > 0 ? (int)(log10((double)num) + 1) : 1;
+    return std::to_string(num).size();
 }
 
 using std::hex;
@@ -120,42 +123,49 @@ using std::endl;
 void spit_asidstory() {
     FILE *fp = fopen("asidstory", "w");
 
-    for (auto &pd_kv : process_datas) {
+    std::vector<ProcessKV> count_sorted_pds(process_datas.begin(), process_datas.end());
+    std::sort(count_sorted_pds.begin(), count_sorted_pds.end(),
+            [](const ProcessKV &lhs, const ProcessKV &rhs) {
+                return lhs.second.count > rhs.second.count; });
+
+    std::stringstream head;
+    head << 
+        setw(digits(max_instr / SAMPLE_RATE)) << "Count" <<
+        setw(6) << "Pid" << "  " <<
+        setw(NAMELEN) << "Name" << "  " <<
+        setw(sizeof(target_ulong) * 2) << "Asid" <<
+        "  " << setw(digits(max_instr)) << "First" << 
+        "      " << setw(digits(max_instr)) << "Last" << endl;
+    fprintf(fp, "%s", head.str().c_str());
+    for (auto &pd_kv : count_sorted_pds) {
         const NamePid &namepid = pd_kv.first;
         const ProcessData &pd = pd_kv.second;
         if (pd.count >= SAMPLE_CUTOFF) {
             std::stringstream ss;
-            std::string shortname = namepid.name;
-            if (shortname.compare(shortname.size() - 4, 4, ".exe") == 0) {
-                shortname = shortname.substr(0, shortname.size() - 4); 
-            }
-            if (shortname.size() > 10) shortname = shortname.substr(0, 10);
-            ss << setw(6) << namepid.pid << "  " <<
-               setw(10) << shortname << " : " <<
-               "(count=" << setw(digits(max_instr / SAMPLE_RATE)) << pd.count <<
-               ", asid=0x" << setw(sizeof(target_ulong) * 2) <<
-               hex << setfill('0') << namepid.asid << dec << setfill(' ') <<
-               ") : " << setw(digits(max_instr)) << pd.first <<
-               " -> " << setw(digits(max_instr)) << pd.last << endl;
+            ss <<
+                setw(digits(max_instr / SAMPLE_RATE)) << pd.count <<
+                setw(6) << namepid.pid << "  " <<
+                setw(NAMELEN) << pd.shortname << "  " <<
+                setw(sizeof(target_ulong) * 2) <<
+                hex << namepid.asid << dec << setfill(' ') <<
+                "  " << setw(digits(max_instr)) << pd.first <<
+                "  ->  " << setw(digits(max_instr)) << pd.last << endl;
             fprintf(fp, "%s", ss.str().c_str());
         }
     }
 
     fprintf(fp, "\n");
 
-    std::vector<ProcessKV> sorted_pds(process_datas.begin(), process_datas.end());
-    std::sort(sorted_pds.begin(), sorted_pds.end(),
+    std::vector<ProcessKV> first_sorted_pds(process_datas.begin(), process_datas.end());
+    std::sort(first_sorted_pds.begin(), first_sorted_pds.end(),
             [](const ProcessKV &lhs, const ProcessKV &rhs) {
                 return lhs.second.first < rhs.second.first; });
 
-    for (auto &pd_kv : sorted_pds) {
-        const NamePid &namepid = pd_kv.first;
+    for (auto &pd_kv : first_sorted_pds) {
         const ProcessData &pd = pd_kv.second;
 
         if (pd.count >= SAMPLE_CUTOFF) {
-            std::stringstream ss;
-            ss << namepid.pid << "-" << namepid.name;
-            fprintf(fp, "%20s : [", ss.str().c_str());
+            fprintf(fp, "%" NAMELENS "s : [", pd.shortname.c_str());
             for (unsigned i = 0; i < num_cells; i++) {
                 auto it = pd.cells.find(i);
                 if (it == pd.cells.end() || it->second < 2) {
@@ -168,22 +178,14 @@ void spit_asidstory() {
         }
     }
 
-    fprintf(fp, "\n");
-    for (uint32_t i=5; i<num_cells; i+=5) {
-        uint64_t instr = i / scale;
-        fprintf (fp, "%20" PRIu64 " :  ", instr);
-        for (uint32_t j=0; j<i; j++) {
-            fprintf (fp, " ");
-        }
-        fprintf (fp, "^\n");
-    }
     fclose(fp);
-
 }
 
 char *last_name = 0;
 target_ulong last_pid = 0;
 target_ulong last_asid = 0;
+
+static std::map<std::string, unsigned> name_count;
 
 int asidstory_before_block_exec(CPUState *env, TranslationBlock *tb) {
     if ((a_counter % 1000000) == 0) {
@@ -203,10 +205,29 @@ int asidstory_before_block_exec(CPUState *env, TranslationBlock *tb) {
     }
     OsiProc *p = get_current_process(env);
     if (pid_ok(p->pid)) {
-        ProcessData &pd = process_datas[NamePid(p->name, p->pid, p->asid)];
+        const NamePid namepid(p->name, p->pid, p->asid);
+        ProcessData &pd = process_datas[namepid];
         // keep track of first rr instruction for each name/pid
         if (pd.first == 0) {
             pd.first = rr_get_guest_instr_count();
+
+            unsigned count = ++name_count[namepid.name];
+            std::string count_str(std::to_string(count));
+            std::string shortname(namepid.name);
+
+            if (shortname.compare(shortname.size() - 4, 4, ".exe") == 0) {
+                shortname = shortname.substr(0, shortname.size() - 4); 
+            }
+            if (count > 1) {
+                if (shortname.size() + count_str.size() > NAMELEN) {
+                    shortname = shortname.substr(0, NAMELEN - count_str.size()) + count_str;
+                } else {
+                    shortname += count_str;
+                }
+            } else if (shortname.size() > NAMELEN) {
+                shortname = shortname.substr(0, NAMELEN);
+            }
+            pd.shortname = shortname;
         }
         if (pandalog) {
             if (last_name == 0
@@ -266,7 +287,7 @@ bool init_plugin(void *self) {
     panda_register_callback(self, PANDA_CB_AFTER_BLOCK_EXEC, pcb);
     
     panda_arg_list *args = panda_get_args("asidstory");
-    num_cells = panda_parse_ulong(args, "width", 100) - 25;
+    num_cells = std::max(panda_parse_uint64(args, "width", 100), 80UL) - NAMELEN - 5;
     
     min_instr = 0;   
     return true;
