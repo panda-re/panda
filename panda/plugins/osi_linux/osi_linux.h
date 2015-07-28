26b6a293be0f12fe5bfef6ce9996f93b01798138
Manolis Stamatogiannakis <manolis.stamatogiannakis@vu.nl>
2015-07-28 16:05:13 -0700

    OCD fix: all identation converted to tabs

--- panda/plugins/osi_linux/osi_linux.h
+++ panda/plugins/osi_linux/osi_linux.h
@@ -12,8 +12,8 @@
  *
  *
  * @author Manolis Stamatogiannakis <manolis.stamatogiannakis@vu.nl>
- * @copyright   This work is licensed under the terms of the GNU GPL, version 2.
- *              See the COPYING file in the top-level directory. 
+ * @copyright This work is licensed under the terms of the GNU GPL, version 2.
+ * See the COPYING file in the top-level directory.
  */
 #ifndef OSI_LINUX_H
 #define OSI_LINUX_H
@@ -26,7 +26,7 @@
  * @brief Pointer type of the guest VM.
  *
  * @note This definition implies that the guest VM pointer size matches the
- * size of unsigned long of the target processor. This is a reasonable 
+ * size of unsigned long of the target processor. This is a reasonable
  * assumption to make -- at least in the context of a research prototype.
  */
 #define PTR target_ulong
@@ -73,9 +73,9 @@
  * @brief Platform specific macro for retrieving the page directory address.
  */
 #if defined(TARGET_I386)
-#define _PGD    (env->cr[3])
+#define _PGD		(env->cr[3])
 #elif defined(TARGET_ARM)
-#define _PGD    (env->cp15.c2_base0 & env->cp15.c2_base_mask)
+#define _PGD		(env->cp15.c2_base0 & env->cp15.c2_base_mask)
 #else
 #error	"_PGD macro not defined for target architecture."
 #endif
@@ -90,7 +90,7 @@
 /* check for supervisor mode in the Current Program Status register */
 #define _IN_KERNEL ((env->uncached_cpsr & CPSR_M) == ARM_CPU_MODE_SVC)
 #else
-#error  "_IN_KERNEL macro not defined for target architecture."
+#error	"_IN_KERNEL macro not defined for target architecture."
 #endif
 
 #define LOG_ERR(fmt, args...) fprintf(stderr, "ERROR(%s:%s): " fmt "\n", __FILE__, __func__, ## args)
@@ -109,33 +109,33 @@ extern int panda_memory_errors;
  * @brief IMPLEMENT_OFFSET_GET is a macro for generating uniform
  * inlines for retrieving data based on a location+offset.
  */
-#define IMPLEMENT_OFFSET_GET(_name, _paramName, _retType, _offset, _errorRetValue)                        \
-static inline _retType _name(CPUState* env, PTR _paramName) {                                             \
-  _retType _t;                                                                                            \
-  if (-1 == panda_virtual_memory_rw(env, _paramName + _offset, (uint8_t *)&_t, sizeof(_retType), 0)) { \
-    panda_memory_errors++;                                                                                \
-    return (_errorRetValue);                                                                              \
-  }                                                                                                       \
-  return (_t);                                                                                            \
+#define IMPLEMENT_OFFSET_GET(_name, _paramName, _retType, _offset, _errorRetValue) \
+static inline _retType _name(CPUState* env, PTR _paramName) { \
+	_retType _t; \
+	if (-1 == panda_virtual_memory_rw(env, _paramName + _offset, (uint8_t *)&_t, sizeof(_retType), 0)) { \
+		panda_memory_errors++; \
+		return (_errorRetValue); \
+	} \
+	return (_t); \
 }
 
 /**
  * @brief IMPLEMENT_OFFSET_GET2L is a macro for generating uniform
  * inlines for retrieving data based on a *(location+offset1) + offset2.
  */
-#define IMPLEMENT_OFFSET_GET2L(_name, _paramName, _retType1, _offset1, _retType2, _offset2, _errorRetValue)   \
-static inline _retType2 _name(CPUState* env, PTR _paramName) {                                                \
-  _retType1 _t1;                                                                                              \
-  _retType2 _t2;                                                                                              \
-  if (-1 == panda_virtual_memory_rw(env, _paramName + _offset1, (uint8_t *)&_t1, sizeof(_retType1), 0)) {  \
-    panda_memory_errors++;                                                                                    \
-    return (_errorRetValue);                                                                                  \
-  }                                                                                                           \
-  if (-1 == panda_virtual_memory_rw(env, _t1 + _offset2, (uint8_t *)&_t2, sizeof(_retType2), 0)) {         \
-    panda_memory_errors++;                                                                                    \
-    return (_errorRetValue);                                                                                  \
-  }                                                                                                           \
-  return (_t2);                                                                                               \
+#define IMPLEMENT_OFFSET_GET2L(_name, _paramName, _retType1, _offset1, _retType2, _offset2, _errorRetValue) \
+static inline _retType2 _name(CPUState* env, PTR _paramName) { \
+	_retType1 _t1; \
+	_retType2 _t2; \
+	if (-1 == panda_virtual_memory_rw(env, _paramName + _offset1, (uint8_t *)&_t1, sizeof(_retType1), 0)) { \
+		panda_memory_errors++; \
+		return (_errorRetValue); \
+	} \
+	if (-1 == panda_virtual_memory_rw(env, _t1 + _offset2, (uint8_t *)&_t2, sizeof(_retType2), 0)) { \
+		panda_memory_errors++; \
+		return (_errorRetValue); \
+	} \
+	return (_t2); \
 }
 
 
@@ -259,7 +259,7 @@ IMPLEMENT_OFFSET_GET2L(get_vma_dentry, vma_struct, PTR, ki.vma.vm_file_offset, P
  * @brief Retrieves the vfsmount dentry associated with a vma_struct.
  *
  * XXX: Reading the vfsmount dentry is required to get the full pathname of files not located in the root fs.
- *			This hasn't been implemented yet...
+ * This hasn't been implemented yet...
  */
 IMPLEMENT_OFFSET_GET2L(get_vma_vfsmount_dentry, vma_struct, PTR, ki.vma.vm_file_offset, PTR, ki.fs.f_path_dentry_offset, 0)
 
@@ -270,7 +270,7 @@ IMPLEMENT_OFFSET_GET(get_files, task_struct, PTR, ki.task.files_offset, 0)
 
 /**
  * @brief Retrieves the array of file structs from the files struct.
- *				The n-th element of the array corresponds to the n-th open fd.
+ * The n-th element of the array corresponds to the n-th open fd.
  */
 IMPLEMENT_OFFSET_GET2L(get_files_fds, files_struct, PTR, ki.fs.fdt_offset, PTR, ki.fs.fd_offset, 0)
 
@@ -310,15 +310,15 @@ IMPLEMENT_OFFSET_GET(get_mnt_root_dentry, vfsmount_struct, PTR, ki.fs.mnt_root_o
 /**
  * @brief Size of the qstr kernel struct. Used to resolve dentry struct to names.
  *
- *  Because the struct is simple with no conditionally defined members,
- *  we choose to not use kernel offsets read from kernelinfo.conf to retrieve it.
+ *	Because the struct is simple with no conditionally defined members,
+ *	we choose to not use kernel offsets read from kernelinfo.conf to retrieve it.
  *
  * @code{.c}
- *  struct qstr {
- *    unsigned int hash;
- *    unsigned int len;
- *    const unsigned char *name;
- *  };
+ *	struct qstr {
+ *		unsigned int hash;
+ *		unsigned int len;
+ *		const unsigned char *name;
+ *	};
  * @endcode
  */
 #define _SIZEOF_QSTR (2*sizeof(target_uint) + sizeof(PTR))
@@ -354,82 +354,82 @@ static inline PTR get_fd_file(CPUState *env, PTR fd_file_array, int n) {
 static inline char *read_dentry_name(CPUState *env, PTR dentry) {
 	char *name = NULL;
 	PTR current_dentry;
-  uint8_t d_name[_SIZEOF_QSTR];
-  int err;
+	uint8_t d_name[_SIZEOF_QSTR];
+	int err;
 
-  // current path component
-  char *pcomp = NULL;
-  target_uint pcomp_length = 0;
-  unsigned int pcomp_capacity = 32;
+	// current path component
+	char *pcomp = NULL;
+	target_uint pcomp_length = 0;
+	unsigned int pcomp_capacity = 32;
 
-  // all path components read so far
-  char **pcomps = NULL;
-  unsigned int pcomps_idx = 0;
-  unsigned int pcomps_capacity = 16;
+	// all path components read so far
+	char **pcomps = NULL;
+	unsigned int pcomps_idx = 0;
+	unsigned int pcomps_capacity = 16;
 
-  // for reversing pcomps
-  char **pcomps_start, **pcomps_end;
+	// for reversing pcomps
+	char **pcomps_start, **pcomps_end;
 
-  pcomp = (char *)g_malloc(pcomp_capacity * sizeof(char));
-  pcomps = (char **)g_malloc(pcomps_capacity * sizeof(char *));
-  do {
+	pcomp = (char *)g_malloc(pcomp_capacity * sizeof(char));
+	pcomps = (char **)g_malloc(pcomps_capacity * sizeof(char *));
+	do {
 		current_dentry = dentry;
 
-    // read d_name qstr
+		// read d_name qstr
 		err = panda_virtual_memory_rw(env, current_dentry + ki.fs.d_name_offset, d_name, _SIZEOF_QSTR, 0);
-    if (-1 == err) goto error;
+		if (-1 == err) goto error;
 
-    // read component
-    pcomp_length = *(target_uint *)(d_name + sizeof(target_uint)) + 1;
-    if (pcomp_capacity < pcomp_length) {
-      pcomp_capacity = pcomp_length;
-      pcomp = (char *)g_realloc(pcomp, pcomp_capacity * sizeof(char));
-    }
-    err = panda_virtual_memory_rw(env, *(PTR *)(d_name + 2*sizeof(target_uint)), (uint8_t *)pcomp, pcomp_length*sizeof(char), 0);
-    if (-1 == err) goto error;
+		// read component
+		pcomp_length = *(target_uint *)(d_name + sizeof(target_uint)) + 1;
+		if (pcomp_capacity < pcomp_length) {
+			pcomp_capacity = pcomp_length;
+			pcomp = (char *)g_realloc(pcomp, pcomp_capacity * sizeof(char));
+		}
+		err = panda_virtual_memory_rw(env, *(PTR *)(d_name + 2*sizeof(target_uint)), (uint8_t *)pcomp, pcomp_length*sizeof(char), 0);
+		if (-1 == err) goto error;
 
-    // copy component
-    if (pcomps_idx == pcomps_capacity - 1) { // -1 leaves room for NULL termination
-      pcomps_capacity *= 2;
-      pcomps = (char **)g_realloc(pcomps, pcomps_capacity * sizeof(char *));
-    }
-    pcomps[pcomps_idx++] = g_strdup(pcomp);
+		// copy component
+		if (pcomps_idx == pcomps_capacity - 1) { // -1 leaves room for NULL termination
+			pcomps_capacity *= 2;
+			pcomps = (char **)g_realloc(pcomps, pcomps_capacity * sizeof(char *));
+		}
+		pcomps[pcomps_idx++] = g_strdup(pcomp);
 
-    // read the parent dentry
+		// read the parent dentry
 		err = panda_virtual_memory_rw(env, current_dentry + ki.fs.d_parent_offset, (uint8_t *)&dentry, sizeof(PTR), 0);
-    if (-1 == err) goto error;
+		if (-1 == err) goto error;
 	} while (dentry != current_dentry);
 
-  // reverse components order
-  g_free(pcomp);
-  pcomps_start = pcomps;
-  pcomps_end = &pcomps[pcomps_idx - 1];
-  while (pcomps_start < pcomps_end) {
-    pcomp = *pcomps_start;
-    *pcomps_start = *pcomps_end;
-    *pcomps_end = pcomp;
-    pcomps_start++;
-    pcomps_end--;
-  }
+	// reverse components order
+	g_free(pcomp);
+	pcomps_start = pcomps;
+	pcomps_end = &pcomps[pcomps_idx - 1];
+	while (pcomps_start < pcomps_end) {
+		pcomp = *pcomps_start;
+		*pcomps_start = *pcomps_end;
+		*pcomps_end = pcomp;
+		pcomps_start++;
+		pcomps_end--;
+	}
 
-  // join components and return
-  pcomps[pcomps_idx] = NULL;      // NULL terminate vector
+	// join components and return
+	pcomps[pcomps_idx] = NULL;			// NULL terminate vector
 	if (dentry == current_dentry) { // Eliminate root directory.
-    pcomps[0][0] = '\0';
-  }
-  name = g_strjoinv("/", pcomps);
-  g_strfreev(pcomps);
+		pcomps[0][0] = '\0';
+	}
+	name = g_strjoinv("/", pcomps);
+	g_strfreev(pcomps);
 
-  return name;
+	return name;
 
 error:
-  LOG_INFO("Error reading d_entry.");
-  panda_memory_errors++;
-  pcomps[pcomps_idx] = NULL;
-  g_free(pcomp);
-  g_strfreev(pcomps);
+	LOG_INFO("Error reading d_entry.");
+	panda_memory_errors++;
+	pcomps[pcomps_idx] = NULL;
+	g_free(pcomp);
+	g_strfreev(pcomps);
 
-  return NULL;
+	return NULL;
 }
 
 /**
@@ -495,25 +495,24 @@ static inline char *read_vfsmount_name(CPUState *env, PTR vfsmount) {
  * This means that we don't have to account for the terminating '\0'.
  */
 static inline char *get_name(CPUState *env, PTR task_struct, char *name) {
-  if (name == NULL) { name = (char *)g_malloc0(ki.task.comm_size * sizeof(char)); }
-  else { name = (char *)g_realloc(name, ki.task.comm_size * sizeof(char)); }
-  if (-1 == panda_virtual_memory_rw(env, task_struct + ki.task.comm_offset, (uint8_t *)name, ki.task.comm_size * sizeof(char), 0)) {
-    panda_memory_errors++;
-    strncpy(name, "N/A", ki.task.comm_size*sizeof(char));
-  }
-  return name;
+	if (name == NULL) { name = (char *)g_malloc0(ki.task.comm_size * sizeof(char)); }
+	else { name = (char *)g_realloc(name, ki.task.comm_size * sizeof(char)); }
+	if (-1 == panda_virtual_memory_rw(env, task_struct + ki.task.comm_offset, (uint8_t *)name, ki.task.comm_size * sizeof(char), 0)) {
+		panda_memory_errors++;
+		strncpy(name, "N/A", ki.task.comm_size*sizeof(char));
+	}
+	return name;
 }
 
 /**
  * @brief Retrieves the address of the following task_struct in the process list.
  */
 static inline PTR get_task_struct_next(CPUState *env, PTR task_struct) {
-  PTR tasks = get_tasks(env, task_struct);
+	PTR tasks = get_tasks(env, task_struct);
 
-  if (!tasks) return (PTR)NULL;
-  else return tasks-ki.task.tasks_offset;
+	if (!tasks) return (PTR)NULL;
+	else return tasks-ki.task.tasks_offset;
 }
 #endif
-
 
 /* vim:set tabstop=4 softtabstop=4 noexpandtab */
--- panda/plugins/osi_linux/utils/kernelinfo/kernelinfo.c
+++ panda/plugins/osi_linux/utils/kernelinfo/kernelinfo.c
@@ -3,8 +3,8 @@
  * @brief Retrieves offset information from the running Linux kernel and prints them in the kernel log.
  *
  * @author Manolis Stamatogiannakis <manolis.stamatogiannakis@vu.nl>
- * @copyright   This work is licensed under the terms of the GNU GPL, version 2.
- *              See the COPYING file in the top-level directory. 
+ * @copyright This work is licensed under the terms of the GNU GPL, version 2.
+ * See the COPYING file in the top-level directory.
  */
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
@@ -28,12 +28,12 @@
  *
  * E.g. for:
  * struct file {
- *  ...
- *  struct dentry {
- *      struct *vfsmount;
- *      struct *dentry;
- *  }
- *  ...
+ *	...
+ *	struct dentry {
+ *		struct *vfsmount;
+ *		struct *dentry;
+ *	}
+ *	...
  * };
  *
  * Caveat: Because a static buffer is returned, the function
@@ -41,13 +41,13 @@
  */
 #define MAX_MEMBER_NAME 31
 static char *cp_memb(const char *s) {
-    static char memb[MAX_MEMBER_NAME+1];
-    int i;
-    for (i = 0; i<MAX_MEMBER_NAME && s[i]!='\0'; i++) {
-        memb[i] = s[i] == '.' ? '_' : s[i];
-    }
-    memb[i] = 0;
-    return memb;
+	static char memb[MAX_MEMBER_NAME+1];
+	int i;
+	for (i = 0; i<MAX_MEMBER_NAME && s[i]!='\0'; i++) {
+		memb[i] = s[i] == '.' ? '_' : s[i];
+	}
+	memb[i] = 0;
+	return memb;
 }
 
 /*
@@ -57,105 +57,106 @@ static char *cp_memb(const char *s) {
 
 int init_module(void)
 {
-    struct cred credstruct;
-    struct vm_area_struct vmastruct;
-    struct dentry dentrystruct;
-    struct file filestruct;
-    struct thread_info threadinfostruct;
-    struct files_struct filesstruct; /* mind the extra 's' :-P */
-    struct fdtable fdtablestruct;
-    struct vfsmount vfsmountstruct;
+	struct cred credstruct;
+	struct vm_area_struct vmastruct;
+	struct dentry dentrystruct;
+	struct file filestruct;
+	struct thread_info threadinfostruct;
+	struct files_struct filesstruct; /* mind the extra 's' :-P */
+	struct fdtable fdtablestruct;
+	struct vfsmount vfsmountstruct;
 
-    struct task_struct *ts_p;
-    struct cred *cs_p;
-    struct mm_struct *mms_p;
-    struct vm_area_struct *vma_p;
-    struct dentry *ds_p;
-    struct file *fs_p;
-    struct fdtable *fdt_p;
-    struct thread_info *ti_p;
-    struct files_struct *fss_p;
-    struct vfsmount *vfsmnt_p;
+	struct task_struct *ts_p;
+	struct cred *cs_p;
+	struct mm_struct *mms_p;
+	struct vm_area_struct *vma_p;
+	struct dentry *ds_p;
+	struct file *fs_p;
+	struct fdtable *fdt_p;
+	struct thread_info *ti_p;
+	struct files_struct *fss_p;
+	struct vfsmount *vfsmnt_p;
 
-    ts_p = &init_task;
-    cs_p = &credstruct;
-    mms_p = init_task.mm;
-    vma_p = &vmastruct;
-    ds_p = &dentrystruct;
-    fs_p = &filestruct;
-    ti_p = &threadinfostruct;
-    fss_p = &filesstruct;
-    fdt_p = &fdtablestruct;
-    vfsmnt_p = &vfsmountstruct;
+	ts_p = &init_task;
+	cs_p = &credstruct;
+	mms_p = init_task.mm;
+	vma_p = &vmastruct;
+	ds_p = &dentrystruct;
+	fs_p = &filestruct;
+	ti_p = &threadinfostruct;
+	fss_p = &filesstruct;
+	fdt_p = &fdtablestruct;
+	vfsmnt_p = &vfsmountstruct;
 
-    printk(KERN_INFO "--KERNELINFO-BEGIN--\n");
-    printk(KERN_INFO "name = %s %s\n", utsname()->version, utsname()->machine);
+	printk(KERN_INFO "--KERNELINFO-BEGIN--\n");
+	printk(KERN_INFO "name = %s %s\n", utsname()->version, utsname()->machine);
 
-    printk(KERN_INFO "task.size = %zu\n", sizeof(init_task));
-    printk(KERN_INFO "#task.init_addr = 0x%08lX\n", (unsigned long)ts_p);
-    printk(KERN_INFO "task.init_addr = %lu\n", (unsigned long)ts_p);
+	printk(KERN_INFO "task.size = %zu\n", sizeof(init_task));
+	printk(KERN_INFO "#task.init_addr = 0x%08lX\n", (unsigned long)ts_p);
+	printk(KERN_INFO "task.init_addr = %lu\n", (unsigned long)ts_p);
 
-    PRINT_OFFSET(ti_p,  task,           "task");
-    PRINT_OFFSET(ts_p,  tasks,          "task");
-    PRINT_OFFSET(ts_p,  pid,            "task");
-    PRINT_OFFSET(ts_p,  tgid,           "task");
-    PRINT_OFFSET(ts_p,  group_leader,   "task");
-    PRINT_OFFSET(ts_p,  thread_group,   "task");
-    PRINT_OFFSET(ts_p,  real_parent,    "task");
-    PRINT_OFFSET(ts_p,  parent,         "task");
-    PRINT_OFFSET(ts_p,  mm,             "task");
-    PRINT_OFFSET(ts_p,  stack,          "task");
-    PRINT_OFFSET(ts_p,  real_cred,      "task");
-    PRINT_OFFSET(ts_p,  cred,           "task");
-    PRINT_OFFSET(ts_p,  comm,           "task");
-    printk(KERN_INFO "task.comm_size = %zu\n", sizeof(ts_p->comm));
-    PRINT_OFFSET(ts_p,  files,          "task");
+	PRINT_OFFSET(ti_p,	task,			"task");
+	PRINT_OFFSET(ts_p,	tasks,			"task");
+	PRINT_OFFSET(ts_p,	pid,			"task");
+	PRINT_OFFSET(ts_p,	tgid,			"task");
+	PRINT_OFFSET(ts_p,	group_leader,	"task");
+	PRINT_OFFSET(ts_p,	thread_group,	"task");
+	PRINT_OFFSET(ts_p,	real_parent,	"task");
+	PRINT_OFFSET(ts_p,	parent,			"task");
+	PRINT_OFFSET(ts_p,	mm,				"task");
+	PRINT_OFFSET(ts_p,	stack,			"task");
+	PRINT_OFFSET(ts_p,	real_cred,		"task");
+	PRINT_OFFSET(ts_p,	cred,			"task");
+	PRINT_OFFSET(ts_p,	comm,			"task");
+	printk(KERN_INFO "task.comm_size = %zu\n", sizeof(ts_p->comm));
+	PRINT_OFFSET(ts_p,	files,			"task");
 
-    PRINT_OFFSET(cs_p,  uid,            "cred");
-    PRINT_OFFSET(cs_p,  gid,            "cred");
-    PRINT_OFFSET(cs_p,  euid,           "cred");
-    PRINT_OFFSET(cs_p,  egid,           "cred");
+	PRINT_OFFSET(cs_p,	uid,			"cred");
+	PRINT_OFFSET(cs_p,	gid,			"cred");
+	PRINT_OFFSET(cs_p,	euid,			"cred");
+	PRINT_OFFSET(cs_p,	egid,			"cred");
 
-    PRINT_OFFSET(mms_p, mmap,           "mm");
-    PRINT_OFFSET(mms_p, pgd,            "mm");
-    PRINT_OFFSET(mms_p, arg_start,      "mm");
-    PRINT_OFFSET(mms_p, start_brk,      "mm");
-    PRINT_OFFSET(mms_p, brk,            "mm");
-    PRINT_OFFSET(mms_p, start_stack,    "mm");
+	PRINT_OFFSET(mms_p, mmap,			"mm");
+	PRINT_OFFSET(mms_p, pgd,			"mm");
+	PRINT_OFFSET(mms_p, arg_start,		"mm");
+	PRINT_OFFSET(mms_p, start_brk,		"mm");
+	PRINT_OFFSET(mms_p, brk,			"mm");
+	PRINT_OFFSET(mms_p, start_stack,	"mm");
 
-    PRINT_OFFSET(vma_p, vm_mm,          "vma");
-    PRINT_OFFSET(vma_p, vm_start,       "vma");
-    PRINT_OFFSET(vma_p, vm_end,         "vma");
-    PRINT_OFFSET(vma_p, vm_next,        "vma");
-    PRINT_OFFSET(vma_p, vm_flags,       "vma");
+	PRINT_OFFSET(vma_p, vm_mm,			"vma");
+	PRINT_OFFSET(vma_p, vm_start,		"vma");
+	PRINT_OFFSET(vma_p, vm_end,			"vma");
+	PRINT_OFFSET(vma_p, vm_next,		"vma");
+	PRINT_OFFSET(vma_p, vm_flags,		"vma");
 
-    /* used in reading OsiModules */
-    PRINT_OFFSET(vma_p, vm_file,        "vma");
+	/* used in reading OsiModules */
+	PRINT_OFFSET(vma_p, vm_file,		"vma");
 
-    PRINT_OFFSET(fs_p,      f_path.dentry,  "fs");
-    PRINT_OFFSET(fs_p,      f_path.mnt,     "fs");
-    PRINT_OFFSET(vfsmnt_p,  mnt_parent,     "fs");
-    PRINT_OFFSET(vfsmnt_p,  mnt_mountpoint, "fs");
-    PRINT_OFFSET(vfsmnt_p,  mnt_root,       "fs");  /* XXX: We don't use this anywhere. Marked for removal. */
+	PRINT_OFFSET(fs_p,		f_path.dentry,	"fs");
+	PRINT_OFFSET(fs_p,		f_path.mnt,		"fs");
+	PRINT_OFFSET(vfsmnt_p,	mnt_parent,		"fs");
+	PRINT_OFFSET(vfsmnt_p,	mnt_mountpoint, "fs");
+	PRINT_OFFSET(vfsmnt_p,	mnt_root,		"fs");	/* XXX: We don't use this anywhere. Marked for removal. */
 
-    /* used in reading FDs */
-    PRINT_OFFSET(fss_p,  fdt,           "fs");
-    PRINT_OFFSET(fss_p,  fdtab,         "fs");
-    PRINT_OFFSET(fdt_p,  fd,            "fs");
+	/* used in reading FDs */
+	PRINT_OFFSET(fss_p,	fdt,			"fs");
+	PRINT_OFFSET(fss_p,	fdtab,			"fs");
+	PRINT_OFFSET(fdt_p,	fd,				"fs");
 
-    PRINT_OFFSET(ds_p,  d_name,         "fs");
-    PRINT_OFFSET(ds_p,  d_iname,        "fs");
-    PRINT_OFFSET(ds_p,  d_parent,       "fs");
-    printk(KERN_INFO "---KERNELINFO-END---\n");
+	PRINT_OFFSET(ds_p,	d_name,			"fs");
+	PRINT_OFFSET(ds_p,	d_iname,		"fs");
+	PRINT_OFFSET(ds_p,	d_parent,		"fs");
+	printk(KERN_INFO "---KERNELINFO-END---\n");
 
-    /* Return a failure. We only want to print the info. */
-    return -1;
+	/* Return a failure. We only want to print the info. */
+	return -1;
 }
 
 void cleanup_module(void)
 {
-    printk(KERN_INFO "Information module removed.\n");
+	printk(KERN_INFO "Information module removed.\n");
 }
 
 MODULE_LICENSE("GPL");
 
+/* vim:set tabstop=4 softtabstop=4 noexpandtab */
