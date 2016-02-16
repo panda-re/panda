4b8f1ff032139a0bf7ac554b9161af22715edca5
Tim Leek <tleek@ll.mit.edu>
2016-02-16 14:34:55 -0500

    finished implementing chunked compressed pandalog.  not yet compiling

--- qemu/panda/pandalog.c
+++ qemu/panda/pandalog.c
@@ -15,18 +15,16 @@
 
 
 // directory mapping instructions to chunks in the chunksfile
-// say el[0].instr, el[1].instr = 0,1234
-// that means chunk 0 contains all pandalog info for instructions 0..1233
+// say el[0].instr = 1234
+// that means chunk 0 contains all pandalog info for instructions 0..1234
+// and say el[1].instr = 2468
+// that means chunk 1 contains all .. for instr 12345 .. 2468
 // additionally, the data for compressed chunk 0 is in the chunksfile
 // from pos[0] .. pos[1]-1
-// invariants:
-// el[i].start <= el[i].end
-// el[i].end < el[i+1].start (they are in order)
-// this means you can find the chunk for an instruction with binary search
 typedef struct pandalog_dir_struct {
-    uint32_t size;     // number of intervals
-    uint64_t *instr;   // array of instr
-    uint64_t *pos;     // array of file positions for start of chunk 
+    uint32_t max_size;   // max number of entries
+    uint64_t *instr;     // array of instruction counts.  instr[i] is last instruction in chunk i
+    uint64_t *pos;       // array of file positions for start of chunk 
 } PandalogDir;
 
 
@@ -48,28 +46,47 @@ unsigned char *chunk_buf = 0;
 unsigned char *chunk_buf_p = 0;
 
 
+// grow pandalog dir to be this new bigger size
+void grow_pandalog_dir(uint32_t new_size) {
+    assert (new_size > pandalog.dir.max_size);
+    pandalog.dir.max_size = new_size;
+    pandalog.dir.instr = (uint64_t *) realloc(pandalog.dir.instr, sizeof(uint64_t) * new_size);
+    pandalog.dir.pos = (uint64_t *) realloc(pandalog.dir.pos, sizeof(uint64_t) * new_size);
+}
+
+// add dir entry for this chunk
+// last_instr is last instr in this chunk
+void add_dir_entry(uint32_t chunk, uint64_t file_pos, uint64_t last_instr) {
+    if (chunk >= pandalog.dir.max_size) {
+        grow_pandalog_dir(pandalog.dir.size * 1.5);
+    }
+    assert (chunk <= pandalog.dir.size);
+    pandalog.dir.instr[chunk] = last_instr;
+    pandalog.dir.pos[chunk] = file_pos;
+}
 
 // compress current chunk and write it to chunksfile,
 // also updating directory 
 void write_current_chunk() {
-    // actual chunk size
+    // uncompressed chunk size
     uint32_t cs = chunk_buf_p - chunk_buf;
     if (cs == 0) return;
+    // compress pandalog chunk
     pandalog.strm.avail_in = cs;
     pandalog.strm.next_in = chunk_buf;
     int ret = deflate(&(pandalog.strm), Z_NO_FLUSH);
     assert(ret != Z_STREAM_ERROR); 
     assert(pandalog.strm.avail_in == 0);
+    // size of compressed chunk
     uint32_t css = pandalog.strm.avail_out;
     assert(css > 0);
     assert(cs >= css);
-    // size of compressed chunk
     printf ("writing chunk %d of pandalog %d / %d = %.2f compression\n",
             pandalog.chunk, cs, css, ((float) cs) / ((float) css)); 
     // position in chunksfile of start of this chunk
     uint64_t pos = ftell(pandalog.chunksfile);
     fwrite(chunk_buf, 1, css, pandalog.chunksfile);
-    pandalog.dir
+    add_dir_entry(pandalog.chunk, pos, rr_get_guest_instr_count ());
 }
 
 
@@ -77,13 +94,16 @@ void write_current_chunk() {
 void check_write(size_t n) {
     // initial alloc
     if (chunk_buf == 0) {
-        chunk_buf = (unsigned char *) malloc(pandalog.chunk_size);
+        // NB: malloc chunk a little big since we need to maintain
+        // the invariant that all log entries for an instruction reside in same
+        // chunk.  this should be big enough...
+        chunk_buf = (unsigned char *) malloc(pandalog.chunk_size * 1.25);
         chunk_buf_p = chunk_buf;
         assert(n < pandalog.chunk_size);
         return;
     }
-    if (chunk_buf_p - chunk_buf > n) {
-        // find to write -- it fits
+    if (chunk_buf_p + n < chunk_buf + pandalog.chunk_size) {
+        // fine to write -- it fits
         return;
     }
     // write won't fit in current chunk.  
@@ -101,15 +121,10 @@ void check_write(size_t n) {
 // 16 MB chunk
 #define PL_CHUNKSIZE (1024 * 1024 * 16)
 
-void __grow_pandalog_dir(uint32_t new_size) {
-    assert (new_size > pandalog.dir.size);
-    pandalog.dir.size = new_size;
-    pandalog.dir.instr = (uint64_t *) realloc(pandalog.dir.instr, sizeof(uint64_t) * pandalog.dir.size);
-    pandalog.dir.pos = (uint64_t *) realloc(pandalog.dir.pos, sizeof(uint64_t) * pandalog.dir.size);
-}
-
 
 // open pandalog for write
+// path is the chunks file
+// path + '.dir' is the directory
 void pandalog_open_write(const char *path) {
     pandalog.writep = 1;
     pandalog.filename = path;
@@ -144,7 +159,7 @@ int  pandalog_close(void) {
     if (pandalog.writep) {
         // finish current chunk & close chunksfile 
         write_current_chunk();
-        // write out directory 
+        // write directory 
         uint32_t n = strlen(pandalog.filename);
         char *dirfilename = (char*) malloc(strlen(pandalog.filename)+4);
         dirfilename[0] = '\0';
@@ -180,7 +195,8 @@ void pandalog_write_entry(Panda__LogEntry *entry) {
     }
     size_t n = panda__log_entry__get_packed_size(entry);   
     // possibly compress and write current chunk
-    // but dont 
+    // but dont do so if it would spread log entries for same instruction between chunks
+    // invariant: all log entries for an instruction belong in a single chunk
     if (instr_last_entry != entr->instr) {
         check_write(n+4);
     }
