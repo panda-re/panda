90a5041c861a87866767f9e4242e9c6cb3c88b03
ngillum <ngillum@users.noreply.github.com>
2016-06-14 09:25:43 -0700

    Make interrupt size correct, more formatting

--- rr_log.h
+++ rr_log.h
@@ -13,20 +13,18 @@
 // accessors
 uint64_t rr_get_pc(void);
 uint64_t rr_get_secondary(void);
-uint64_t rr_get_guest_instr_count (void);
+uint64_t rr_get_guest_instr_count(void);
 double rr_get_percentage(void);
 
 
-void rr_clear_rr_guest_instr_count(CPUState *cpu_state);
-
-//mz structure for arguments to cpu_physical_memory_rw()
+// mz structure for arguments to cpu_physical_memory_rw()
 typedef struct {
     hwaddr addr;
-    uint8_t *buf;
+    uint8_t* buf;
     uint32_t len;
 } RR_cpu_mem_rw_args;
 
-//mz structure for arguments to cpu_register_physical_memory()
+// mz structure for arguments to cpu_register_physical_memory()
 typedef struct {
     hwaddr start_addr;
     ram_addr_t size;
@@ -36,38 +34,54 @@ typedef struct {
 // structure for arguments to cpu_physical_memory_unmap
 typedef struct {
     hwaddr addr;
-    uint8_t *buf;
+    uint8_t* buf;
     hwaddr len;
 } RR_cpu_mem_unmap;
 
-void rr_record_cpu_mem_rw_call(RR_callsite_id call_site, hwaddr addr, uint8_t *buf, int len, int is_write);
-void rr_record_cpu_reg_io_mem_region(RR_callsite_id call_site, hwaddr start_addr, ram_addr_t size, ram_addr_t phys_offset);
-void rr_record_cpu_mem_unmap(RR_callsite_id call_site, hwaddr addr, uint8_t *buf, hwaddr len, int is_write);
+void rr_record_cpu_mem_rw_call(RR_callsite_id call_site, hwaddr addr,
+                               uint8_t* buf, int len, int is_write);
+void rr_record_cpu_reg_io_mem_region(RR_callsite_id call_site,
+                                     hwaddr start_addr, ram_addr_t size,
+                                     ram_addr_t phys_offset);
+void rr_record_cpu_mem_unmap(RR_callsite_id call_site, hwaddr addr,
+                             uint8_t* buf, hwaddr len, int is_write);
 
-static inline void rr_cpu_physical_memory_unmap_record(hwaddr addr, uint8_t *buf, hwaddr len, int is_write) {
-  rr_record_cpu_mem_unmap((RR_callsite_id) rr_skipped_callsite_location, addr, buf, len, is_write);
+static inline void rr_cpu_physical_memory_unmap_record(hwaddr addr,
+                                                       uint8_t* buf, hwaddr len,
+                                                       int is_write)
+{
+    rr_record_cpu_mem_unmap((RR_callsite_id)rr_skipped_callsite_location, addr,
+                            buf, len, is_write);
 }
 
-//mz XXX addr should be hwaddr
-static inline void rr_device_mem_rw_call_record(hwaddr addr, uint8_t *buf, int len, int is_write) {
-    rr_record_cpu_mem_rw_call((RR_callsite_id) rr_skipped_callsite_location, addr, buf, len, is_write);
+// mz XXX addr should be hwaddr
+static inline void rr_device_mem_rw_call_record(hwaddr addr, uint8_t* buf,
+                                                int len, int is_write)
+{
+    rr_record_cpu_mem_rw_call((RR_callsite_id)rr_skipped_callsite_location,
+                              addr, buf, len, is_write);
 }
 
-//mz XXX addr should be hwaddr
-static inline void rr_reg_mem_call_record(hwaddr start_addr, ram_addr_t size, ram_addr_t phys_offset) {
-    rr_record_cpu_reg_io_mem_region((RR_callsite_id) rr_skipped_callsite_location, start_addr, size, phys_offset);
+// mz XXX addr should be hwaddr
+static inline void rr_reg_mem_call_record(hwaddr start_addr, ram_addr_t size,
+                                          ram_addr_t phys_offset)
+{
+    rr_record_cpu_reg_io_mem_region(
+        (RR_callsite_id)rr_skipped_callsite_location, start_addr, size,
+        phys_offset);
 }
 
-//mz using uint8_t for kind and callsite_loc to control space - enums default to int.
-//mz NOTE: make sure RR_callsite_id has at most 255 members
-//mz NOTE: make sure RR_log_entry_kind has at most 255 members
+// mz using uint8_t for kind and callsite_loc to control space - enums default
+// to int.
+// mz NOTE: make sure RR_callsite_id has at most 255 members
+// mz NOTE: make sure RR_log_entry_kind has at most 255 members
 typedef struct {
     RR_prog_point prog_point;
     uint8_t kind;
-    uint8_t callsite_loc;  //mz This is used for another sanity check
+    uint8_t callsite_loc; // mz This is used for another sanity check
 } RR_header;
 
-//mz generic args
+// mz generic args
 typedef struct {
     uint8_t kind;
     union {
@@ -78,14 +92,14 @@ typedef struct {
         RR_net_transfer_args net_transfer_args;
         RR_handle_packet_args handle_packet_args;
     } variant;
-    //mz XXX HACK 
-  uint64_t old_buf_addr;
+    // mz XXX HACK
+    uint64_t old_buf_addr;
 } RR_skipped_call_args;
 
 // an item in a program-point indexed record/replay log
 typedef struct rr_log_entry_t {
     RR_header header;
-    //mz all possible options, depending on log_entry.kind
+    // mz all possible options, depending on log_entry.kind
     union {
         // if log_entry.kind == RR_INPUT_1
         uint8_t input_1;
@@ -96,7 +110,8 @@ typedef struct rr_log_entry_t {
         // if log_entry.kind == RR_INPUT_8
         uint64_t input_8;
         // if log_entry.kind == RR_INTERRUPT_REQUEST
-        uint16_t interrupt_request;         //mz 2-bytes is enough for the interrupt request value!
+        int32_t interrupt_request; // mz 2-bytes is enough for the interrupt
+                                    // request value!
         // if log_entry.kind == RR_EXIT_REQUEST
         uint16_t exit_request;
         // if log_entry.kind == RR_SKIPPED_CALL
@@ -104,27 +119,28 @@ typedef struct rr_log_entry_t {
         // if log_entry.kind == RR_LAST
         // no variant fields
     } variant;
-    struct rr_log_entry_t *next;
+    struct rr_log_entry_t* next;
 } RR_log_entry;
 
 // a program-point indexed record/replay log
-typedef enum {RECORD, REPLAY} RR_log_type;
+typedef enum { RECORD, REPLAY } RR_log_type;
 typedef struct RR_log_t {
-  //mz TODO this field seems redundant given existence of rr_mode
-  RR_log_type type;            // record or replay
-  RR_prog_point last_prog_point; // to report progress
+    // mz TODO this field seems redundant given existence of rr_mode
+    RR_log_type type;              // record or replay
+    RR_prog_point last_prog_point; // to report progress
 
-  char *name;                  // file name
-  FILE *fp;                    // file pointer for log
-  unsigned long long size;     // for a log being opened for read, this will be the size in bytes
-  uint64_t bytes_read;
+    char* name; // file name
+    FILE* fp;   // file pointer for log
+    unsigned long long
+        size; // for a log being opened for read, this will be the size in bytes
+    uint64_t bytes_read;
 
-  RR_log_entry current_item;
-  uint8_t current_item_valid;
-  uint64_t item_number;
+    RR_log_entry current_item;
+    uint8_t current_item_valid;
+    uint64_t item_number;
 } RR_log;
 
-RR_log_entry *rr_get_queue_head(void);
+RR_log_entry* rr_get_queue_head(void);
 
 void panda_end_replay(void);
 
