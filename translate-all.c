b2a5435bbf821f6841ebc2ab9d56bc7b50bfb3cb
Tim Leek <tleek@ll.mit.edu>
2016-06-16 13:29:40 -0400

    pulled from panda

--- translate-all.c
+++ translate-all.c
@@ -1362,9 +1362,10 @@ void tb_invalidate_phys_page_range(tb_page_addr_t start, tb_page_addr_t end,
                 that the modification is after the current PC, but it
                 would require a specialized function to partially
                 restore the CPU state */
-
                 current_tb_modified = 1;
                 cpu_restore_state_from_tb(cpu, current_tb, cpu->mem_io_pc);
+                 // Fix interrupt tracking bug with SMC.   
+                rr_num_instr_before_next_interrupt++;
                 cpu_get_tb_cpu_state(env, &current_pc, &current_cs_base,
                                      &current_flags);
             }
