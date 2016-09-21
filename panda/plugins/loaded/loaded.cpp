d94dccb5961254d111797d7c7fc15a8e724d9706
Patrick Hulin <patrick.hulin@gmail.com>
2016-09-21 16:05:32 -0400

    Fix up loaded for panda2 and enable.

--- panda/plugins/loaded/loaded.cpp
+++ panda/plugins/loaded/loaded.cpp
@@ -1,51 +1,40 @@
 /* PANDABEGINCOMMENT
- * 
+ *
  * Authors:
  *  Tim Leek               tleek@ll.mit.edu
  *  Ryan Whelan            rwhelan@ll.mit.edu
  *  Joshua Hodosh          josh.hodosh@ll.mit.edu
  *  Michael Zhivich        mzhivich@ll.mit.edu
  *  Brendan Dolan-Gavitt   brendandg@gatech.edu
- * 
- * This work is licensed under the terms of the GNU GPL, version 2. 
- * See the COPYING file in the top-level directory. 
- * 
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.
+ * See the COPYING file in the top-level directory.
+ *
 PANDAENDCOMMENT */
 // This needs to be defined before anything is included in order to get
 // the PRIx64 macro
 #define __STDC_FORMAT_MACROS
 
-#include "panda/addr.h"
-extern "C" {
+#include <cstdio>
+#include <map>
 
-//#include "config.h"
 #include "panda/rr/rr_log.h"
-#include "qemu-common.h"
-#include "cpu.h"
 #include "panda/plugin.h"
-#include "loaded.h"
+#include "panda/plugin_plugin.h"
 #include "panda/plog.h"
-#include "panda_common.h"
 
-#include "../osi/osi_types.h"
-#include "../osi/osi_ext.h"
+#include "loaded.h"
+
+extern "C" {
+#include "panda/addr.h"
+
+#include "osi/osi_types.h"
+#include "osi/osi_ext.h"
 
 // this provides the fd resolution magic
-#include "../osi_linux/osi_linux_ext.h"
+#include "osi_linux/osi_linux_ext.h"
 
-#include "../syscalls2/gen_syscalls_ext_typedefs.h"
-#include "panda/plugin_plugin.h"
-
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <libgen.h>
+#include "syscalls2/gen_syscalls_ext_typedefs.h"
 
 }
 
@@ -56,31 +45,30 @@ extern "C" {
 bool init_plugin(void *);
 void uninit_plugin(void *);
 
-// prototype for register-this-callabck
-PPP_PROT_REG_CB(on_library_load);
 }
 
+// prototype for register-this-callabck
+PPP_PROT_REG_CB(on_library_load);
 // This creates the global for this call back fn (on_library_load)
 PPP_CB_BOILERPLATE(on_library_load)
 
-#include <map>
 #define MAX_FILENAME 256
 std::map <target_ulong, OsiProc> running_procs;
 
 void die(const char* fmt, ...) {
     va_list args;
-    
+
     va_start(args, fmt);
     vfprintf(stderr, fmt, args);
     va_end(args);
 }
 
-uint32_t guest_strncpy(CPUState *env, char *buf, size_t maxlen, target_ulong guest_addr) {
+uint32_t guest_strncpy(CPUState *cpu, char *buf, size_t maxlen, target_ulong guest_addr) {
     buf[0] = 0;
     unsigned i;
     for (i=0; i<maxlen; i++) {
         uint8_t c;
-        panda_virtual_memory_rw(env, guest_addr+i, &c, 1, 0);
+        panda_virtual_memory_rw(cpu, guest_addr+i, &c, 1, 0);
         buf[i] = c;
         if (c==0) {
             break;
@@ -91,8 +79,9 @@ uint32_t guest_strncpy(CPUState *env, char *buf, size_t maxlen, target_ulong gue
 }
 
 #if defined(TARGET_I386)
-void linux_mmap_pgoff_return(CPUState *env,target_ulong pc,uint32_t addr,uint32_t len,uint32_t prot,uint32_t flags,uint32_t fd,uint32_t pgoff) {
-    target_ulong asid = panda_current_asid(env);
+void linux_mmap_pgoff_return(CPUState *cpu,target_ulong pc,uint32_t addr,uint32_t len,uint32_t prot,uint32_t flags,uint32_t fd,uint32_t pgoff) {
+    CPUArchState *env = (CPUArchState*)cpu->env_ptr;
+    target_ulong asid = panda_current_asid(cpu);
     if (running_procs.count(asid) == 0) {
         //printf ("linux_mmap_pgoff_enter for asid=0x%x fd=%d -- dont know about that asid.  discarding \n", (unsigned int) asid, (int) fd);
         return;
@@ -101,28 +90,28 @@ void linux_mmap_pgoff_return(CPUState *env,target_ulong pc,uint32_t addr,uint32_
         //printf ("linux_mmap_pgoff_enter for asid=0x%x fd=%d flags=%x -- not valid fd . . . \n", (unsigned int) asid, (int) fd, flags);
         return;
     }
-    OsiProc proc = running_procs[asid];        
-    char *filename = osi_linux_fd_to_filename(env, &proc, fd);
+    OsiProc proc = running_procs[asid];
+    char *filename = osi_linux_fd_to_filename(cpu, &proc, fd);
     // gets us offset into the file.  could be useful
     //uint64_t pos = osi_linux_fd_to_pos(env, &proc, fd);
     // if a filename exists and permission is executable
     // TODO: fix this magic constant of 0x04 for PROT_EXEC
     if (filename != NULL && ((prot & 0x04) == 0x04)) {
-        printf ("linux_mmap_pgoff(fd=%d filename=[%s] len=%d prot=%x flags=%x pgoff=%d)=" TARGET_FMT_lx "\n", (int) fd, filename, len, prot, flags, pgoff, EAX);        
-        //printf ("linux_mmap_pgoff(fd=%d filename=[%s] len=%d prot=%x flags=%x pgoff=%d)=%x\n", (int) fd, filename, len, prot, flags, pgoff, EAX);        
-        PPP_RUN_CB(on_library_load, env, pc, filename, EAX)
-
+        printf ("linux_mmap_pgoff(fd=%d filename=[%s] len=%d prot=%x "
+                "flags=%x pgoff=%d)=" TARGET_FMT_lx "\n",
+                (int) fd, filename, len, prot, flags, pgoff, env->regs[R_EAX]);
+        PPP_RUN_CB(on_library_load, cpu, pc, filename, env->regs[R_EAX])
     }
 }
 #endif
 
 // get current process before each bb execs
 // which will probably help us actually know the current process
-int osi_foo(CPUState *env, TranslationBlock *tb) {
+int osi_foo(CPUState *cpu, TranslationBlock *tb) {
 
-    if (panda_in_kernel(env)) {
+    if (panda_in_kernel(cpu)) {
 
-        OsiProc *p = get_current_process(env);      
+        OsiProc *p = get_current_process(cpu);
 
         //some sanity checks on what we think the current process is
         // this means we didnt find current task
@@ -141,13 +130,13 @@ int osi_foo(CPUState *env, TranslationBlock *tb) {
         // name doesnt consist of solely printable characters
         //        printf ("np=%d n=%d\n", np, n);
         if (np != n) return 0;
-        target_ulong asid = panda_current_asid(env);
+        target_ulong asid = panda_current_asid(cpu);
         if (running_procs.count(asid) == 0) {
             printf ("adding asid=0x%x to running procs.  cmd=[%s]  task=0x%x\n", (unsigned int)  asid, p->name, (unsigned int) p->offset);
         }
         running_procs[asid] = *p;
     }
-    
+
     return 0;
 }
 bool init_plugin(void *self) {
@@ -165,7 +154,7 @@ bool init_plugin(void *self) {
         pcb.before_block_exec = osi_foo;
         panda_register_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC, pcb);
     }
-    
+
     PPP_REG_CB("syscalls2", on_sys_mmap_pgoff_return, linux_mmap_pgoff_return);
 #else
     fprintf(stderr, "The loaded plugin is not currently supported on this platform.\n");
