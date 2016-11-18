95dedf7125b5192e4622001ea23de10ab14ed8d1
nisi <nisi@eurecom.fr>
2016-11-18 14:36:43 +0100

    IPC: fixes permission on message queues and semaphores creation

--- util/qemu-thread-posix.c
+++ util/qemu-thread-posix.c
@@ -513,7 +513,8 @@ void qemu_avatar_sem_open(QemuAvatarSemaphore *sem, const char *name)
 {
 #if defined(__APPLE__) || defined(__NetBSD__)
 #else
-    sem_t *rc = sem_open(name, O_CREAT, O_RDWR, 1);
+    sem_unlink(name);
+    sem_t *rc = sem_open(name, O_CREAT, S_IRUSR | S_IWUSR, 1);
 
     if(rc == SEM_FAILED) {
         error_exit(errno, __func__);
@@ -549,7 +550,7 @@ void qemu_avatar_mq_open_read(QemuAvatarMessageQueue *mq, const char *name)
 {
 #if defined(__APPLE__) || defined(__NetBSD__)
 #else
-    mqd_t m = mq_open(name, O_CREAT | O_RDONLY);
+    mqd_t m = mq_open(name, O_CREAT | O_RDONLY, 0666, NULL);
 
     if(m == -1)
     {
@@ -564,7 +565,7 @@ void qemu_avatar_mq_open_write(QemuAvatarMessageQueue *mq, const char *name)
 {
 #if defined(__APPLE__) || defined(__NetBSD__)
 #else
-    mqd_t m = mq_open(name, O_CREAT | O_WRONLY);
+    mqd_t m = mq_open(name, O_CREAT | O_WRONLY, 0666, NULL);
  
     if(m == -1)
     {
