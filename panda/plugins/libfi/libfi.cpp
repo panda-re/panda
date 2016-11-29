1cec4ced28f7c9effb521c88eedebda5aa65f105
Tim Leek <tleek@ll.mit.edu>
2016-11-29 16:12:03 -0500

    cleanup

--- panda/plugins/libfi/libfi.cpp
+++ panda/plugins/libfi/libfi.cpp
@@ -9,6 +9,7 @@
 #include "panda/plugin.h"
 #include "panda/plugin_plugin.h"
 
+
 extern "C" {
 #include "panda/rr/rr_log.h"
 #include "panda/plog.h"
@@ -28,8 +29,11 @@ void libfi_add_callback(char *libname, char *fnname, int isenter, uint32_t numar
 
 }
 
-using namespace std;
+#include "callstack_instr/callstack_instr.h"
+#include "callstack_instr/prog_point.h"
+#include "callstack_instr/callstack_instr_ext.h"
 
+using namespace std;
 
 #if defined(TARGET_I386) && !defined(TARGET_X86_64)
 
@@ -43,17 +47,19 @@ struct LibFICbEntry {
 
 std::vector<LibFICbEntry> libficbes;
 
+// ok this is pretty perfunctory
 enum CallingConv {CC_CDECL, CC_DUNNO};
 int calling_convention = CC_CDECL;
 
 #define ESP ((CPUX86State *)((CPUState *)env->env_ptr))->regs[R_ESP]
-
-
+#define EAX ((CPUArchState*)env->env_ptr)->regs[R_EAX]
 #define MAX_ARGS 16
 uint32_t word_size = 0;
 
-// program args
+// this will be used to hold onto program args
 uint8_t *arg=NULL;
+
+bool debug = false;
 
 static inline void set_word_size() {
     if (word_size == 0) {
@@ -69,67 +75,52 @@ static inline uint32_t get_word(CPUState *env, target_ulong addr) {
     return result;
 }
 
-
-
-
-
 static inline uint32_t get_stack(CPUState *env, int offset_number) {
     return get_word(env, ESP + word_size*offset_number);
 }
 
-
 void fn_start(CPUState *env, target_ulong pc, const char *file_name, 
               const char *funct_name) {
-    printf ("fn start %s pc=0x%x\n", funct_name, pc);
+    if (debug) printf ("fn start %s pc=0x%x\n", funct_name, pc);
     // grab args for this fn if this guy has either enter or exit cb
     for (LibFICbEntry &cbe : libficbes) {
         if (cbe.fnname == funct_name) {
             set_word_size();
             if (calling_convention == CC_CDECL ) {                
-                printf ("grabbing %d fn args\n", cbe.numargs);
+                if (debug) printf ("grabbing %d fn args\n", cbe.numargs);
                 for (uint32_t i=0; i<cbe.numargs; i++) {
                     if (word_size == 4) {
-                        ((uint32_t *) arg)[i] = get_stack(env, i+1);
+                        uint32_t a = get_stack(env, i+1);
+                        *((uint32_t *) (arg + i*word_size)) = a;
+                        if (debug) printf ("fn_start arg %d = 0x%x\n", i, a);
                     }
                     else {
                         assert (1==0);
                     }
                 }
-/*
-                printf ("libfi ESP = 0x%x\n", ESP);
-                for (int i=-8; i<=8; i++) {
-                    target_ulong va = ESP - i*4;
-                    uint32_t wrd = 0;
-                    int x = panda_virtual_memory_read(env, va, (uint8_t *) &wrd, 4);
-                    assert (x != -1);
-                    printf ("0x%x %04x ", va, wrd);
-                    va += 4;
-                }
-                printf ("\n");
-*/
-           }
+            }
             break;
         }
     }
     for (LibFICbEntry &cbe : libficbes) {
         if (cbe.isenter && cbe.fnname == funct_name) {            
+            if (debug) printf (" -- fn start callback\n");
             (*cbe.callback)(env, pc, (uint8_t *) arg);
         }
     }   
 }
 
-#define EAX ((CPUArchState*)env->env_ptr)->regs[R_EAX]
-//#define EAX ((CPUX86State *)((CPUState *)env->env_ptr))->regs[R_EAX]
-
 void fn_return(CPUState *env, target_ulong pc, const char *file_name, 
                const char *funct_name) {
-    printf ("fn end %s EAX=%x\n", funct_name, EAX);
+    if (debug) printf ("fn end %s EAX=%x\n", funct_name, EAX);
     for (LibFICbEntry &cbe : libficbes) {
         if (!cbe.isenter && cbe.fnname == funct_name) {
             // args populated by fn_start i hope
             for (uint32_t i=0; i<cbe.numargs; i++) {
-                printf ("arg %d = %x\n", i, arg[i]);
+                uint32_t a = *((uint32_t *) (arg + word_size*i));
+                if (debug) printf ("arg %d = %x\n", i, a);
             }
+            if (debug) printf (" -- fn exit callback\n");
             (*cbe.callback)(env, pc, (uint8_t *) arg);
         }           
     }   
@@ -139,15 +130,6 @@ void libfi_add_callback(char *libname, char *fnname, int isenter, uint32_t numar
     LibFICbEntry cbe = {string(libname), string(fnname), (isenter == 1), numargs, cb};
     libficbes.push_back(cbe);
     printf ("adding callback %s %s %d \n", libname, fnname, isenter);
-/*
-    LibFICbEntry *cbe = (LibFICbEntry *) malloc(sizeof(LibFICbEntry));
-    cbe->libname = string(libname);
-    cbe->fnname = string(fnname);
-    cbe->isenter = (isenter == 1);
-    cbe->numargs = numargs;
-    cbe->callback = cb;
-    libficbes.push_back(*cbe);    
-*/
 }
 #endif 
 
@@ -155,6 +137,7 @@ bool init_plugin(void *self) {
 #if defined(TARGET_I386) && !defined(TARGET_X86_64)
     printf ("Initializing plugin libfi\n");
     panda_require("pri");
+    assert(init_callstack_instr_api());
     PPP_REG_CB("pri", on_fn_start, fn_start);
     PPP_REG_CB("pri", on_fn_return, fn_return);
 #endif
@@ -162,5 +145,6 @@ bool init_plugin(void *self) {
 }
 
 void uninit_plugin(void *self) {
+    printf ("Uninitializing plugin libfi\n");
 }
 
