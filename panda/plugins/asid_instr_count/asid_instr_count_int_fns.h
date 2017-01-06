ac1e26b803ab6a190df9ac9782829b18f91c0593
Tim Leek <tleek@ll.mit.edu>
2017-01-06 16:22:26 -0500

    added api to per-asid instr count plugin

---
+++ panda/plugins/asid_instr_count/asid_instr_count_int_fns.h
@@ -0,0 +1,9 @@
+
+#ifndef __ASID_INSTR_COUNT_INT_FNS_H__
+#define __ASID_INSTR_COUNT_INT_FNS_H__
+
+Instr get_instr_count_current_asid(void);
+
+Instr get_instr_count_by_asid(target_ulong asid);
+
+#endif
