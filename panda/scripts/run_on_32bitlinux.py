9631e1c6788a573aafd6425d70597bc36fcb3ee8
Patrick Hulin <patrick.hulin@gmail.com>
2017-02-05 15:53:47 -0500

    Rewrite run_guest and run_on_32bl.

--- panda/scripts/run_on_32bitlinux.py
+++ panda/scripts/run_on_32bitlinux.py
@@ -2,7 +2,7 @@
 
 """run_on_32bitlinux.py binary [args]
 
-So you want to try panda but dont have any recordings.  Poor you.
+So you want to try panda but dont have any replays.  Poor you.
 This script allows you to run commands on a 32-bit linux guest.
 
 1st arg is binary which should be a 32-bit ELF.
@@ -36,13 +36,12 @@ dir.
 """
 
 import os
-import json
-import pipes
 import shutil
 import subprocess as sp
 import sys
 
 from os.path import basename, dirname, join
+from run_guest import create_recording
 
 home_dir = os.getenv("HOME")
 dot_dir = join(home_dir, '.panda')
@@ -57,77 +56,46 @@ panda_build_dir = os.getenv("PANDA_BUILD", default_build_dir)
 
 filemap = {}
 
-def filecopy(orig_filename):
+def transform_arg_copy(orig_filename):
     if orig_filename.startswith('guest:'):
         return orig_filename[6:]
-    else:
+    elif os.path.exists(orig_filename):
         name = basename(orig_filename)
         copy_filename = join(install_dir, name)
         shutil.copy(orig_filename, copy_filename)
         filemap[orig_filename] = copy_filename
         return copy_filename
+    else:
+        return orig_filename
 
 binary = sys.argv[1]
-args = []
-if (len(sys.argv) >= 2):
-    args = sys.argv[2:]
+if binary.startswith('guest:'): binary = binary[6:]
 
-# create installdir if necessary
+# Directory structure:
+# + replays
+# +---+ binary1
+#     +---- cdrom
+#     +---- cdrom.iso
+binary_dir = join(os.getcwd(), 'replays', binary)
+if not os.path.exists(binary_dir):
+    os.makedirs(binary_dir)
 
-rcp_dir = join(os.getcwd(), 'rcp-panda')
-if os.path.exists(rcp_dir):
-    shutil.rmtree(rcp_dir)
-os.mkdir(rcp_dir)
-
-install_dir = join(rcp_dir, 'install')
-if os.path.exists(install_dir):
-    shutil.rmtree(install_dir)
-os.mkdir(install_dir)
-
-# get qcow if necessary
+install_dir = join(binary_dir, 'cdrom')
+if not os.path.exists(install_dir):
+    os.mkdir(install_dir)
 
 qcow = join(dot_dir, "wheezy_panda2.qcow2")
-
-if not (os.path.isfile(qcow)):
+if not os.path.isfile(qcow):
     print "\nYou need a qcow. Downloading from moyix. Thanks moyix!\n"
     sp.check_call(["wget", "http://panda.moyix.net/~moyix/wheezy_panda2.qcow2", "-O", qcow])
 
-exename = basename(binary)
-binary_copy = filecopy(binary)
+new_args = map(transform_arg_copy, sys.argv[1:])
+exename = basename(new_args[0])
 
-new_args = []
-for arg in args:
-    if os.path.exists(arg) or arg.startswith('guest:'):
-        new_args.append(filecopy(arg))
-    else:
-        new_args.append(arg)
-
-print "args =", args
+print "args =", sys.argv[1:]
 print "new_args =", new_args
 
-proj = {
-    "qemu": join(panda_build_dir, 'i386-softmmu', 'qemu-system-i386'),
-    "qcow": qcow,
-    "snapshot": "root",
-    "install_dir": install_dir,
-    "directory": rcp_dir,
-    "name": exename,
-    "recording_name": join(rcp_dir, exename + "-recording"),
-    "command": pipes.quote(binary_copy) + " " + sp.list2cmdline(new_args),
-}
-
-jsonfile = join(rcp_dir, 'rc.json')
-f = open(jsonfile, "w")
-json.dump(proj, f)
-f.close()
-
-print "jsonfile: [{}]".format(jsonfile)
-
-rcog = join(this_script_dir, "run_guest.py")
-
-cmd = ['python', rcog, jsonfile]
-
-print "cmd = [{}]".format(sp.list2cmdline(cmd))
-print "filemap:", filemap
-
-sp.check_call(cmd)
+create_recording(
+    join(panda_build_dir, 'i386-softmmu', 'qemu-system-i386'),
+    qcow, "root", new_args, install_dir, join(binary_dir, binary)
+)
