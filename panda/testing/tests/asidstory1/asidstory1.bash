cbc0592f0567a2d0722999d9dd6bdf05a8a0d3d5
Tim Leek <tleek@ll.mit.edu>
2017-02-04 23:36:05 -0500

    stringsearch test

--- panda/testing/tests/asidstory1/asidstory1.bash
+++ panda/testing/tests/asidstory1/asidstory1.bash
@@ -15,11 +15,10 @@ tst=asidstory1
 # this is a fn defined in testing.defs
 set_outputs $tst
 
-# delete pandalog & asidstory because we want to make sure that this run creates them
+# delete output b/c we want to make sure this script creates it
 /bin/rm -f ./asidstory
 
 # run qemu with asidstory & pandalog
-echo replaydir $replaydir
 ${testingdir}/runqemu.bash i386 ${replaydir}/${tst}/netstat-recording -os linux-32-lava32 -panda asidstory
 
 # output is textual output of asidstory
