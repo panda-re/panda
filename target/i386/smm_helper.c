24a9b1fdaaab03276ec69f43bd53fb46c46af59c
Patrick Hulin <patrick.hulin@gmail.com>
2017-02-06 17:21:23 -0500

    Remove recording of smm update.

--- target/i386/smm_helper.c
+++ target/i386/smm_helper.c
@@ -71,11 +71,7 @@ void do_smm_enter(X86CPU *cpu)
         env->hflags2 |= HF2_NMI_MASK;
     }
 
-    RR_DO_RECORD_OR_REPLAY(
-    /*action=*/cpu_smm_update(cpu),
-    /*record=*/RR_NO_ACTION,
-    /*replay=*/RR_NO_ACTION,
-    /*location=*/RR_CALLSITE_DO_SMM_ENTER);
+    cpu_smm_update(cpu);
 
     sm_state = env->smbase + 0x8000;
 
@@ -340,11 +336,7 @@ void helper_rsm(CPUX86State *env)
     env->hflags2 &= ~HF2_SMM_INSIDE_NMI_MASK;
     env->hflags &= ~HF_SMM_MASK;
 
-    RR_DO_RECORD_OR_REPLAY(
-    /*action=*/cpu_smm_update(cpu),
-    /*record=*/RR_NO_ACTION,
-    /*replay=*/RR_NO_ACTION,
-    /*location=*/RR_CALLSITE_HELPER_RSM);
+    cpu_smm_update(cpu);
 
     qemu_log_mask(CPU_LOG_INT, "SMM: after RSM\n");
     log_cpu_state_mask(CPU_LOG_INT, CPU(cpu), CPU_DUMP_CCOP);
