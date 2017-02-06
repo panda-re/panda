f608e4da701d18256d0aaadaca5b3f418cedee28
Ryan Whelan <rwhelan@ll.mit.edu>
2017-02-06 18:09:29 -0500

    panda: guard code in main-loop.c so make check works.

--- main-loop.c
+++ main-loop.c
@@ -493,9 +493,13 @@ int main_loop_wait(int nonblocking)
     g_array_set_size(gpollfds, 0); /* reset for new iteration */
     /* XXX: separate device handlers from system ones */
 #ifdef CONFIG_SLIRP
+#ifdef CONFIG_SOFTMMU
     if (!rr_in_replay()) {
+#endif
         slirp_pollfds_fill(gpollfds, &timeout);
+#ifdef CONFIG_SOFTMMU
     }
+#endif
 #endif
 
     if (timeout == UINT32_MAX) {
@@ -508,10 +512,12 @@ int main_loop_wait(int nonblocking)
                                       timerlistgroup_deadline_ns(
                                           &main_loop_tlg));
 
+#ifdef CONFIG_SOFTMMU
     if (rr_in_record()) {
         rr_record_in_main_loop_wait = 1;
         rr_skipped_callsite_location = RR_CALLSITE_MAIN_LOOP_WAIT;
     }
+#endif
 
     ret = os_host_main_loop_wait(timeout_ns);
 
@@ -522,16 +528,22 @@ int main_loop_wait(int nonblocking)
     /* CPU thread can infinitely wait for event after
        missing the warp */
     // ru: add check if in in replay for running timers
+#ifdef CONFIG_SOFTMMU
     if (!rr_in_replay()) {
+#endif
         qemu_start_warp_timer();
         qemu_clock_run_all_timers();
+#ifdef CONFIG_SOFTMMU
     }
+#endif
 
+#ifdef CONFIG_SOFTMMU
     if (rr_in_record()) {
         rr_record_in_main_loop_wait = 0;
         // Check if DMA-mapped regions have changed
         rr_tracked_mem_regions_record();
     }
+#endif
 
     return ret;
 }
