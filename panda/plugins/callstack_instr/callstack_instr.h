dccd69d4779fcee7ffda824c2f8573349824ab5d
Tim Leek <tleek@ll.mit.edu>
2017-02-09 12:12:15 -0500

    fixing header

--- panda/plugins/callstack_instr/callstack_instr.h
+++ panda/plugins/callstack_instr/callstack_instr.h
@@ -1,6 +1,8 @@
 #ifndef __CALLSTACK_INSTR_H
 #define __CALLSTACK_INSTR_H
 
+#include "prog_point.h"
+
 typedef void (* on_call_t)(CPUState *env, target_ulong func);
 typedef void (* on_ret_t)(CPUState *env, target_ulong func);
 
