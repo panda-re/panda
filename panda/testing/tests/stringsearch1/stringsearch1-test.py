0b786346e7efccf299799912f98bfac51717d4d6
Tim Leek <tleek@ll.mit.edu>
2017-02-12 16:53:52 -0500

    three tests now work

---
+++ panda/testing/tests/stringsearch1/stringsearch1-test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import os
+import sys
+import shutil
+
+thisdir = os.path.dirname(os.path.realpath(__file__))
+td = os.path.realpath(thisdir + "/../..")
+sys.path.append(td)
+
+from ptest_utils import *
+
+ss_filename = miscdir + "/cat"
+run_test_32bitlinux("-panda stringsearch:name=" + ss_filename)
+
+os.chdir(tmpoutdir)
+shutil.move(miscdir + "/cat_string_matches.txt", tmpoutfile)
