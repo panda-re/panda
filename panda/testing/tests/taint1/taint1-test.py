9c5419835a16697ee285f2fbc57866dec7226160
Tim Leek <tleek@ll.mit.edu>
2017-02-13 13:34:54 -0500

    added another test for tstringsearch and tainted_instr but sadly it doesn't work since replay is so buggered at present

---
+++ panda/testing/tests/taint1/taint1-test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import os
+import sys
+import subprocess as sp
+
+thisdir = os.path.dirname(os.path.realpath(__file__))
+td = os.path.realpath(thisdir + "/../..")
+sys.path.append(td)
+
+from ptest_utils import *
+
+run_test_32bitlinux("-panda general:first_instr=1 " \
+                    + "-panda tstringsearch:only_first -panda stringsearch:name=" \
+                    + search_string_file_pfx \
+                    + " -panda tainted_instr:summary,num=2000")
+
+os.chdir(tmpoutdir)
+shutil.move("asidstory", tmpoutfile)
