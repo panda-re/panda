7ed42066db24ea27b9a35a002fcf8175ada897c3
Dario Nisi <nisi@eurecom.fr>
2017-03-02 14:15:05 +0100

    Avatar: Modifications to the IPC interface
    Extracted from 6e31210a3c13f4b6db7d7db5262cc47278e1f693

--- include/qemu/thread-posix.h
+++ include/qemu/thread-posix.h
@@ -54,9 +54,10 @@ void qemu_avatar_sem_wait(QemuAvatarSemaphore *sem);
 void qemu_avatar_sem_post(QemuAvatarSemaphore *sem);
 void qemu_avatar_sem_open(QemuAvatarSemaphore *sem, const char *name);
 
-void qemu_avatar_mq_open_read(QemuAvatarMessageQueue *mq, const char *name);
-void qemu_avatar_mq_open_write(QemuAvatarMessageQueue *mq, const char *name);
+void qemu_avatar_mq_open_read(QemuAvatarMessageQueue *mq, const char *name, size_t msg_size);
+void qemu_avatar_mq_open_write(QemuAvatarMessageQueue *mq, const char *name, size_t msg_size);
 void qemu_avatar_mq_send(QemuAvatarMessageQueue *mq, void *msg, size_t len);
 int qemu_avatar_mq_receive(QemuAvatarMessageQueue *mq, void *buffer, size_t len);
+int  qemu_avatar_mq_get_fd(QemuAvatarMessageQueue *mq);
 
 #endif
