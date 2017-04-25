cb48848d5a7f9075e299515f08a5f64dde848be6
mariusmue <marius.muench@eurecom.fr>
2017-04-25 20:11:53 +0200

    Append freshly registered callbacks to the panda_cbs[type] list instead of prepending them

--- panda/src/callbacks.c
+++ panda/src/callbacks.c
@@ -218,6 +218,7 @@ void * panda_get_plugin_by_name(const char *plugin_name) {
 }
 
 void panda_register_callback(void *plugin, panda_cb_type type, panda_cb cb) {
+    panda_cb_list *plist;
     panda_cb_list *new_list = g_new0(panda_cb_list,1);
     new_list->entry = cb;
     new_list->owner = plugin;
@@ -225,10 +226,13 @@ void panda_register_callback(void *plugin, panda_cb_type type, panda_cb cb) {
     new_list->next = NULL;
     new_list->enabled = true;
     if(panda_cbs[type] != NULL) {
-        new_list->next = panda_cbs[type];
-        panda_cbs[type]->prev = new_list;
+        for(plist = panda_cbs[type]; panda_cb_list_next(plist) != NULL; plist = panda_cb_list_next(plist));
+        plist->next = new_list;
+        new_list->prev = plist;
     }
-    panda_cbs[type] = new_list;
+    else {
+        panda_cbs[type] = new_list;
+    }
 }
 
 
