64e2c0b63a28be5b5fb1a136d2430d0af8ba7247
John Arbuckle <programmingkidx@gmail.com>
2017-06-28 15:37:16 -0400

    ui/cocoa.m: Fix compatibility issue with Mac OS 10.9 and under
    The [NSEvent modifierFlags] method returns an NSEventModifierFlags type value in Mac OS 10.10. It use to be of type NSUInteger. Replacing NSEventModifierFlags with NSUInteger allows for the cooca.m file to be compiled on older versions of Mac OS. This patch was been tested on Mac OS 10.6 and Mac OS 10.12 without problem.
    
    Signed-off-by: John Arbuckle <programmingkidx@gmail.com>
    Message-id: F6C36C1A-4661-48F4-BEA6-3994889927D0@gmail.com
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- ui/cocoa.m
+++ ui/cocoa.m
@@ -572,7 +572,7 @@ QemuCocoaView *cocoaView;
                 // bitmask.
 
                 if (qemu_console_is_graphic(NULL)) {
-                    NSEventModifierFlags modifiers = [event modifierFlags];
+                    NSUInteger modifiers = [event modifierFlags];
 
                     if (!!(modifiers & NSEventModifierFlagCapsLock) != !!modifiers_state[Q_KEY_CODE_CAPS_LOCK]) {
                         [self toggleStatefulModifier:Q_KEY_CODE_CAPS_LOCK];
