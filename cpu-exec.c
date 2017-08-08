bb4ab4be79b15ed0fcbb265d37c59f0c2d840e68
Ray Wang <raywang@mit.edu>
2017-08-08 18:27:59 -0400

    Moved powerpc exception record back to original location, fixes replay

--- cpu-exec.c
+++ cpu-exec.c
@@ -476,9 +476,6 @@ static inline void cpu_handle_debug_exception(CPUState *cpu)
 
 static inline bool cpu_handle_exception(CPUState *cpu, int *ret)
 {
-#ifdef TARGET_PPC
-    rr_exception_index_at(RR_CALLSITE_CPU_EXCEPTION_INDEX, &cpu->exception_index);
-#endif
     if (cpu->exception_index >= 0) {
         if (cpu->exception_index >= EXCP_INTERRUPT) {
             /* exit request from the cpu execution loop */
@@ -502,6 +499,9 @@ static inline bool cpu_handle_exception(CPUState *cpu, int *ret)
             return true;
 #else
             if (replay_exception()) {
+#ifdef TARGET_PPC
+                rr_exception_index_at(RR_CALLSITE_CPU_EXCEPTION_INDEX, &cpu->exception_index);
+#endif
                 CPUClass *cc = CPU_GET_CLASS(cpu);
                 cc->do_interrupt(cpu);
                 cpu->exception_index = -1;
