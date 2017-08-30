6a36ab0baa4eb79481c1881de24c5c7ba789b93d
Ray Wang <raywang@mit.edu>
2017-08-30 11:26:36 -0400

    Finally allowing plog_reader to compile with extra flag

--- panda/include/panda/plog-cc.hpp
+++ panda/include/panda/plog-cc.hpp
@@ -10,13 +10,11 @@
 #ifndef __PANDALOG_CC_H_
 #define __PANDALOG_CC_H_
 
-//#define PLOG_READER
-
 extern "C" {
-//#ifndef PLOG_READER
+#ifndef PLOG_READER
 #include "panda/rr/rr_log.h"
 #include "panda/common.h"
-//#endif
+#endif
 
 #include <zlib.h>
 #include "panda/plog.h"
