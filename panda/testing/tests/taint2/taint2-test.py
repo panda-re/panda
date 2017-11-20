3ef97fc17e2250765b223b50e04c29a1d6960b91
Tim Leek <tleek@ll.mit.edu>
2017-11-20 10:41:07 -0500

    better software

--- panda/testing/tests/taint2/taint2-test.py
+++ panda/testing/tests/taint2/taint2-test.py
@@ -9,24 +9,38 @@ thisdir = os.path.dirname(os.path.realpath(__file__))
 td = os.path.realpath(thisdir + "/../..")
 sys.path.append(td)
 
-
 from ptest_utils import *
+
+sys.path.append(pandascriptsdir)
+
+from plog_reader import plogiter
 
 # run taint_instr and tainted_branch on file_branch_taint program
 testdir = testingscriptsdir + "/tests/taint2"
 sp.check_call([pandascriptsdir + "/taint_debian.py", testdir + "/file_branch_taint", testdir + "/taint2.input"])
 
-out = sp.check_output(["%s/plog_reader.py" % pandascriptsdir, "taint.plog"])
+ulsm = {}
 
-#out2 = [x for x in out.split('\n') if (not ('ptr' in x))]
+with open("%s/taint2.out" % tmpoutdir, "w") as out:
 
-f = open("%s/taint2.out" % tmpoutdir, "w")
-f.write(out)
+    def print_taint_query(label,tqelist):
+        out.write(label + " ")
+        for tqe in tqelist:
+            out.write("( off=%d tcn=%d" % (tqe.offset, tqe.tcn))
+            if tqe.HasField("unique_label_set"):
+                uls = tqe.unique_label_set
+                ulsm[uls.ptr] = uls.label
+            ptr = tqe.ptr
+            for l in ulsm[ptr]:
+                out.write(" %d" % l)
+            out.write(")\n")
 
-#for line in out2:
-#    f.write(line + '\n')
 
-f.close()
+    for entry in plogiter("taint.plog"):
+        if entry.HasField("tainted_instr"):
+            print_taint_query("tainted_instr", entry.tainted_instr.taint_query)
+        if entry.HasField("tainted_branch"):
+            print_taint_query("tainted_branch", entry.tainted_branch.taint_query)
 
 os.chdir(tmpoutdir)
 shutil.move("taint2.out", tmpoutfile)
