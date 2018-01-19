6e8ada7ca270f4cbc00efc66e2cd54893289eca4
Tim Leek <tleek@ll.mit.edu>
2018-01-19 10:00:22 -0500

    some fixes

--- panda/testing/tests/scissors/scissors-test.py
+++ panda/testing/tests/scissors/scissors-test.py
@@ -5,22 +5,37 @@ import sys
 import subprocess as sp
 import random
 import struct
+import shutil
 
 thisdir = os.path.dirname(os.path.realpath(__file__))
 td = os.path.realpath(thisdir + "/../..")
 sys.path.append(td)
 
 from ptest_utils import *
-# This test doesn't write any output files, just checks that replay works for each snip
 
 # we try this many scissor snips for each of the replays we created in setup
 # note, each snip is random start / end
-num_tests = 30
+num_tests = 5
 
 num_pass = 0
 num_fail = 0
 
 binaries = ["netstat", "find"]
+
+try:
+    os.remove(tmpfulloutfile)
+except:
+    pass
+
+def progress_fullout(msg):
+    with open(tmpfulloutfile, "a") as out: 
+        out.write(msg+"\n")
+
+
+random.seed(0)
+seed = random.randint(0,1000000000)
+random.seed(seed)
+progress_fullout("random seed is %d" % seed)
 
 # get number of instructions in file 
 for binary in binaries:
@@ -29,9 +44,10 @@ for binary in binaries:
         num_instrs = struct.unpack("<Q", f.read()[:8])
         num_instrs = num_instrs[0]
 
-    random.seed()
+#    random.seed()
     for i in range(num_tests):
         progress ("binary %s test %d" % (binary, i))
+        progress_fullout ("\n\nbinary %s test %d" % (binary, i))
         start_pos = random.randint(0, num_instrs)
         end_pos = random.randint(start_pos, num_instrs)
 
@@ -43,11 +59,13 @@ for binary in binaries:
             run_test_debian("", "%s_reduced" % binary, "i386")
             msg = "Replay for %s (snipping %d to %d) succeeded" % (testname, start_pos, end_pos)
             progress(msg)
+            progress_fullout(msg)            
             num_pass += 1
         except Exception as e:
             msg = "Replay for %s (snipping %d to %d) FAILED" % (testname, start_pos, end_pos)
             error(msg)
-            error(e)
+            progress_fullout(msg)            
+            error(str(e))
             num_fail += 1
 
 
@@ -60,3 +78,6 @@ with open(tmpoutfile, "w") as f:
     else:
         f.write("Scissors FAIL\n")
 
+# also copy full results into tmpoutdir
+
+#shutil.copyfile(tmpfulloutfile, notsotmpfulloutfile)
