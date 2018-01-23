08127c29f88a4d4003d52dd6fd4229b9b57411f8
Manolis Stamatogiannakis <mstamat@gmail.com>
2018-01-23 23:05:53 +0100

    Made script executables. Made copy of the old parser.

--- panda/plugins/syscalls2/recreate_all_os_arch.sh
+++ panda/plugins/syscalls2/recreate_all_os_arch.sh
@@ -1,1 +1,3 @@
-python ./syscall_parser.py ./ linux arm linux x86 windows7 x86 windowsxp_sp2 x86 windowsxp_sp3 x86
+#!/bin/bash
+
+./syscall_parser_old.py ./ linux arm linux x86 windows7 x86 windowsxp_sp2 x86 windowsxp_sp3 x86
