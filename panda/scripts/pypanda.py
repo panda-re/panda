fed38df9e36fa9c3961a7cc945e83bfd10fff7e1
Wesley Van Pelt <wcvp@me.com>
2018-12-10 19:42:46 -0500

    Fixed tabs

--- panda/scripts/pypanda.py
+++ panda/scripts/pypanda.py
@@ -6,12 +6,12 @@ import os
 from ctypes import *
 from enum import Enum
 from colorama import Fore, Style
-from panda_datatypes import cb_types, panda_cb, PandaState
+from panda_datatypes import *
 debug = True
 
 
 def progress(msg):
-    print Fore.GREEN + '[pypanda.py] ' + Fore.RESET + Style.BRIGHT + msg + Style.RESET_ALL
+	print Fore.GREEN + '[pypanda.py] ' + Fore.RESET + Style.BRIGHT + msg + Style.RESET_ALL
 
 
 # location of panda build dir
@@ -20,10 +20,10 @@ home = os.getenv("HOME")
 
 
 def strarr2c(str_arr):
-    c_str_arr = (POINTER(c_char) * len(str_arr))()
-    for ix,el in enumerate(str_arr):
-        c_str_arr[ix] = create_string_buffer(el)
-    return c_str_arr
+	c_str_arr = (POINTER(c_char) * len(str_arr))()
+	for ix,el in enumerate(str_arr):
+		c_str_arr[ix] = create_string_buffer(el)
+	return c_str_arr
 
 
 # NOTE:
@@ -31,82 +31,82 @@ def strarr2c(str_arr):
 # Note that qcow for this os is assumed to exist and live in
 # ~/.panda/"%s-%s-%s.qcow" % (the_os, arch, mem)
 os2osstring = {
-    "debian:3.2.0-4-686-pae" : "linux-32-debian:3.2.0-4-686-pae"
+	"debian:3.2.0-4-686-pae" : "linux-32-debian:3.2.0-4-686-pae"
 }
-    
+
 
 class Panda:
 
-    """
-    arch should be "i386" or "x86_64" or ...
-    NB: wheezy is debian:3.2.0-4-686-pae
-    """    
-    def __init__(self, arch="i386", mem="128M", the_os="debian:3.2.0-4-686-pae", qcow="default", extra_args = []):
-        if debug:
-            progress("Initializing panda")
-        self.arch = arch
-        self.mem = mem
-        self.os = the_os
-        self.qcow = qcow
-        if qcow is None:
-            # this means we wont be using a qcow -- replay only presumably
-            pass
-        else:
-            if qcow is "default":
-                # this means we'll use arch / mem / os to find a qcow
-                self.qcow = pjoin(home, ".panda", "%s-%s-%s.qcow" % (the_os, arch, mem))
-            if not (os.path.exists(self.qcow)):
-                print "Missing qcow -- %s" % self.qcow
-                print "Please go create that qcow and give it to moyix!"
-        self.bindir = pjoin(panda_build, "%s-softmmu" % arch)
-        self.panda = pjoin(self.bindir, "qemu-system-%s" % arch)
-        self.libpanda = CDLL(pjoin(self.bindir, "libpanda-%s.so" % arch), mode=RTLD_GLOBAL)
-        biospath = realpath(pjoin(self.panda, "..", "..", "pc-bios"))
+	"""
+	arch should be "i386" or "x86_64" or ...
+	NB: wheezy is debian:3.2.0-4-686-pae
+	"""
+	def __init__(self, arch="i386", mem="128M", the_os="debian:3.2.0-4-686-pae", qcow="default", extra_args = []):
+		if debug:
+			progress("Initializing panda")
+		self.arch = arch
+		self.mem = mem
+		self.os = the_os
+		self.qcow = qcow
+		if qcow is None:
+			# this means we wont be using a qcow -- replay only presumably
+			pass
+		else:
+			if qcow is "default":
+				# this means we'll use arch / mem / os to find a qcow
+				self.qcow = pjoin(home, ".panda", "%s-%s-%s.qcow" % (the_os, arch, mem))
+			if not (os.path.exists(self.qcow)):
+				print "Missing qcow -- %s" % self.qcow
+				print "Please go create that qcow and give it to moyix!"
+		self.bindir = pjoin(panda_build, "%s-softmmu" % arch)
+		self.panda = pjoin(self.bindir, "qemu-system-%s" % arch)
+		self.libpanda = CDLL(pjoin(self.bindir, "libpanda-%s.so" % arch), mode=RTLD_GLOBAL)
+		biospath = realpath(pjoin(self.panda, "..", "..", "pc-bios"))
 
-        bits = None
-        if self.arch == "i386":
-            bits = 32
-        elif self.arch == "x86_64":
-            bits = 64
-        else:
-            print "For arch %s: I need logic to figure out num bits"
-        assert (not (bits == None))            
+		bits = None
+		if self.arch == "i386":
+			bits = 32
+		elif self.arch == "x86_64":
+			bits = 64
+		else:
+			print "For arch %s: I need logic to figure out num bits"
+		assert (not (bits == None))
 
-        # note: weird that we need panda as 1st arg to lib fn to init? 
-        self.panda_args = [self.panda, "-m", self.mem, "-display", "none", "-L", biospath, "-os", os2osstring[the_os], self.qcow]
-        cargs = strarr2c(self.panda_args)
-        # start up panda!
-        cenvp =  POINTER(c_int)()
+		# note: weird that we need panda as 1st arg to lib fn to init?
+		self.panda_args = [self.panda, "-m", self.mem, "-display", "none", "-L", biospath, "-os", os2osstring[the_os], self.qcow]
+		cargs = strarr2c(self.panda_args)
+		# start up panda!
+		cenvp =  POINTER(c_int)()
 
-        print("Panda args: [" + (" ".join(self.panda_args)) + "]")
-        self.libpanda.panda_init(len(cargs), cargs, cenvp)
+		print("Panda args: [" + (" ".join(self.panda_args)) + "]")
+		self.libpanda.panda_init(len(cargs), cargs, cenvp)
 
 
-    def load_plugin(self, name, args=[]):
-        if debug:
-            progress ("Loading plugin %s" % name),
-            print("plugin args: [" + (" ".join(args)) + "]")
-        n = len(args)
-        cargs = strarr2c(args)
-        self.libpanda.panda_init_plugin(create_string_buffer(name), cargs, n)
+	def load_plugin(self, name, args=[]):
+		if debug:
+			progress ("Loading plugin %s" % name),
+			print("plugin args: [" + (" ".join(args)) + "]")
+		n = len(args)
+		cargs = strarr2c(args)
+		self.libpanda.panda_init_plugin(create_string_buffer(name), cargs, n)
 
 	def load_python_plugin(self, name, plugin):
-		# plugin.init 
+		# plugin.init
 		# load_python_plugin -> lep -> init -> capi_init(pcb) -> init -> lep -> load_python_plugin -> python
 		self.libpanda.panda_load_external_plugin(plugin.filename, plugin.name, plugin.uuid, plugin.init)
-	
+
 	def register_callback(handle, enum_value, pcb):
 		self.libpanda.panda_register_callback(handle, enum_value, pcb)
 
-    def begin_replay(self, replaypfx):
-        if debug:
-            progress ("Replaying %s" % replaypfx)
-        self.libpanda.panda_replay(create_string_buffer(replaypfx))
+	def begin_replay(self, replaypfx):
+		if debug:
+			progress ("Replaying %s" % replaypfx)
+		self.libpanda.panda_replay(create_string_buffer(replaypfx))
 
-    def run(self):
-        if debug:
-            progress ("Running")
-        self.libpanda.panda_run()
+	def run(self):
+		if debug:
+			progress ("Running")
+		self.libpanda.panda_run()
 
 
 class Plugin:
@@ -120,11 +120,11 @@ class CoolPlugin(Plugin):
 		super(name="CoolPlugin",uuid=43).__init(t
 	def init(self, handle):
 		progress("init in python")
-		#	panda_cb pcb = { .before_block_exec = self.before_block_execute };
+		# panda_cb pcb = { .before_block_exec = self.before_block_execute };
 		pcb = panda_cb()
 		pcb.before_block_execute = self.before_block_execute
 		panda_register_callback(handle, cb_types.PANDA_BEFORE_BLOCK_EXEC, pcb)
-		
+
 	def before_block_execute(self, a,b):
 		progress("before block in python")
 
@@ -137,5 +137,3 @@ panda = Panda(qcow="/home/luke/ubuntu-14.04-server-cloudimg-i386-disk1.img")
 cool_plugin = CoolPlugin()
 panda.load_python_plugin(cool_plugin)
 panda.run()
-
-
