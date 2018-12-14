42e36a957827f4dd055d638760fc9e48a07b4986
Luke Craig <lacraig3@gmail.com>
2018-12-13 23:53:27 -0500

    new folder and some other updates

---
+++ panda/scripts/pypanda/panda_qemu_support.h
@@ -0,0 +1,222 @@
+/**
+ * ObjectClass:
+ *
+ * The base for all classes.  The only thing that #ObjectClass contains is an
+ * integer type handle.
+ */
+struct ObjectClass
+{
+    /*< private >*/
+    Type type;
+    GSList *interfaces;
+
+    const char *object_cast_cache[OBJECT_CLASS_CAST_CACHE];
+    const char *class_cast_cache[OBJECT_CLASS_CAST_CACHE];
+
+    ObjectUnparent *unparent;
+
+    GHashTable *properties;
+};
+
+/**
+ * Object:
+ *
+ * The base for all objects.  The first member of this object is a pointer to
+ * a #ObjectClass.  Since C guarantees that the first member of a structure
+ * always begins at byte 0 of that structure, as long as any sub-object places
+ * its parent as the first member, we can cast directly to a #Object.
+ *
+ * As a result, #Object contains a reference to the objects type as its
+ * first member.  This allows identification of the real type of the object at
+ * run time.
+ */
+struct Object
+{
+    /*< private >*/
+    ObjectClass *klass;
+    ObjectFree *free;
+    GHashTable *properties;
+    uint32_t ref;
+    Object *parent;
+};
+
+
+/**
+ * DeviceState:
+ * @realized: Indicates whether the device has been fully constructed.
+ *
+ * This structure should not be accessed directly.  We declare it here
+ * so that it can be embedded in individual device state structures.
+ */
+struct DeviceState {
+    /*< private >*/
+    Object parent_obj;
+    /*< public >*/
+    
+    const char *id;
+    bool realized;
+    bool pending_deleted_event;
+    QemuOpts *opts;
+    int hotplugged;
+    BusState *parent_bus;
+    QLIST_HEAD(, NamedGPIOList) gpios;
+    QLIST_HEAD(, BusState) child_bus;
+    int num_child_bus;
+    int instance_id_alias;
+    int alias_required_for_version;
+};
+
+
+/**
+ * CPUState:
+ * @cpu_index: CPU index (informative).
+ * @nr_cores: Number of cores within this CPU package.
+ * @nr_threads: Number of threads within this CPU.
+ * @numa_node: NUMA node this CPU is belonging to.
+ * @host_tid: Host thread ID.
+ * @running: #true if CPU is currently running (lockless).
+ * @has_waiter: #true if a CPU is currently waiting for the cpu_exec_end;
+ * valid under cpu_list_lock.
+ * @created: Indicates whether the CPU thread has been successfully created.
+ * @interrupt_request: Indicates a pending interrupt request.
+ * @halted: Nonzero if the CPU is in suspended state.
+ * @stop: Indicates a pending stop request.
+ * @stopped: Indicates the CPU has been artificially stopped.
+ * @unplug: Indicates a pending CPU unplug request.
+ * @crash_occurred: Indicates the OS reported a crash (panic) for this CPU
+ * @tcg_exit_req: Set to force TCG to stop executing linked TBs for this
+ *           CPU and return to its top level loop.
+ * @singlestep_enabled: Flags for single-stepping.
+ * @icount_extra: Instructions until next timer event.
+ * @icount_decr: Number of cycles left, with interrupt flag in high bit.
+ * This allows a single read-compare-cbranch-write sequence to test
+ * for both decrementer underflow and exceptions.
+ * @can_do_io: Nonzero if memory-mapped IO is safe. Deterministic execution
+ * requires that IO only be performed on the last instruction of a TB
+ * so that interrupts take effect immediately.
+ * @cpu_ases: Pointer to array of CPUAddressSpaces (which define the
+ *            AddressSpaces this CPU has)
+ * @num_ases: number of CPUAddressSpaces in @cpu_ases
+ * @as: Pointer to the first AddressSpace, for the convenience of targets which
+ *      only have a single AddressSpace
+ * @env_ptr: Pointer to subclass-specific CPUArchState field.
+ * @gdb_regs: Additional GDB registers.
+ * @gdb_num_regs: Number of total registers accessible to GDB.
+ * @gdb_num_g_regs: Number of registers in GDB 'g' packets.
+ * @next_cpu: Next CPU sharing TB cache.
+ * @opaque: User data.
+ * @mem_io_pc: Host Program Counter at which the memory was accessed.
+ * @mem_io_vaddr: Target virtual address at which the memory was accessed.
+ * @kvm_fd: vCPU file descriptor for KVM.
+ * @work_mutex: Lock to prevent multiple access to queued_work_*.
+ * @queued_work_first: First asynchronous work pending.
+ * @trace_dstate: Dynamic tracing state of events for this vCPU (bitmask).
+ *
+ * State of one CPU core or thread.
+ */
+	struct CPUState {
+	    /*< private >*/
+	    DeviceState parent_obj;
+	    /*< public >*/
+
+	    int nr_cores;
+	    int nr_threads;
+	    int numa_node;
+
+	    struct QemuThread *thread;
+	#ifdef _WIN32
+	    HANDLE hThread;
+	#endif
+	    int thread_id;
+	    uint32_t host_tid;
+	    bool running, has_waiter;
+	    struct QemuCond *halt_cond;
+	    bool thread_kicked;
+	    bool created;
+	    bool stop;
+	    bool stopped;
+	    bool unplug;
+	    bool crash_occurred;
+	    bool exit_request;
+	    uint32_t interrupt_request;
+	    int singlestep_enabled;
+	    int64_t icount_extra;
+	    sigjmp_buf jmp_env;
+
+	    QemuMutex work_mutex;
+	    struct qemu_work_item *queued_work_first, *queued_work_last;
+
+	    CPUAddressSpace *cpu_ases;
+	    int num_ases;
+	    AddressSpace *as;
+	    MemoryRegion *memory;
+
+	    void *env_ptr; /* CPUArchState */
+
+	    /* Writes protected by tb_lock, reads not thread-safe  */
+	    struct TranslationBlock *tb_jmp_cache[TB_JMP_CACHE_SIZE];
+
+	    struct GDBRegisterState *gdb_regs;
+	    int gdb_num_regs;
+	    int gdb_num_g_regs;
+	    QTAILQ_ENTRY(CPUState) node;
+
+	    /* ice debug support */
+	    QTAILQ_HEAD(breakpoints_head, CPUBreakpoint) breakpoints;
+
+	    QTAILQ_HEAD(watchpoints_head, CPUWatchpoint) watchpoints;
+	    CPUWatchpoint *watchpoint_hit;
+	    bool watchpoints_disabled;
+
+	    void *opaque;
+
+	    /* In order to avoid passing too many arguments to the MMIO helpers,
+	     * we store some rarely used information in the CPU context.
+	     */
+	    uintptr_t mem_io_pc;
+	    vaddr mem_io_vaddr;
+
+	    int kvm_fd;
+	    bool kvm_vcpu_dirty;
+	    struct KVMState *kvm_state;
+	    struct kvm_run *kvm_run;
+
+	    /*
+	     * Used for events with 'vcpu' and *without* the 'disabled' properties.
+	     * Dynamically allocated based on bitmap requried to hold up to
+	     * trace_get_vcpu_event_count() entries.
+	     */
+	    unsigned long *trace_dstate;
+
+	    /* TODO Move common fields from CPUArchState here. */
+	    int cpu_index; /* used by alpha TCG */
+	    uint32_t halted; /* used by alpha, cris, ppc TCG */
+	    union {
+		uint32_t u32;
+		icount_decr_u16 u16;
+	    } icount_decr;
+	    uint32_t can_do_io;
+	    int32_t exception_index; /* used by m68k TCG */
+	    uint64_t rr_guest_instr_count;
+	    uint64_t panda_guest_pc;
+
+	    // Used for rr reverse debugging
+	    uint8_t reverse_flags;
+	    uint64_t last_gdb_instr; // Instruction count from which we last sent a GDB command
+	    uint64_t last_bp_hit_instr; // Last bp observed during this checkpoint run
+	    uint64_t temp_rr_bp_instr; // Saved bp. Used by rstep/rcont, which disables bp to move forward, then restores on next tb in cpu-exec.c
+
+	    /* Used to keep track of an outstanding cpu throttle thread for migration
+	     * autoconverge
+	     */
+	    bool throttle_thread_scheduled;
+
+	    /* Note that this is accessed at the start of every TB via a negative
+	       offset from AREG0.  Leave this field at the end so as to make the
+	       (absolute value) offset as small as possible.  This reduces code
+	       size, especially for hosts without large memory offsets.  */
+	    uint32_t tcg_exit_req;
+
+	    bool hax_vcpu_dirty;
+	    struct hax_vcpu_state *hax_vcpu;
+	};
