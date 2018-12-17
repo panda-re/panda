b4b68215d008e0f1245bd62077bc6da86ba9b2fa
Luke Craig <lacraig3@gmail.com>
2018-12-16 20:03:43 -0500

    updated example plugin and example c plugin comparison

--- panda/scripts/pypanda/example_print_regs.py
+++ panda/scripts/pypanda/example_print_regs.py
@@ -1,35 +1,22 @@
 from pypanda import *
 from time import sleep
 
-EAX = 0
-EBX = 3
-ECX = 1
-EDX = 2
-ESP = 4
-EBP = 5
-ESI = 6
-EDI = 7
-NUM = 0
 @pyp.callback("bool(void*)")
 def init(handle):
-	progress("init in python. handle="+str(handle))
 	panda.register_callback(handle, "before_block_exec", 3, before_block_execute)
 	return True
 
 @pyp.callback("int(CPUState*, TranslationBlock*)")
 def before_block_execute(cpustate,transblock):
-	if panda.static_var >= 10000:
-		regs = cpustate.env_ptr.regs
-		fregs = ["{:08x}".format(i) for i in regs]
+	if panda.static_var == 10000:
+		fregs = ["{:08x}".format(i) for i in cpustate.env_ptr.regs]
 		fregs.append("{:08x}".format(cpustate.env_ptr.eip))
 		fregs.append("{:08x}".format(cpustate.env_ptr.eflags))
-		progress("EAX: %s EBX: %s ECX: %s EDX: %s ESP: %s EBP: %s ESI: %s EDI: %s EIP: %s EFLAGS: %s" %(fregs[EAX], fregs[EBX], fregs[ECX], fregs[EDX], fregs[ESP], fregs[EBP], fregs[ESI], fregs[EDI], fregs[8], fregs[9]))
-		panda.static_var = 0
-		sleep(sleeptime)
-	panda.static_var += 1 
+		progress("EAX: %s EBX: %s ECX: %s EDX: %s ESP: %s EBP: %s ESI: %s EDI: %s EIP: %s EFLAGS: %s" %(fregs[0], fregs[3], fregs[1], fregs[2], fregs[4], fregs[5], fregs[6], fregs[7], fregs[8], fregs[9]))
+		sleep(0.4)
+	panda.static_var = (panda.static_var+1) % (10001)
 	return 0
 
-sleeptime = 0.4
 panda = Panda(qcow="/home/luke/ubuntu-14.04-server-cloudimg-i386-disk1.img")
-panda.load_python_plugin(init,"Cool Plugin")
+panda.load_python_plugin(init,"register_printer")
 panda.run()
