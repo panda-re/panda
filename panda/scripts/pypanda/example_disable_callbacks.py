c1af3faf8cbc5e15d965b3251275cd73e5882eac
Luke Craig <lacraig3@gmail.com>
2019-04-30 19:13:03 -0400

    disable_callbacks examples

--- panda/scripts/pypanda/example_disable_callbacks.py
+++ panda/scripts/pypanda/example_disable_callbacks.py
@@ -3,12 +3,9 @@ from time import sleep
 from sys import argv
 
 panda = Panda(qcow=argv[1])
-outside_handle = 0
 
 @panda.callback.init
 def init(handle):
-	global outside_handle
-	outside_handle = handle
 	progress("init in python. handle="+str(handle))
 	panda.register_callback(handle, panda.callback.before_block_exec, before_block_execute)
 	panda.register_callback(handle, panda.callback.after_block_exec, after_block_execute)
@@ -22,11 +19,9 @@ def before_block_execute(cpustate,transblock):
 	if count < 9:
 		progress("before block in python %d" %count)
 	if count == 2:
-		global outside_handle
-		panda.disable_callback(outside_handle, panda.callback.after_block_exec)
+		panda.disable_callback(panda.callback.after_block_exec)
 	if count == 4:
-		global outside_handle
-		panda.enable_callback(outside_handle, panda.callback.after_block_exec)
+		panda.enable_callback(panda.callback.after_block_exec)
 	count+=1
 	return 0
 
