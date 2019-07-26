16a1cf963a3518ba5bc009b465eeb35b7fea2ca1
luke <Luke Craig>
2019-07-26 16:38:38 -0400

    starting work on multiarch support. ARM seems to work pretty well

---
+++ panda/scripts/pypanda/experimental/example_plugin_arm.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''
+example_plugin.py
+
+This is the simplest of plugins. It registers a callback for `before_block_exec`
+and gives the user a pdb trace each time it is hit.
+
+Run this with python3 example_plugin.py
+
+'''
+from pypanda import *
+from time import sleep
+from sys import argv
+
+
+# Single arg of arch, defaults to i386
+arch = "arm" if len(argv) <= 1 else argv[1]
+panda = Panda(generic=arch,arch=arch)
+
+@panda.callback.init
+def init(handle):
+	progress("init in python. handle="+str(handle))
+	panda.register_callback(handle, panda.callback.before_block_exec, \
+	 						before_block_execute)
+	return True
+
+@panda.callback.before_block_exec
+def before_block_execute(cpustate,transblock):
+	arm_cpustate = panda.get_cpu(cpustate)
+	progress("PC: "+str(arm_cpustate.pc))
+	return 0
+
+panda.load_python_plugin(init,"example_plugin")
+panda.run()
