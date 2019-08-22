62ff71b0b1d32f4b1c4a2ffe5e893b9a0016c67f
Andrew Fasano <fasano@mit.edu>
2019-08-21 22:08:33 -0400

    Fix broken commit of hooks. Change some panda_datatypes logic to handle simple externs

--- panda/plugins/hooks/hooks.h
+++ panda/plugins/hooks/hooks.h
@@ -1,4 +1,6 @@
 // Hook functions must be of this type
 typedef bool (*hook_func_t)(CPUState *, TranslationBlock *);
 
-void add_hook(target_ulong addr, hook_func_t hook);
+// XXX the extern is a single line (no curly braces) so it can be programatically removed
+// when we use CFFI- see create_panda_datatypes.py for the replacement logic
+extern "C" void add_hook(target_ulong addr, hook_func_t hook);
