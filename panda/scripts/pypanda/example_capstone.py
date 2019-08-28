2d849eb03faecb1a3398213f0eb43f9e3b06b127
Andrew Fasano <fasano@mit.edu>
2019-08-28 09:12:41 -0400

    Update example capstone

--- panda/scripts/pypanda/example_capstone.py
+++ panda/scripts/pypanda/example_capstone.py
@@ -8,48 +8,44 @@ import capstone
 # Single arg of arch, defaults to i386
 arch = "i386" if len(argv) <= 1 else argv[1]
 panda = Panda(generic=arch)
-
 md = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_64)
 insn_cache = {} # address -> disassembly string
+executed_pcs = [] # List of addresses we executed
 
 # Run a command in the guest
 @blocking
 def my_runcmd():
     panda.revert_sync('root')
     panda.run_serial_cmd("find . /proc/self")
+    panda.run_monitor_cmd("quit") # XXX: need a better way to return control to main thread
 
-    # By quitting here main thread can continue executing after panda.run
-    # XXX: Need a better way to transfer control back to main thread - maybe via main_loop_wait callbacks?
-    panda.run_monitor_cmd("quit")
+def generate_insns(env, tb):
+    # Disassemble each basic block and store in insn_cache
+    if tb.pc in insn_cache: return
 
-@panda.cb_after_block_translate(name="before")
-def before_block_trans(env, tb):
-    '''
-    Disassemble each basic block as we translate. Store in insn_cache
-    '''
     code_buf = ffi.new("char[]", tb.size)
     panda.virtual_memory_read(env, tb.pc, code_buf, tb.size)
     code = ffi.unpack(code_buf, tb.size)
 
-    assert (tb.pc not in insn_cache)
     insn_cache[tb.pc] = ""
     for i in md.disasm(code, tb.pc):
         insn_cache[tb.pc] += ("0x%x:\t%s\t%s\n" %(i.address, i.mnemonic, i.op_str))
+
+@panda.cb_after_block_translate(name="before", procname="find")
+def before_block_trans(env, tb):
+    # Before we translate each block in find cache its disassembly
+    generate_insns(env, tb)
     return 0
 
-@panda.cb_before_block_exec(name="exec")
+@panda.cb_before_block_exec(name="exec", procname="find")
 def before_block_exec(env, tb):
-    '''
-    At each BB's execution, print from the cache
-    '''
+    # At each BB's execution in 'find', ensure translation is cached and add to executed_pcs
     pc = panda.current_pc(env)
-    assert(pc in insn_cache)
-    print(insn_cache[pc])
+    if pc not in insn_cache: # If we miss the cache, update it
+        generate_insns(env, tb)
+    executed_pcs.append(pc)
     return 0
-
 
 panda.queue_async(my_runcmd)
-
-
-
 panda.run()
+for pc in executed_pcs: print(insn_cache[pc])
