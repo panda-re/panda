195cc24b3884e2baac01364ac71be79499a280cd
Manolis Stamatogiannakis <mstamat@gmail.com>
2019-10-01 18:38:36 +0200

    qemu: Replaced "qemu" with "panda" in strings used to construct paths.

--- os-posix.c
+++ os-posix.c
@@ -77,7 +77,7 @@ void os_setup_signal_handling(void)
 /* Find a likely location for support files using the location of the binary.
    For installed binaries this will be "$bindir/../share/qemu".  When
    running from the build tree this will be "$bindir/../pc-bios".  */
-#define SHARE_SUFFIX "/share/qemu"
+#define SHARE_SUFFIX "/share/panda"
 #define BUILD_SUFFIX "/pc-bios"
 char *os_find_datadir(void)
 {
