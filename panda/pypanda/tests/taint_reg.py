d3ad74ad2917c6bd62d99cd1d87fb12c3ca8bc8c
Andrew Fasano <fasano@mit.edu>
2019-10-09 13:07:37 -0400

    Update tests to normal imports- taint_ram is failing

--- panda/pypanda/tests/taint_reg.py
+++ panda/pypanda/tests/taint_reg.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-from sys import argv, path as syspath
+from sys import argv
 from os import path
 import capstone
 from elftools.elf.elffile import ELFFile
 from elftools.elf.sections import SymbolTableSection
-
-syspath.append('..')
 from panda import Panda, blocking, ffi
 from panda.x86.helper import R_EAX, R_EBX, R_ECX, registers
 
