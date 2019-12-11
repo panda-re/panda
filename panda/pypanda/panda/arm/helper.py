5077f87ac1abc26b19d7ef2571082a1a882f9a63
Andrew Fasano <fasano@mit.edu>
2019-12-11 11:27:26 -0500

    Fixup arm register names and missing register

--- panda/pypanda/panda/arm/helper.py
+++ panda/pypanda/panda/arm/helper.py
@@ -5,24 +5,25 @@ Work in progress
 
 import binascii
 
-# 0 through 12 are just numbers
+# R0 through 12 are just accessed by number
 R_SP = 13
 R_LR = 14
 R_IP = 15
 
 registers = {
-    "1": 1,
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "10": 10,
-    "11": 11,
-    "12": 12,
+    "R0": 0,
+    "R1": 1,
+    "R2": 2,
+    "R3": 3,
+    "R4": 4,
+    "R5": 5,
+    "R6": 6,
+    "R7": 7,
+    "R8": 8,
+    "R9": 9,
+    "R10": 10,
+    "R11": 11,
+    "R12": 12,
     "SP": R_SP,
     "LR": R_LR,
     "IP": R_IP,
