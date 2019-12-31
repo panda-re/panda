09f61a68f435d83aaf46bf3670de8a8cd22e23bd
Andrew Fasano <fasano@mit.edu>
2019-12-30 21:42:42 -0500

    Add asserts to sleep in cb test

--- panda/pypanda/tests/sleep_in_cb.py
+++ panda/pypanda/tests/sleep_in_cb.py
@@ -9,21 +9,29 @@ and end call to panda.run
 '''
 
 panda = Panda(generic='i386')
-started_sleeping = False
+sleeping_started = False
+sleeping_ended = False
 
 @blocking
 def hang():
-    global started_sleeping
-    started_sleeping = True
+    '''
+    This function should start but never finish because the
+    before block exec callback should end_analysis which
+    preempts this
+    '''
+    global sleeping_started, sleeping_ended
+    sleeping_started = True
     sleep(1000)
+    sleeping_ended = True
     panda.end_analysis()
 
 @panda.cb_before_block_exec
 def before_block_execute(cpustate, transblock):
-    if started_sleeping:
+    if sleeping_started:
         panda.end_analysis()
-    return 0
 
 panda.queue_async(hang)
 panda.run()
 print("Run finished. Ready to exit...")
+assert(sleeping_started)
+assert(not sleeping_ended)
