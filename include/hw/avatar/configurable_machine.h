430d67d70d9409d8be6977be086b333ac7baab38
mariusmue <m.muench@vu.nl>
2020-02-28 17:32:24 +0100

    creating pypanda wrapper for configurable

---
+++ include/hw/avatar/configurable_machine.h
@@ -0,0 +1,14 @@
+#ifndef HW_AVATAR_CONFIGURABLE_MACHINE_H
+#define HW_AVATAR_CONFIGURABLE_MACHINE_H
+
+// BEGIN_PYPANDA_NEEDS_THIS -- do not delete this comment bc pypanda
+// api autogen needs it.  And don't put any compiler directives
+// between this and END_PYPANDA_NEEDS_THIS except includes of other
+// files in this directory that contain subsections like this one.
+
+// from panda_api.c
+
+
+
+// END_PYPANDA_NEEDS_THIS -- do not delete this comment!
+#endif
