ae2abaf576ffdaf476c698cc480018c7628190b9
Andrew Fasano <fasano@mit.edu>
2020-03-30 12:38:22 -0400

    Fixup file_hook

--- panda/python/core/panda/extras/file_hook.py
+++ panda/python/core/panda/extras/file_hook.py
@@ -1,17 +1,4 @@
 import logging
-# TEMP
-import sys
-root = logging.getLogger()
-root.setLevel(logging.DEBUG)
-
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-root.addHandler(handler)
-import coloredlogs
-coloredlogs.install()
-#end temp
 
 class FileHook:
     '''
