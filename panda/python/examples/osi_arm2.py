3114d0c1666705800c8e061d65c21a40f97a3cd0
Andrew Fasano <fasano@mit.edu>
2020-05-03 20:26:26 -0400

    Cleanup osi_arm2.py PoC

--- panda/python/examples/osi_arm2.py
+++ panda/python/examples/osi_arm2.py
@@ -21,28 +21,46 @@ panda.load_plugin("syscalls2")
 # At first execve, enable on_enter_svc callback
 @panda.ppp("syscalls2", "on_sys_execve_enter")
 def first_syscall(cpu, pc, fname_ptr, argv_ptr, envp):
-    #TODO: change to run on first syscall, not just execve
-    panda.enable_callback('asid_changed')
-    #panda.disable_callback("first_syscall") # XXX: can't disable because it's a PPP fn
+    #TODO: change to run on first syscall, not just execve?
+    panda.enable_callback('enter_svc')
+    #panda.disable_callback("first_syscall") # XXX: can't disable because it's a PPP fn (Issue #644)
 
-task_thread_info = None
 @panda.cb_on_enter_svc(enabled=False)
-def asid_changed(cpu):
-    global task_thread_info
+def enter_svc(cpu):
+    '''
+    whenever we go into kernel, run a callback on our next basic block
+    to try getting thread info
+    '''
+    global bbe_ctr
+    panda.enable_callback("bbe")
+
+current_task = None
+@panda.cb_before_block_exec(enabled=False)
+def bbe(cpu, tb):
+    '''
+    For the first block that runs in the kernel, grab the current task struct
+    the kernel stack pointer and use it to find the current task struct
+    '''
+    panda.disable_callback("bbe")
+    global current_task
 
     sp = cpu.env_ptr.regs[R_SP]
     task_thread_info = sp & 0xffffe000
-    print(hex(task_thread_info))
 
     try:
         # task_struct is 0xC into the struct
         task_ptr = panda.virtual_memory_read(cpu, task_thread_info+0xC,
                                                 4, fmt='int')
-    except Exception as e:
+    except MemoryError as e:
         print(f"Fail to read task ptr from task_thread_info at 0x{task_thread_info:x}")
         return
+
     if task_ptr:
-        print(f"Task ptr: 0x{task_ptr:x}")
+        if task_ptr != current_task:
+            print(f"New task: 0x{task_ptr:x}")
+        current_task = task_ptr
+    else:
+        print(f"Fail to read task ptr from task_thread_info at 0x{task_thread_info:x}")
 
 # Start guest execution
 panda.run()
