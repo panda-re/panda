87d3880c1135ac39b21941887567e0263da3b732
Andrew Fasano <fasano@mit.edu>
2020-07-08 12:56:35 -0400

    x86 configurable machine, set default mode to 32-bit (not 16-bit)

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -3067,7 +3067,7 @@ static void x86_cpu_reset(CPUState *s)
     target_ulong cs_base = 0xffff0000;
     if (x86_configurable_machine) {
       // Unicorn sets these two properties. Not sure if we should?
-      //env->hflags = 0;
+      //env->hflags = 0; // Note we set this below!
       //env->cr[0] = 0;
 
       // For x86 configurable machine, we don't want to start in real mode
@@ -3099,6 +3099,10 @@ static void x86_cpu_reset(CPUState *s)
       // setting up initial state. These registers
       // values should all be 0 or undefined at the start
       // of a unicorn-style execution
+
+      // But do set hflags so we're in 32-bit mode (else we end up in 16-bit)
+      // Not exposed to users as an option (yet)
+      env->hflags |= HF_CS32_MASK;
       return;
     }
 
