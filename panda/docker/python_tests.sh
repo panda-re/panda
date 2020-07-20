22ed17e6b46c65a068d26aafef72d6653c3d6237
Andrew Fasano <fasano@mit.edu>
2020-07-20 12:18:25 -0400

    Faster build for qemu tests

--- panda/docker/python_tests.sh
+++ panda/docker/python_tests.sh
@@ -2,37 +2,7 @@
 set -ex
 
 # To be run inside panda docker container where PANDA is already built
-# in /panda/build (from pre-built container) and /panda.new is mapped in
-# from host with newer panda source.
-
-# The /panda.new code will be copied on top of the (old) /panda directory
-# Then /panda will be rebuilt
-# If called with an argument of 'clean' /panda will first be made clean
-
-# Sync new code on top of old - Preservs old build artifacts
-time rsync -rh  /panda.new /panda
-cd /panda/build
-
-# Build 
-NPROC=$(nproc || sysctl -n hw.ncpu)
-
-if [ "$1" = "clean" ]; then
-    make clean
-    shift
-fi
-
-make -j${NPROC} $@
-
-# Install Pypanda (for Bionic and newer)
-cd /panda/panda/python/core/
-pip3 install -q pycparser cffi colorama protobuf # Pypanda dependencies
-python3 setup.py install >/dev/null
-
-# TODO: Can we cache container here and then run tests?
-
-exit 0
-
-# Run pypanda tests
+# in /panda/build and pypanda is installed - run pypanda tests
 
 major_version=$(lsb_release --release | awk -F':[\t ]+' '{print $2}' | awk -F'.' '{print $1}')
 
