6e11f1ac9c782cbadcb95e91ef5deb3fd084ab87
Andrew Fasano <fasano@mit.edu>
2020-08-03 21:39:08 -0400

    Pypanda: setup.py install should include mips helpers
    Also update gitattributes on pypanda autogenerated files
    so they don't show up in github diffs by default

--- panda/python/core/setup.py
+++ panda/python/core/setup.py
@@ -113,7 +113,8 @@ setup(name='panda',
       author_email='fasano@mit.edu',
       url='https://github.com/panda-re/panda/',
       packages=['panda', 'panda.taint', 'panda.autogen',
-                'panda.images', 'panda.arm', 'panda.x86', 'panda.extras'],
+                'panda.images', 'panda.arm', 'panda.x86', 'panda.mips',
+                'panda.extras'],
       package_data = { 'panda': ['data/**/*', # Copy everything (fails?)
           'data/*/panda/plugins/*',    # Copy all plugins
           'data/*/panda/plugins/**/*', # Copy all plugin files
