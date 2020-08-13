0b26bdbfe05cdf8076fec57e0c700666e1d59a81
Andrew Fasano <fasano@mit.edu>
2020-08-13 16:43:22 -0400

    Pypanda: update osi syscalls example

--- panda/python/examples/osi_syscalls.py
+++ panda/python/examples/osi_syscalls.py
@@ -14,6 +14,39 @@ def on_sys_read_return(cpu, pc, fd, buf, count):
     fname = ffi.string(fname_ptr) if fname_ptr != ffi.NULL else "error"
     print(f"[PANDA] {procname} read from {fname}")
 
+@panda.ppp("syscalls2", "on_sys_execve_enter")
+def on_sys_execve_enter(cpu, pc, fname_ptr, argv_ptr, envp):
+    # Log commands and arguments passed to execve unless in kernel
+    if panda.in_kernel(cpu):
+        return
+    try:
+        fname = panda.read_str(cpu, fname_ptr)
+        argv_ptrlist = panda.virtual_memory_read(cpu, argv_ptr, 100, fmt='ptrlist')
+    except ValueError: return
+    argv = []
+    for ptr in argv_ptrlist:
+        if ptr == 0: break
+        try:
+            argv.append(panda.read_str(cpu, ptr))
+        except ValueError:
+            argv.append(f"(error: 0x{ptr:x})")
+
+    print(get_calltree(cpu) + " => " + ' '.join(argv))
+
+def get_calltree(cpu):
+    # Print the calltree to the current process
+    proc = panda.plugins['osi'].get_current_process(cpu)
+    if proc == ffi.NULL:
+        print("Error determining current process")
+        return
+    procs = panda.get_processes_dict(cpu)
+
+    chain = [{'name': ffi.string(proc.name).decode('utf8', 'ignore'),
+              'pid': proc.pid, 'parent_pid': proc.ppid}]
+    while chain[-1]['pid'] > 1 and chain[-1]['parent_pid'] in procs.keys():
+        chain.append(procs[chain[-1]['parent_pid']])
+    return " -> ".join(f"{item['name']} ({item['pid']})" for item in chain[::-1])
+
 
 @blocking
 def start():
