5a2b1347b367f04ca92672d21f2df093494d230d
tnballo <tnballo@users.noreply.github.com>
2020-09-17 11:24:14 -0400

    Commit WIP to branch switch case I hate git stash

--- panda/plugins/dwarf_query/README.md
+++ panda/plugins/dwarf_query/README.md
@@ -6,6 +6,14 @@ Summary
 
 TODO
 
+Generating a DWARF JSON for a Linux Kernel
+---------
+
+1. Acquire a decompressed kernel image, e.g. `vmlinux` (may need to extract from `zImage`).
+2. Install the Go programming language toolchain, as [outlined here](https://golang.org/doc/install).
+3. Clone and build [Volatility's `dwarf2json` tool](https://github.com/volatilityfoundation/dwarf2json).
+4. Run `dwarf2json linux --elf your_vmlinux > vmlinux_dwarf_info.json`.
+
 Arguments
 ---------
 
--- panda/plugins/dwarf_query/dwarf_query.cpp
+++ panda/plugins/dwarf_query/dwarf_query.cpp
@@ -14,6 +14,7 @@ PANDAENDCOMMENT */
 
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <jsoncpp/json/json.h>
 
 #include "panda/plugin.h"
@@ -30,8 +31,7 @@ extern "C" {
 
 // Globals -------------------------------------------------------------------------------------------------------------
 
-Json::Reader reader;
-Json::Value obj;
+// TODO
 
 // Python CFFI API -----------------------------------------------------------------------------------------------------
 
@@ -39,30 +39,79 @@ Json::Value obj;
 
 // Core ----------------------------------------------------------------------------------------------------------------
 
-// TODO
+void load_struct(const Json::Value& struct_entry) {
+    // TODO: stuff here
+}
+
+void load_func(const Json::Value& func_entry) {
+    // TODO: stuff here
+}
+
+void load_json(const Json::Value& root) {
+
+    unsigned struct_cnt = 0;
+    unsigned func_cnt = 0;
+    std::string struct_str("struct");
+    std::string base_str("base");
+
+    for (auto sym : root["user_types"]) {
+
+        if (!sym.isNull()) {
+
+            std::string type;
+
+            if (!sym["kind"].isNull()) {
+                type.assign(sym["kind"].asString());
+            } else if (!sym["type"]["kind"].isNull()) {
+                type.assign(sym["type"]["kind"].asString());
+            }
+
+            if (type.compare(struct_str) == 0) {
+                struct_cnt++;
+                load_struct(sym);
+
+                // TODO: temp debug
+                printf("Loaded struct \'%s\'\n", type.c_str());
+            } else if (type.compare(base_str) == 0) {
+                func_cnt++;
+                load_func(sym);
+
+                // TODO: temp debug
+                printf("Loaded struct \'%s\'\n", type.c_str());
+            }
+        }
+    }
+
+    printf("Loaded %u funcs, %u structs.\n", func_cnt, struct_cnt);
+}
 
 // Setup/Teardown ------------------------------------------------------------------------------------------------------
 
 bool init_plugin(void *_self) {
 
-    if (!pandalog) {
-        fprintf(stderr, "[ERROR] dwarf_query: Set with -pandalog [filename]\n");
-        return  false;
-    }
-
     panda_arg_list *args = panda_get_args("dwarf_query");
-    const char* json_filename = panda_parse_string(args, "json", "dwarf2json_output.json");
+    const char* json_filename = panda_parse_string_req(args, "json", "dwarf2json_output.json");
     std::ifstream ifs(json_filename);
+
+    Json::Reader reader;
+    Json::Value obj;
+
+    if (!reader.parse(ifs, obj)) {
+        fprintf(stderr, "[ERROR] dwarf_query: invalid JSON!\n");
+        return false;
+    } else {
+        load_json(obj);
+    }
 
     switch (panda_os_familyno) {
 
         case OS_LINUX: {
-           return reader.parse(ifs, obj);
+           return true;
         } break;
 
         default: {
             fprintf(stderr, "[WARNING] dwarf_query: This has never been tested for a non-Linux OS!\n");
-            return reader.parse(ifs, obj);
+            return true;
         }
     }
 }
