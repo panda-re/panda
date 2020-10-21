b4d7403fd1a1a03410b0b23cf6eb7ecd0b8b9fe5
Andrew Fasano <fasano@mit.edu>
2020-10-21 16:55:04 -0400

    Backport python3 support to iotests and qemu.py

--- scripts/qemu.py
+++ scripts/qemu.py
@@ -151,7 +151,7 @@ class QEMUMachine(object):
             self._post_shutdown()
             self._popen = None
 
-    underscore_to_dash = string.maketrans('_', '-')
+    underscore_to_dash = bytes.maketrans(b'_', b'-')
     def qmp(self, cmd, conv_keys=True, **args):
         '''Invoke a QMP command and return the result dict'''
         qmp_args = dict()
