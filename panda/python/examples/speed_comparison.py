2781ff1c8fe9b38beae480fdce2cfb3af2256415
Luke Craig <lacraig3@gmail.com>
2020-10-30 19:21:27 -0400

    speed comparison

--- panda/python/examples/speed_comparison.py
+++ panda/python/examples/speed_comparison.py
@@ -1,30 +1,36 @@
 from pandare import Panda, blocking
 from os.path import exists
+from time import sleep
+from pandare.qcows import SUPPORTED_IMAGES
 
-panda = Panda(generic="x86_64")
+panda = Panda(generic="i386")
+
+
 recording_name = "speed_compare"
-
 if not exists(recording_name + "-rr-snp"):
     print("Taking recording")
+
     @panda.queue_blocking
-    def take_recording():
-        panda.record(recording_name, snapshot_name="root")
-        panda.run_serial_cmd("whoami")
-        panda.run_serial_cmd("ls -la")
-        panda.end_record()
+    def run():
+        panda.record_cmd(command="cat /a",recording_name=recording_name)
 
     panda.run()
 else:
     print("Recording exists")
 
+import os
+os._exit(0)
 
 print("Begin C analysis")
+sleep(10)
 panda.load_plugin("cskeleton")
 panda.run_replay(recording_name)
 
 print("Begin Python analysis.")
+sleep(10)
 @panda.cb_before_block_exec
 def bbe(cpu, tb):
+    print("hello")
     pass
 
 panda.run_replay(recording_name)
