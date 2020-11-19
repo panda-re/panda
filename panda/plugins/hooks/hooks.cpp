d8ffb6a03a0cea5348338f385534cd409de68104
Andrew Fasano <fasano@mit.edu>
2020-11-19 10:35:06 -0500

    Update hooks to use before-block-exec callback instead of before translate

--- panda/plugins/hooks/hooks.cpp
+++ panda/plugins/hooks/hooks.cpp
@@ -40,15 +40,16 @@ void* self = NULL;
 // Enable and disable callbacks
 void enable_hooking() {
   assert(self != NULL);
-  panda_enable_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC_INVALIDATE_OPT, c_callback);
+  panda_enable_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC, c_callback);
 }
 void disable_hooking() {
   assert(self != NULL);
-  panda_disable_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC_INVALIDATE_OPT, c_callback);
+  panda_disable_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC, c_callback);
 }
 
 void update_hook(hook_func_t hook, target_ulong value){
-    for (auto it = hooks.begin(); it != hooks.end(); ++it){
+  //Given hook function, move it to fire on a different address
+  for (auto it = hooks.begin(); it != hooks.end(); ++it){
 		if (it->first == value) continue;
         std::vector<hook_func_t> hook_pile = it->second;
         auto i = hook_pile.begin();
@@ -63,6 +64,9 @@ void update_hook(hook_func_t hook, target_ulong value){
        it->second = hook_pile;
     }
 	hooks[value].push_back(hook);
+#if DEBUG
+  printf("Updated hook to fire at %p\n", &hook);
+#endif
 }
 
 void enable_hook(hook_func_t hook, target_ulong value){
@@ -88,9 +92,11 @@ void disable_hook(hook_func_t hook){
 
 
 void add_hook(target_ulong addr, hook_func_t hook) {
+#ifdef DEBUG
   printf("Adding hook from guest 0x" TARGET_FMT_lx " to host %p\n", addr, hook);
+#endif
 
-  if (!panda_is_callback_enabled(self, PANDA_CB_BEFORE_BLOCK_EXEC_INVALIDATE_OPT, c_callback)) enable_hooking(); // Ensure our panda callback is enabled when we add a hook
+  if (!panda_is_callback_enabled(self, PANDA_CB_BEFORE_BLOCK_EXEC, c_callback)) enable_hooking(); // Ensure our panda callback is enabled when we add a hook
 	// check for existing hook
   std::vector<hook_func_t> hook_pile = hooks[addr];
 	for (auto it=hook_pile.begin(); it!=hook_pile.end(); ++it){
@@ -103,25 +109,18 @@ void add_hook(target_ulong addr, hook_func_t hook) {
 
 
 // The panda callback to determine if we should call a python callback
-bool before_block_exec_invalidate_opt(CPUState *cpu, TranslationBlock *tb) {
-    // Call any callbacks registered at this PC. Any called callback may invalidate the translation block
- 
-    bool ret = false;
-
+//bool before_block_exec_invalidate_opt(CPUState *cpu, TranslationBlock *tb) {
+void before_block_exec(CPUState *cpu, TranslationBlock *tb) {
+    // Call any callbacks registered at this PC.
     auto func_hooks = hooks.find(tb->pc);
     if (func_hooks != hooks.end()) {
         for (auto &hook : func_hooks->second) {
-            ret |= (*hook)(cpu, tb);
+#ifdef DEBUG
+          printf("[hooks] Calling hook at %p since guest hit BB at 0x" TARGET_FMT_lx "\n", &hook, tb->pc);
+#endif
+            (*hook)(cpu, tb);
         }
     }
-
-#ifdef DEBUG
-    if (ret) {
-        printf("Invalidating the translation block at 0x" TARGET_FMT_lx "\n", tb->pc);
-    }
-#endif
-
-    return ret;
 }
 
 
@@ -131,9 +130,9 @@ bool init_plugin(void *_self) {
 
     panda_disable_tb_chaining();
 
-    c_callback.before_block_exec_invalidate_opt = before_block_exec_invalidate_opt;
-    panda_register_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC_INVALIDATE_OPT, c_callback);
-    panda_disable_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC_INVALIDATE_OPT, c_callback);
+    c_callback.before_block_exec = before_block_exec;
+    panda_register_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC, c_callback);
+    panda_disable_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC, c_callback);
 
     panda_enable_memcb();
 
