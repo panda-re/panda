e67bb1c06f2f89816ac2b9d0bd49a5a438cb59e4
Luke Craig <lacraig3@gmail.com>
2020-11-23 19:07:43 -0500

    added more work

--- panda/python/examples/do_speed_analysis.py
+++ panda/python/examples/do_speed_analysis.py
@@ -1,3 +1,4 @@
+import numpy as np
 def do_run_on_file(fname):
     arr = []
     num = 0
@@ -6,10 +7,11 @@ def do_run_on_file(fname):
             if "Time taken for" in line and " before_block_exec (nanoseconds): " in line:
                 arr.append(int(line.split(": ")[1]))
                 num += 1
-    return max(arr), min(arr), sum(arr) * 1.0 / num, num
+    arr = np.array(arr)
+    return max(arr), min(arr), sum(arr) * 1.0 / num, np.std(arr), num
 
-f_list = ["a.py", "c.py"]
+f_list = ["c_times", "py_times"]
 
 for q in f_list:
-    max_num, min_num, average, num_counted = do_run_on_file(q)
-    print(f"{q}: average: {average} min: {min_num} max: {max_num} sample size of {num_counted}")
+    max_num, min_num, average, std, num_counted = do_run_on_file(q)
+    print(f"{q}: average: {average} min: {min_num} max: {max_num} std: {std} sample size of {num_counted}")
