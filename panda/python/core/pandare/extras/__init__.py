ac67377be11af67eefa03260f32ecede82f97487
Andrew Fasano <fasano@mit.edu>
2021-01-31 15:25:04 -0500

    Pypanda extras: ModeFilter decorator

--- panda/python/core/pandare/extras/__init__.py
+++ panda/python/core/pandare/extras/__init__.py
@@ -1,3 +1,12 @@
 """
 Extras are a bit like python-based plugins. You can import them, instantiate them as classes, and use their functionality.
+
+Note file names should NOT contain underscores, let's keep these in (upper)
+CamelCase going forward (e.g., ModeFilter) so they match the class names.
 """
+from .ModeFilter import ModeFilter
+
+from .file_hook import FileHook
+from .file_faker import FakeFile
+from .ioctl_faker import IoctlFaker
+from .proc_write_capture import ProcWriteCapture
