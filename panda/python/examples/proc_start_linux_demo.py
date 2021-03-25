4794677448c2984e1044f4f78914325e7a98216b
Luke Craig <lacraig3@gmail.com>
2021-03-25 11:37:41 -0400

    New plugin: proc_start_linux (#914)
    Co-authored-by: Andrew Fasano <fasano@mit.edu>

---
+++ panda/python/examples/proc_start_linux_demo.py
@@ -0,0 +1,18 @@
+from pandare import Panda
+from sys import argv
+
+panda = Panda(generic=argv[1])
+
+@panda.queue_blocking
+def guest_interaction():
+    panda.revert_sync("root")
+    for cmd in ["ls -la", "whoami", "time ls -la"]:
+        print(f"{cmd} {panda.run_serial_cmd('LD_SHOW_AUXV=1 '+cmd)}")
+    panda.end_analysis()
+
+@panda.ppp("proc_start_linux", "on_rec_auxv")
+def recv_auxv(cpu, tb, auxv):
+    procname = panda.ffi.string(auxv.procname)
+    print(f"started proc {procname} {auxv.phdr:x} {auxv.entry:x}")
+
+panda.run()
