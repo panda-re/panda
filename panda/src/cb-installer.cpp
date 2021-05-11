5c91dda051f5cc666453258092172aae4fb1395b
Luke Craig <lacraig3@gmail.com>
2021-05-11 16:50:41 -0400

    add call on each exit

--- panda/src/cb-installer.cpp
+++ panda/src/cb-installer.cpp
@@ -12,10 +12,19 @@ void panda_install_block_callbacks(CPUState* cpu, TranslationBlock *tb){
     }else{
         printf("error on start\n");
     }
-    TCGOp* end = find_last_guest_insn();
-    if (end != NULL){
-        insert_call(&end, panda_callbacks_end_block_exec, first_cpu, tb);
-    }else{
+    
+    bool found_exit = false;
+    TCGOp *last_op = NULL;
+    TCGOp *op = NULL;
+    for (int oi = tcg_ctx.gen_op_buf[0].next; oi != 0; oi = op->next) {
+        op = &tcg_ctx.gen_op_buf[oi];
+        if (INDEX_op_exit_tb == op->opc) {
+            insert_call(&last_op, panda_callbacks_end_block_exec, first_cpu, tb);
+            found_exit = true;
+        }
+        last_op = op;
+    }
+    if (!found_exit) {
         printf("error on end %d\n", tb->size);
     }
 }
