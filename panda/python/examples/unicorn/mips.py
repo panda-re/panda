fd85069a5d0b9707c3e91a552d078fca1e1ee12c
Luke Craig <lacraig3@gmail.com>
2021-05-21 17:18:28 -0400

    mips example

--- panda/python/examples/unicorn/mips.py
+++ panda/python/examples/unicorn/mips.py
@@ -19,15 +19,15 @@ nop
 addiu $t1, 1  # t1++
 """
 
-ks = keystone.Ks(keystone.KS_ARCH_MIPS, keystone.KS_MODE_MIPS32)
-#ks = keystone.Ks(keystone.KS_ARCH_MIPS, keystone.KS_MODE_MIPS32 + KS_MODE_BIG_ENDIAN)
+#ks = keystone.Ks(keystone.KS_ARCH_MIPS, keystone.KS_MODE_MIPS32)
+ks = keystone.Ks(keystone.KS_ARCH_MIPS, keystone.KS_MODE_MIPS32 + keystone.KS_MODE_BIG_ENDIAN)
 
-ADDRESS = 0x1000
+ADDRESS = 0x80000000#0x1000
 encoding, count = ks.asm(CODE, ADDRESS)
 stop_addr = ADDRESS + len(encoding)
 
-panda = Panda("mipsel",
-        extra_args=["-M", "configurable", "-nographic"],
+panda = Panda("mips",
+        extra_args=["-M", "configurable", "-nographic", '-d', 'int'],
         raw_monitor=True) # Allows for a user to ctrl-a + c then type quit if things go wrong
 
 @panda.cb_after_machine_init
@@ -42,7 +42,8 @@ def setup(cpu):
     panda.physical_memory_write(ADDRESS, bytes(encoding))
 
     # Set up registers
-    cpu.env_ptr.active_tc.gpr[panda.arch.registers['t0']] = 0x10
+    #cpu.env_ptr.active_tc.gpr[panda.arch.registers['t0']] = 0x10
+    panda.arch.set_reg(cpu, 't0', 0x10)
 
     # Set starting_pc
     cpu.env_ptr.active_tc.PC = ADDRESS
@@ -59,8 +60,8 @@ def on_insn(cpu, pc):
     if pc >= stop_addr:
         print("Finished execution")
         #dump_regs(panda, cpu)
-        print("Register t0 contains:", hex(cpu.env_ptr.active_tc.gpr[panda.arch.registers['t0']]))
-        print("Register t1 contains:", hex(cpu.env_ptr.active_tc.gpr[panda.arch.registers['t1']]))
+        print("Register t0 contains:", hex(panda.arch.get_reg(cpu, 't0')))
+        print("Register t1 contains:", hex(panda.arch.get_reg(cpu,'t1')))
         os._exit(0) # TODO: we need a better way to stop here
 
     code = panda.virtual_memory_read(cpu, pc, 12)
