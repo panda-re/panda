a76a53c411341773ddb85bac5b4085040f8572e6
Luke Craig <lacraig3@gmail.com>
2021-05-24 23:27:23 -0400

    FIX MIPS datatype alignment and update example (#1011)

--- panda/python/examples/unicorn/mips.py
+++ panda/python/examples/unicorn/mips.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 from pandare import Panda, ffi
-import capstone
-import keystone
+from capstone import *
+from capstone.mips import *
+from keystone import *
 import os
 
 CODE = b"""
@@ -19,14 +20,13 @@ nop
 addiu $t1, 1  # t1++
 """
 
-ks = keystone.Ks(keystone.KS_ARCH_MIPS, keystone.KS_MODE_MIPS32)
-#ks = keystone.Ks(keystone.KS_ARCH_MIPS, keystone.KS_MODE_MIPS32 + KS_MODE_BIG_ENDIAN)
+ks = Ks(KS_ARCH_MIPS,KS_MODE_MIPS32 + KS_MODE_BIG_ENDIAN)
 
 ADDRESS = 0x1000
 encoding, count = ks.asm(CODE, ADDRESS)
 stop_addr = ADDRESS + len(encoding)
 
-panda = Panda("mipsel",
+panda = Panda("mips",
         extra_args=["-M", "configurable", "-nographic"],
         raw_monitor=True) # Allows for a user to ctrl-a + c then type quit if things go wrong
 
@@ -42,7 +42,8 @@ def setup(cpu):
     panda.physical_memory_write(ADDRESS, bytes(encoding))
 
     # Set up registers
-    cpu.env_ptr.active_tc.gpr[panda.arch.registers['t0']] = 0x10
+    #cpu.env_ptr.active_tc.gpr[panda.arch.registers['t0']] = 0x10
+    panda.arch.set_reg(cpu, 't0', 0x10)
 
     # Set starting_pc
     cpu.env_ptr.active_tc.PC = ADDRESS
@@ -50,7 +51,7 @@ def setup(cpu):
 # Always run insn_exec
 panda.cb_insn_translate(lambda x,y: True)
 
-md = capstone.Cs(capstone.CS_ARCH_MIPS, 4) # misp32
+md = Cs(CS_ARCH_MIPS, CS_MODE_MIPS32+ CS_MODE_BIG_ENDIAN) # misp32
 @panda.cb_insn_exec
 def on_insn(cpu, pc):
     '''
@@ -59,11 +60,11 @@ def on_insn(cpu, pc):
     if pc >= stop_addr:
         print("Finished execution")
         #dump_regs(panda, cpu)
-        print("Register t0 contains:", hex(cpu.env_ptr.active_tc.gpr[panda.arch.registers['t0']]))
-        print("Register t1 contains:", hex(cpu.env_ptr.active_tc.gpr[panda.arch.registers['t1']]))
+        print("Register t0 contains:", hex(panda.arch.get_reg(cpu, 't0')))
+        print("Register t1 contains:", hex(panda.arch.get_reg(cpu,'t1')))
         os._exit(0) # TODO: we need a better way to stop here
 
-    code = panda.virtual_memory_read(cpu, pc, 12)
+    code = panda.virtual_memory_read(cpu, pc, 4)
     for i in md.disasm(code, pc):
         print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
         break
