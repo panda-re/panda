f3aec81a8d047675b921d74b077ab78494ab6296
Andrew Fasano <fasano@mit.edu>
2021-07-12 16:04:25 -0400

    Plog_to_pe: first try to import from pandare, bugfix in syscall arg dict keys to match pandelephant new_syscall API

--- panda/scripts/plog_to_pandelephant.py
+++ panda/scripts/plog_to_pandelephant.py
@@ -10,13 +10,14 @@ import collections
 import pandelephant
 
 # PLogReader from pandare package is easiest to import,
-# but if it's unavailable, fallback to searching PYTHONPATH
+# but if it's unavailable, fallback to searching
+# local directory, then PYTHONPATH
 # which users should add panda/panda/scripts to
 try:
-    from plog_reader import PLogReader
+    from pandare.plog_reader import PLogReader
 except ImportError:
     try:
-        from pandare.plog_reader import PLogReader
+        from plog_reader import PLogReader
     except ImportError:
         import PLogReader
     except ImportError:
@@ -201,7 +202,7 @@ def CollectProcessMemoryMappings(pandalog, processes):
                         CollectFrom[k](msg, getattr(msg, k))
                     except Exception as e:
                         FailCounts[k] += 1
-                        print(e)
+                        print("Exception:", e)
                     break
     print('Processed {} messages ({} without mapping)'.format(sum(AttemptCounts[k] for k in AttemptCounts.keys()), num_no_mappings))
     for k in CollectFrom.keys():
@@ -215,7 +216,7 @@ def ConvertTaintFlowsAndSyscallsToDatabase(datastore, CollectedSyscalls, Collect
     for s in CollectedSyscalls:
         args = []
         for a in s.Arguments:
-            args.append({'name': a.Name, 'type': a.Type, 'value': a.Value})
+            args.append({'Name': a.Name, 'Type': a.Type, 'Value': a.Value})
         if s.Thread in CollectedThreadToDatabaseThread:
             CollectedSyscallToDatabaseSyscall[s] = datastore.new_syscall(CollectedThreadToDatabaseThread[s.Thread], s.Name, s.RetVal, args, s.InstructionCount)
 
@@ -249,7 +250,7 @@ def CollectTaintFlowsAndSyscalls(pandalog, CollectedBetterMappingRanges):
             'i64': ('signed64',   '{:d}'),
             'i32': ('signed32',   '{:d}'),
             'i16': ('signed16',   '{:d}'),
-            'bytes_val': ('signed16',   '{:d}'),
+            'bytes_val': ('bytes',   '{:d}'),
         }
         def syscall_arg_value(arg):
             for fld, (typ, fmt) in SyscallFieldInfo.items():
@@ -271,6 +272,7 @@ def CollectTaintFlowsAndSyscalls(pandalog, CollectedBetterMappingRanges):
                 for arg in msg.args
             )
         ))
+
         return
     def CollectFrom_taint_flow(entry, msg):
         source_thread = msg.source.cp.thread
@@ -340,7 +342,7 @@ def CollectTaintFlowsAndSyscalls(pandalog, CollectedBetterMappingRanges):
                         CollectFrom[k](msg, getattr(msg, k))
                     except Exception as e:
                         FailCounts[k] += 1
-                        print(e)
+                        print("Exception:", e)
                     break
     for k in CollectFrom.keys():
         print('\t{} Attempts: {}, Failures: {}'.format(k, AttemptCounts[k], FailCounts[k]))
