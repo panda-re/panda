4f5e5a507291a0ad228dfe6fd08d1e912b59ab04
jamcleod <Jordan.McLeod@ll.mit.edu>
2021-07-23 15:13:22 -0400

    Add aarch64 support to rust_skeleton

--- panda/plugins/rust_skeleton/src/lib.rs
+++ panda/plugins/rust_skeleton/src/lib.rs
@@ -2,9 +2,6 @@ use panda::prelude::*;
 
 #[panda::init]
 fn init(_: &mut PluginHandle) -> bool {
-    #[cfg(feature = "aarch64")] {
-        panic!("aarch64 is not a supported target");
-    }
     println!("Initialized!");
     true
 }
