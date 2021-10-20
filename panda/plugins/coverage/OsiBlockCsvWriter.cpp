b9020fdebbc706318b72ca11e12f90d32ea93530
Andrew Fasano <fasano@mit.edu>
2021-10-20 11:38:31 -0400

    Add summarize mode to coverage plugin

--- panda/plugins/coverage/OsiBlockCsvWriter.cpp
+++ panda/plugins/coverage/OsiBlockCsvWriter.cpp
@@ -5,34 +5,47 @@ namespace coverage
 {
 
 OsiBlockCsvWriter::OsiBlockCsvWriter(const std::string &filename,
-    bool start_disabled)
+    bool _summarize_results, bool start_disabled)
 {
     os.exceptions(std::ofstream::failbit | std::ofstream::badbit);
     if (!start_disabled) {
         os.open(filename);
         write_header();
     }
+
+    summarize_results = _summarize_results;
 }
 
 void OsiBlockCsvWriter::handle(OsiBlock record)
 {
-    if (!os.is_open()) {
-        return;
-    }
+    if (summarize_results) {
+        // If this is a new key, initialize it with an empty set
+        cov_map.emplace(record.process_name, std::unordered_set<target_ulong>());
+        // Set must exist now, add this block
+        cov_map[record.process_name].insert(record.block.addr);
+    } else {
+        if (!os.is_open()) {
+            return;
+        }
 
-    os << record.process_name << ","
-       << std::dec << record.pid << ","
-       << std::dec << record.tid << ","
-       << std::dec << record.in_kernel << ","
-       << "0x" << std::hex << record.block.addr << ","
-       << std::dec << record.block.size << "\n";
+        os << record.process_name << ","
+            << std::dec << record.pid << ","
+            << std::dec << record.tid << ","
+            << std::dec << record.in_kernel << ","
+            << "0x" << std::hex << record.block.addr << ","
+            << std::dec << record.block.size << "\n";
+    }
 }
 
 void OsiBlockCsvWriter::write_header()
 {
     write_metadata(os);
     os << "process\n";
-    os << "process name,process id,thread id,in kernel,block address,block size\n";
+    if (summarize_results){
+        os << "process name,block count\n";
+    } else {
+        os << "process name,process id,thread id,in kernel,block address,block size\n";
+    }
 }
 
 void OsiBlockCsvWriter::handle_enable(const std::string& filename)
@@ -43,6 +56,18 @@ void OsiBlockCsvWriter::handle_enable(const std::string& filename)
 
 void OsiBlockCsvWriter::handle_disable()
 {
+    if (!os.is_open()) {
+        return;
+    }
+
+    if (summarize_results) {
+        // dump results
+        for ( auto it = cov_map.begin(); it != cov_map.end(); ++it )  {
+            std::string name = it->first;
+            size_t sz = it->second.size();
+            os << name << "," << sz << "\n";
+        }
+    }
     os.close();
 }
 
