f63c471c43b39b0fa1072191b81d876a2865b827
Luke Craig <lacraig3@gmail.com>
2021-11-17 12:06:25 -0500

    first attempt at pure syscall injection

--- panda/plugins/linjector/src/lib.rs
+++ panda/plugins/linjector/src/lib.rs
@@ -1,33 +1,21 @@
+use panda::current_asid;
+use panda::sys::get_cpu;
 use panda::{
-    mem::{virtual_memory_read, virtual_memory_write},
-    plugins::{
-        hooks::Hook,
-        proc_start_linux::{AuxvValues, PROC_START_LINUX},
-    },
+    mem::virtual_memory_write,
+    plugins::syscalls2::SYSCALLS,
     prelude::*,
-    regs::{get_pc, get_reg, set_reg},
+    syscall_injection::{run_injector, syscall},
 };
 
 use std::{
     cmp::min,
-    convert::TryFrom,
     sync::atomic::{AtomicUsize, Ordering},
 };
 
-use object::{Object, ObjectSection};
 use once_cell::sync::OnceCell;
 
-static POINTERS: OnceCell<[target_ulong; 3]> = OnceCell::new();
-static POINTERS_READ: AtomicUsize = AtomicUsize::new(0);
-static SAVED_BUF: OnceCell<Vec<u8>> = OnceCell::new();
-static SAVED_PC: OnceCell<target_ulong> = OnceCell::new();
 static ELF_TO_INJECT: OnceCell<Vec<u8>> = OnceCell::new();
 static ELF_READ_POS: AtomicUsize = AtomicUsize::new(0);
-
-const MAGIC: usize = 0x10adc0d3;
-
-mod regs;
-use regs::{REG_ORDER, RET_REG};
 
 #[derive(PandaArgs)]
 #[name = "linjector"] // plugin name
@@ -42,218 +30,155 @@ lazy_static::lazy_static! {
     static ref ARGS: Args = Args::from_panda_args();
 }
 
-#[derive(Copy, Clone)]
-pub enum HcCmd {
-    Noop = 0,
-    Start,         /* start new action */
-    Stop,          /* stop action */
-    Read,          /* read buffer from hypervisor */
-    Write,         /* write buffer TO hypervisor*/
-    Error,         /* report error to hypervisor*/
-    ConditionalOp, /* ask the hypervisor if op should be completed*/
-    NextStateMachine, /* ask the hypervisor manager to move to the next
-                   state machine*/
+const MMAP2: target_ulong = 192;
+const NULL: target_ulong = 0;
+const NEG_1: target_ulong = target_ulong::MAX;
+const PAGE_SIZE: target_ulong = 0x1000;
+const PROT_READ: target_ulong = 0x1; /* Page can be read.  */
+const PROT_WRITE: target_ulong = 0x2; /* Page can be written.  */
+const PROT_EXEC: target_ulong = 0x4; /* Page can be executed.  */
+const MAP_SHARED: target_ulong = 0x1;
+const MAP_ANON: target_ulong = 0x20;
+
+async fn do_mmap() -> target_ulong {
+    syscall(
+        MMAP2,
+        (
+            NULL,
+            PAGE_SIZE,
+            PROT_READ | PROT_WRITE | PROT_EXEC,
+            MAP_SHARED | MAP_ANON,
+            NEG_1,
+            NULL,
+        ),
+    )
+    .await
 }
 
-impl TryFrom<usize> for HcCmd {
-    type Error = ();
+const MEMFD_CREATE: target_ulong = 356;
+const MFD_CLOEXEC: target_ulong = 1;
 
-    fn try_from(value: usize) -> Result<Self, ()> {
-        match value {
-            0 => Ok(HcCmd::Noop),
-            1 => Ok(HcCmd::Start),
-            2 => Ok(HcCmd::Stop),
-            3 => Ok(HcCmd::Read),
-            4 => Ok(HcCmd::Write),
-            5 => Ok(HcCmd::Error),
-            6 => Ok(HcCmd::ConditionalOp),
-            7 => Ok(HcCmd::NextStateMachine),
-            _ => Err(()),
-        }
-    }
+async fn do_memfd_create(mmap_addr: target_ulong) -> target_ulong {
+    // we use the mmap_addr for the name because we've zeroed it
+    // so it will be a '\0' litera,l name
+    syscall(MEMFD_CREATE, (mmap_addr, MFD_CLOEXEC)).await
 }
 
-fn parse_file_data(file_bytes: &[u8]) -> (&[u8], usize, usize) {
-    let obj_file = object::File::parse(file_bytes).expect("Couldn't parse ELF");
-    let text_section = obj_file
-        .section_by_name(".text")
-        .expect("Couldn't locate .text section");
-    let text_data = text_section.data().unwrap();
-    let offset = text_section.address() - obj_file.entry();
-    let section_size = text_section.size();
-    (text_data, offset as usize, section_size as usize)
+const WRITE: target_ulong = 4;
+
+async fn do_write(
+    mem_fd: target_ulong,
+    mmap_addr: target_ulong,
+    len: target_ulong,
+) -> target_ulong {
+    syscall(WRITE, (mem_fd, mmap_addr, len)).await
 }
 
-fn get_hyp_reg(cpu: &mut CPUState, num: usize) -> usize {
-    let reg_to_read = REG_ORDER[num];
-    get_reg(cpu, reg_to_read) as usize
+const FORK: target_ulong = 2;
+
+async fn do_fork() -> target_ulong {
+    syscall(FORK, ()).await
 }
 
-#[panda::hook]
-fn inject_hook(
-    cpu: &mut CPUState,
-    _tb: &mut TranslationBlock,
-    _exit_code: u8,
-    hook: &mut Hook,
-) {
-    println!("Inject hook");
-    let inject_bytes = include_bytes!("./injectables/injector");
-    let (text_data, _offset, _section_size) =
-        parse_file_data(&inject_bytes[..]);
-    let pc = panda::regs::get_pc(cpu);
-    println!("about to inject injector at {:#x}", pc);
-    virtual_memory_write(cpu, pc, text_data);
-    hook.enabled = false;
+const EXECVE: target_ulong = 11;
+
+async fn do_execve(
+    path: target_ulong,
+    argv: target_ulong,
+    envp: target_ulong,
+) -> target_ulong {
+    syscall(EXECVE, (path, argv, envp)).await
 }
 
-fn hyp_start(_cpu: &mut CPUState, arg1: usize, _arg2: usize) -> Option<usize> {
-    inject_hook::hook()
-        .after_block_exec()
-        .at_addr(arg1 as target_ulong);
-    None
-}
-
-fn hyp_write(cpu: &mut CPUState, arg1: usize, arg2: usize) -> Option<usize> {
-    ELF_TO_INJECT.get_or_init(|| std::fs::read(&ARGS.guest_binary).unwrap());
-    let buf_to_write = arg1;
-    let size_requested = arg2;
-
-    let read_pos = ELF_READ_POS.load(Ordering::SeqCst);
-    let buf_size = ELF_TO_INJECT.get().unwrap().len();
-    if read_pos < buf_size {
-        let lower = read_pos;
-        let upper = min(buf_size, read_pos + size_requested);
-        let data_to_write = &ELF_TO_INJECT.get().unwrap()[lower..upper];
-        virtual_memory_write(cpu, buf_to_write as target_ulong, data_to_write);
-        ELF_READ_POS.fetch_add(upper - lower, Ordering::SeqCst);
-        Some(upper - lower)
-    } else {
-        println!("finished processing binary");
-        Some(0)
-    }
-}
-
-fn hyp_read(cpu: &mut CPUState, arg1: usize, _arg2: usize) -> Option<usize> {
-    assert!(arg1 != 0, "arg1 is a fork return 0 is the child");
-    let ptr_pos = POINTERS_READ.load(Ordering::SeqCst);
-    let pointers = POINTERS.get().unwrap();
-    println!("ptr_pos {}, ptrs.len() {}", ptr_pos, pointers.len());
-
-    if ptr_pos < pointers.len() {
-        POINTERS_READ.fetch_add(1, Ordering::SeqCst);
-        println!("Returning {:#x}", pointers[ptr_pos]);
-        // on the last ptr_pos replace the new code
-        if ptr_pos == pointers.len() - 1 {
-            virtual_memory_write(
-                cpu,
-                *SAVED_PC.get().unwrap(),
-                SAVED_BUF.get().unwrap(),
-            );
-        }
-        Some(pointers[ptr_pos] as usize)
-    } else {
-        None
-    }
-}
-
-fn hyp_stop(cpu: &mut CPUState, arg1: usize, _arg2: usize) -> Option<usize> {
-    if POINTERS_READ.load(Ordering::SeqCst) == POINTERS.get().unwrap().len() {
-        virtual_memory_write(cpu, arg1 as target_ulong, b"\x01\x02\x03\x04");
-        virtual_memory_write(
-            cpu,
-            *SAVED_PC.get().unwrap(),
-            SAVED_BUF.get().unwrap(),
-        );
-    }
-    None
-}
-
-#[panda::guest_hypercall]
-fn hypercall_handler(cpu: &mut CPUState) -> bool {
-    let magicval = get_hyp_reg(cpu, 0);
-    if magicval == MAGIC {
-        let action = get_hyp_reg(cpu, 1);
-        let first_arg = get_hyp_reg(cpu, 2);
-        let second_arg = get_hyp_reg(cpu, 3);
-
-        let retval = match HcCmd::try_from(action) {
-            Ok(HcCmd::Start) => hyp_start(cpu, first_arg, second_arg),
-            Ok(HcCmd::Write) => hyp_write(cpu, first_arg, second_arg),
-            Ok(HcCmd::Read) => hyp_read(cpu, first_arg, second_arg),
-            Ok(HcCmd::Stop) => hyp_stop(cpu, first_arg, second_arg),
-            _ => None,
-        };
-
-        if let Some(retval) = retval {
-            set_reg(cpu, RET_REG, retval as target_ulong);
-        }
-    }
-    true
-}
-
-#[panda::hook]
-fn entry_hook(
-    cpu: &mut CPUState,
-    _tb: &mut TranslationBlock,
-    _exit_code: u8,
-    hook: &mut Hook,
-) {
-    println!("Entry hook");
-    let inject_bytes = include_bytes!("./injectables/tiny_mmap");
-    let (text_data, offset, section_size) = parse_file_data(&inject_bytes[..]);
-    assert_eq!(
-        offset, 0,
-        "get better shellcode. why is there another function?"
-    );
-    let pc = get_pc(cpu);
-    SAVED_PC.set(pc).unwrap();
-    SAVED_BUF
-        .set(
-            virtual_memory_read(cpu, pc, section_size as usize)
-                .expect("failed to read buf. you might need a smaller injector or another stage"),
-        )
-        .unwrap();
-    println!("Writing {:#x} bytes at {:#x}", section_size, pc);
-    virtual_memory_write(cpu, pc, text_data);
-    println!("Replacing bytes at PC with tiny_mmap");
-
-    hook.enabled = false;
-}
-
-#[panda::on_rec_auxv]
-fn handle_proc_start(
+#[panda::on_all_sys_enter]
+fn on_sys_enter(
     _cpu: &mut CPUState,
-    _tb: &mut TranslationBlock,
-    auxv: &AuxvValues,
+    pc: SyscallPc,
+    _syscall_num: target_ulong,
 ) {
-    println!("proc start");
-    if !ARGS.require_root || auxv.euid == 0 {
-        println!("accepting new proc with euid {}", auxv.euid);
-        // get pointers to values for re-starting process
+    let file_data = ELF_TO_INJECT.get().unwrap();
+    run_injector(pc, async move {
+        if ARGS.require_root {
+            let is_root = syscall(24, ()).await == 0 as target_ulong;
+            if !is_root {
+                SYSCALLS.add_callback_on_all_sys_enter(on_sys_enter);
+                return;
+            }
+        }
+        let cpu = unsafe { &mut *get_cpu() };
+        println!("In injector");
+        println!("current asid: {:x}", current_asid(cpu));
+        // only problematic in multi-cpu systems
+        // mmap a region
+        let mmap_addr = do_mmap().await;
+        println!("Got mmap addr {:#x}", mmap_addr);
+        println!("current asid: {:x}", current_asid(cpu));
+        // zero it out
+        virtual_memory_write(cpu, mmap_addr, &[0u8; PAGE_SIZE as usize]);
+        let mem_fd = do_memfd_create(mmap_addr).await;
+        println!("Got fd {:#x}", mem_fd);
+        println!("current asid: {:x}", current_asid(cpu));
+        loop {
+            match ELF_READ_POS.fetch_add(PAGE_SIZE as usize, Ordering::SeqCst) {
+                x if x < file_data.len() => {
+                    let size =
+                        min(PAGE_SIZE as isize, (file_data.len() - x) as isize);
+                    println!(
+                        "Writing {} bytes [{}-{}] total: {}...",
+                        PAGE_SIZE,
+                        x,
+                        x + PAGE_SIZE as usize,
+                        file_data.len()
+                    );
+                    println!("current asid: {:x}", current_asid(cpu));
+                    virtual_memory_write(
+                        cpu,
+                        mmap_addr,
+                        &file_data[x..x + size as usize],
+                    );
+                    do_write(mem_fd, mmap_addr, PAGE_SIZE).await;
+                }
+                _ => break,
+            }
+        }
+        println!("forking...");
 
-        let &AuxvValues {
-            execfn_ptr,
-            argv_ptr_ptr,
-            env_ptr_ptr,
-            ..
-        } = auxv;
+        let is_parent = do_fork().await == 0 as target_ulong;
+        println!("is_parent is {}", is_parent);
 
-        // in your callback
-        POINTERS
-            .set([execfn_ptr, argv_ptr_ptr, env_ptr_ptr])
-            .unwrap();
-
-        // set a hook at the entrypoint
-        entry_hook::hook().after_block_exec().at_addr(auxv.entry);
-
-        PROC_START_LINUX.remove_callback_on_rec_auxv(handle_proc_start);
-    }
+        let execbuf = [
+            47u8,
+            112,
+            114,
+            111,
+            99,
+            47,
+            115,
+            101,
+            108,
+            102,
+            47,
+            102,
+            100,
+            47,
+            48 + mem_fd as u8,
+            0,
+        ];
+        virtual_memory_write(cpu, mmap_addr, &execbuf);
+        println!("execve()");
+        let end_mmap_buf =
+            mmap_addr as target_ulong + (execbuf.len() - 1) as target_ulong;
+        do_execve(mmap_addr, end_mmap_buf, end_mmap_buf).await;
+        println!("finished");
+    });
+    SYSCALLS.remove_callback_on_all_sys_enter(on_sys_enter);
 }
 
 #[panda::init]
 fn init(_: &mut PluginHandle) -> bool {
-    println!("linjector hit");
+    println!("linjector asdf hit");
     lazy_static::initialize(&ARGS);
+    ELF_TO_INJECT.get_or_init(|| std::fs::read(&ARGS.guest_binary).unwrap());
     println!("got args {} {}", ARGS.guest_binary, ARGS.require_root);
     true
 }
