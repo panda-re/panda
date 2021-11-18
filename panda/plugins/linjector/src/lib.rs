c7e1e6d0dcc1ba8a4fb194462bf89959355ba5c9
Luke Craig <lacraig3@gmail.com>
2021-11-18 15:33:02 -0500

    forward progress

--- panda/plugins/linjector/src/lib.rs
+++ panda/plugins/linjector/src/lib.rs
@@ -1,10 +1,12 @@
-use panda::sys::get_cpu;
-use panda::{current_asid, current_pc};
+use panda::sys::{get_cpu, qemu_loglevel, CPUX86State};
+use panda::{current_asid, current_pc, current_sp, PppCallback};
 use panda::{
     mem::virtual_memory_read,
     mem::virtual_memory_write,
+    plugins::osi::OSI,
     plugins::syscalls2::SYSCALLS,
     prelude::*,
+    regs::Reg::*,
     syscall_injection::{run_injector, syscall},
 };
 
@@ -45,21 +47,27 @@ const EXECVE: target_ulong = 59;
 const MEMFD_CREATE: target_ulong = 319;
 
 const NULL: target_ulong = 0;
-const NEG_1: target_ulong = target_ulong::MAX;
-const PAGE_SIZE: target_ulong = 0x1000;
-const PROT_READ: target_ulong = 0x1; /* Page can be read.  */
-const PROT_WRITE: target_ulong = 0x2; /* Page can be written.  */
-const PROT_EXEC: target_ulong = 0x4; /* Page can be executed.  */
-const MAP_SHARED: target_ulong = 0x1;
+const NEG_1: target_ulong = u32::MAX as target_ulong;
+const PAGE_SIZE: target_ulong = 1024;
+// const PROT_READ: target_ulong = 0x1; /* Page can be read.  */
+// const PROT_WRITE: target_ulong = 0x2; /* Page can be written.  */
+// const PROT_EXEC: target_ulong = 0x4; /* Page can be executed.  */
+//const MAP_SHARED: target_ulong = 0x1;
+//const MAP_ANON: target_ulong = 0x20;
+//const MAP_PRIVATE: target_ulong = 0x2;
+// andrew's script values
+const PROT_READ: target_ulong = 4;
+const PROT_WRITE: target_ulong = 2;
 const MAP_ANON: target_ulong = 0x20;
+const MAP_SHARED: target_ulong = 0x2;
 
 async fn do_mmap() -> target_ulong {
     syscall(
         MMAP,
         (
-            NULL,
+            0u64,
             PAGE_SIZE,
-            PROT_READ | PROT_WRITE | PROT_EXEC,
+            PROT_READ | PROT_WRITE,
             MAP_SHARED | MAP_ANON,
             NEG_1,
             NULL,
@@ -72,7 +80,7 @@ const MFD_CLOEXEC: target_ulong = 1;
 
 async fn do_memfd_create(mmap_addr: target_ulong) -> target_ulong {
     // we use the mmap_addr for the name because we've zeroed it
-    // so it will be a '\0' litera,l name
+    // so it will be a '\0' literal name
     syscall(MEMFD_CREATE, (mmap_addr, MFD_CLOEXEC)).await
 }
 
@@ -103,35 +111,60 @@ fn read_2_bytes_at_pc(_cpu: &mut CPUState, pc: target_ulong) {
     println!("opcode@PC 0: {:x} 1: {:x}", a[0], a[1]);
 }
 
+const DUP: target_ulong = 32;
+const DUP2: target_ulong = 63;
+const MLOCK: target_ulong = 150;
+
 #[panda::on_all_sys_enter]
 fn on_sys_enter(_cpu: &mut CPUState, pc: SyscallPc, syscall_num: target_ulong) {
+    if OSI.get_current_process(_cpu).get_name() != "cat" {
+        return;
+    }
     println!("Got syscall {}", syscall_num);
     println!(
         "SYSCALLS PC is {:x} CURRENT_PC is {:x}",
         pc.pc(),
         current_pc(_cpu)
     );
+    read_2_bytes_at_pc(_cpu, pc.pc());
     let asid = current_asid(_cpu);
     let cpc = current_pc(_cpu);
     read_2_bytes_at_pc(_cpu, pc.pc());
     read_2_bytes_at_pc(_cpu, cpc);
 
     let file_data = ELF_TO_INJECT.get().unwrap();
+
+    let sys_enter = PppCallback::new();
+    let sys_return = PppCallback::new();
+
     run_injector(pc, async move {
+        // unsafe {
+        // qemu_loglevel |= 1 << 1;
+        // }
         let cpu = unsafe { &mut *get_cpu() };
-        println!("current asid: {:x}", current_asid(cpu));
-        if ARGS.require_root {
-            let is_root = syscall(GETPID, ()).await == 0 as target_ulong;
-            if !is_root {
-                SYSCALLS.add_callback_on_all_sys_enter(on_sys_enter);
-                return;
-            } else {
-                println!("Got root!");
-            }
-        }
+        // println!("current asid: {:x}", current_asid(cpu));
+        // if ARGS.require_root {
+        //     let is_root = syscall(GETPID, ()).await == 0 as target_ulong;
+        //     if !is_root {
+
+        //         SYSCALLS.add_callback_on_all_sys_enter(on_sys_enter);
+        //         return;
+        //     } else {
+        //         println!("Got root!");
+
+        //     }
+        // }
 
         println!("In injector");
         println!("current asid: {:x}", current_asid(cpu));
+
+        // let dupfd = syscall(DUP, (0u64,)).await;
+        // let dupfd = syscall(DUP2, (0u64, 10u64)).await;
+        // println!("got new FD {}", dupfd);
+        // let round_address = 0x7fffffffe000u64;
+        // let not_round = 0x00007fffffffeaa8u64;
+        // let mlock_ret = syscall(MLOCK, (round_address, u64::MAX)).await;
+        // println!("mlock_ret: {}", mlock_ret as i64);
         // only problematic in multi-cpu systems
         // mmap a region
         let mmap_addr = do_mmap().await;
@@ -140,8 +173,11 @@ fn on_sys_enter(_cpu: &mut CPUState, pc: SyscallPc, syscall_num: target_ulong) {
         println!("current asid: {:x}", current_asid(cpu));
         // zero it out
         // virtual_memory_write(cpu, mmap_addr, &[0u8; PAGE_SIZE as usize]);
+        // let mmap_addr = panda::current_sp(cpu) + 0x1000;
+        // virtual_memory_write(cpu, mmap_addr, &[0u8; 20]);
         let mem_fd = do_memfd_create(mmap_addr).await;
         println!("Got fd {:#x}", mem_fd);
+        println!("{}", mmap_addr as i64);
         println!("current asid: {:x}", current_asid(cpu));
         loop {
             match ELF_READ_POS.fetch_add(PAGE_SIZE as usize, Ordering::SeqCst) {
@@ -167,41 +203,78 @@ fn on_sys_enter(_cpu: &mut CPUState, pc: SyscallPc, syscall_num: target_ulong) {
             }
         }
         println!("forking...");
+        panda::hook::start_block_exec(move |cpu, _, hook| {
+            println!("RAX: {:x}", panda::regs::get_reg(cpu, RAX));
+        })
+        .at_addr(pc.pc() + 2);
 
-        let is_parent = do_fork().await == 0 as target_ulong;
-        println!("is_parent is {}", is_parent);
+        // let pid = syscall(GETPID, ()).await;
+        // println!("PID is {}", pid);
 
-        let execbuf = [
-            47u8,
-            112,
-            114,
-            111,
-            99,
-            47,
-            115,
-            101,
-            108,
-            102,
-            47,
-            102,
-            100,
-            47,
-            48 + mem_fd as u8,
-            0,
-        ];
-        virtual_memory_write(cpu, mmap_addr, &execbuf);
-        println!("execve()");
-        let end_mmap_buf =
-            mmap_addr as target_ulong + (execbuf.len() - 1) as target_ulong;
-        do_execve(mmap_addr, end_mmap_buf, end_mmap_buf).await;
-        println!("finished");
+        let fork_ret = do_fork().await;
+        println!("fork ret {:x}", fork_ret);
+
+        // let is_parent = fork_ret != 0 as target_ulong;
+        // println!("is_parent is {}", is_parent);
+
+        // // /proc/self/fd/#
+
+        // let execbuf = [
+        //     47u8,
+        //     112,
+        //     114,
+        //     111,
+        //     99,
+        //     47,
+        //     115,
+        //     101,
+        //     108,
+        //     102,
+        //     47,
+        //     102,
+        //     100,
+        //     47,
+        //     48 + mem_fd as u8,
+        //     0,
+        // ];
+        // virtual_memory_write(cpu, mmap_addr, &execbuf);
+        // println!("execve()");
+        // let end_mmap_buf =
+        //     mmap_addr as target_ulong + (execbuf.len() - 1) as target_ulong;
+        // do_execve(mmap_addr, end_mmap_buf, end_mmap_buf).await;
+        // println!("finished");
+        // // // unsafe {
+        // //     qemu_loglevel = 0;
+        // }
     });
-    panda::hook::before_block_exec(move |cpu, _, hook| {
-        if asid == current_asid(cpu) {
-            println!("Hit PC for SYSCALL {:x}", pc.pc());
-        }
-    })
-    .at_addr(pc.pc());
+
+    // let r = unsafe { _cpu.env_ptr as *mut CPUX86State };
+    // let HF_CS64_SHIFT: u64 = 15;
+    // let HF_CS64_MASK = 1 << HF_CS64_SHIFT;
+
+    // let syscall_addr = unsafe {
+    //     // if HF_CS64_MASK & (*r).eflags != 0 {
+    //     // println!("lstar");
+    //     (*r).lstar
+    //     // } else {
+    //     //     println!("cstar");
+    //     //     (*r).cstar
+    //     // }
+    // };
+
+    // panda::hook::before_block_exec(move |cpu, _, hook| {
+    //     if asid == current_asid(cpu) {
+    //         println!("Hit PC for LSTAR {:x}", pc.pc());
+    //         println!("RAX: {:x}", panda::regs::get_reg(cpu, RAX));
+    //         println!("RDI: {:x}", panda::regs::get_reg(cpu, RDI));
+    //         println!("RSI: {:x}", panda::regs::get_reg(cpu, RSI));
+    //         println!("RDX: {:x}", panda::regs::get_reg(cpu, RDX));
+    //         println!("RCX: {:x}", panda::regs::get_reg(cpu, RCX));
+    //         println!("R8: {:x}", panda::regs::get_reg(cpu, R8));
+    //         println!("R9: {:x}", panda::regs::get_reg(cpu, R9));
+    //     }
+    // })
+    // .at_addr(syscall_addr);
     SYSCALLS.remove_callback_on_all_sys_enter(on_sys_enter);
 }
 
