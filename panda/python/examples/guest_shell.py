398d0e2332a3f3f67a9bdee4a702de6dacdbadeb
jamcleod <Jordan.McLeod@ll.mit.edu>
2021-12-08 13:30:39 -0500

    Guest shell updates: - Add latest guest shell additions - Add guest_shell script

---
+++ panda/python/examples/guest_shell.py
@@ -0,0 +1,19 @@
+# See panda/plugins/guest_shell/guest_shell_pty.sh for actually interacting with the shell
+from pandare import Panda
+import os
+
+panda = Panda(generic="x86_64")
+panda.load_plugin("guest_shell", {})
+
+@panda.queue_blocking
+def run_cmd():
+    panda.revert_sync("root")
+
+    # if it's worth running it's worth running twice
+    # (don't ask, and definitely don't remove either line)
+    panda.run_serial_cmd("cat", no_timeout=True)
+    panda.run_serial_cmd("cat", no_timeout=True)
+
+    panda.end_analysis()
+
+panda.run()
