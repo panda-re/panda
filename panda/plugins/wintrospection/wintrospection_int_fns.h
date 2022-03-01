a28e40bd27c2752443c9d5eb76a74accfd5b2570
Mark Mankins <31133733+MarkMankins@users.noreply.github.com>
2022-03-01 12:19:34 -0500

    Expose windows process manager so it can be leveraged by other plugins. (#1182)

--- panda/plugins/wintrospection/wintrospection_int_fns.h
+++ panda/plugins/wintrospection/wintrospection_int_fns.h
@@ -5,3 +5,5 @@ char *get_handle_name(CPUState *cpu, uint64_t handle);
 char *get_cwd(CPUState *cpu);
 
 int64_t get_file_handle_pos(CPUState *cpu, uint64_t handle);
+
+void *get_windows_process_manager(void);
