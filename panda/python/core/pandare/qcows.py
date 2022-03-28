92e777289027c6b10f3e319b216b5c7df3bb3129
jamcleod <Jordan.McLeod@ll.mit.edu>
2022-03-28 12:03:18 -0400

    Have qcows.py lazily import argv

--- panda/python/core/pandare/qcows.py
+++ panda/python/core/pandare/qcows.py
@@ -5,7 +5,6 @@ Module for fetching generic PANDA images and managing their metadata.
 
 import logging
 from os import path, remove, makedirs
-from sys import argv
 from subprocess import check_call
 from collections import namedtuple
 from shlex import split as shlex_split
@@ -310,6 +309,8 @@ class Qcows():
         Returns:
             string: Path to qcow
         '''
+        from sys import argv
+
         if len(argv) > idx:
             return Qcows.get_qcow(argv[idx])
         else:
