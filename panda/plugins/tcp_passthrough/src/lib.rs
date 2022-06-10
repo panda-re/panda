cbe4f31f5d8afc5538569bd9686bcdaf55b1fd1a
jamcleod <Jordan.McLeod@ll.mit.edu>
2022-06-10 12:38:35 -0400

    Add TCP passthrough python API prototype

--- panda/plugins/tcp_passthrough/src/lib.rs
+++ panda/plugins/tcp_passthrough/src/lib.rs
@@ -1,6 +1,8 @@
 use once_cell::sync::Lazy;
 use panda::{plugins::guest_plugin_manager::*, prelude::*};
+use std::ffi::CStr;
 use std::net::Ipv4Addr;
+use std::os::raw::c_char;
 use std::sync::Mutex;
 
 mod display;
@@ -9,17 +11,84 @@ mod send_ext;
 mod socket_list;
 
 use send_ext::SendExt;
-use socket_list::on_get_socket_list;
 
 use tcp_shared_types::Request;
 
+/// Request that a table of sockets be printed once guest execution resumes
 #[no_mangle]
 pub extern "C" fn print_socket_info() {
-    on_get_socket_list(display::print_table);
+    socket_list::on_get_socket_list(display::print_table);
 }
 
-fn forward_socket(ip: Ipv4Addr, port: u16, host_port: u16) {
+fn forward_socket_internal(ip: Ipv4Addr, port: u16, host_port: u16) {
     forward_socket::forward(ip, port, host_port);
+}
+
+/// Information about a given socket binding
+#[repr(C)]
+pub struct SocketInfo {
+    pub ip: [u8; 4],
+    pub pid: u64,
+    pub port: u16,
+    pub server: bool,
+}
+
+/// Provide a callback for receiving a socket list that will get called once the guest
+/// resumes execution
+#[no_mangle]
+pub extern "C" fn on_get_socket_list(callback: extern "C" fn(*const SocketInfo, usize)) {
+    socket_list::on_get_socket_list(move |socket_list| {
+        let socket_list: Vec<SocketInfo> = socket_list
+            .into_iter()
+            .map(
+                |tcp_shared_types::SocketInfo {
+                     ip,
+                     port,
+                     pid,
+                     server,
+                 }| {
+                    SocketInfo {
+                        ip: ip.octets(),
+                        port,
+                        pid: pid.unwrap_or(u64::MAX),
+                        server,
+                    }
+                },
+            )
+            .collect();
+
+        callback(socket_list.as_ptr(), socket_list.len());
+
+        drop(socket_list);
+    })
+}
+
+/// Forward a socket from the guest, returning true if no issue is hit. Returns `false` if
+/// the IP address fails to parse. A null IP address is treated as 0.0.0.0
+///
+/// Guest must resume execution for an unspecified amount of time before TCP traffic
+/// will actually be processed.
+#[no_mangle]
+pub unsafe extern "C" fn forward_socket(
+    ip: *const c_char,
+    guest_port: u16,
+    host_port: u16,
+) -> bool {
+    let ip = if ip.is_null() {
+        "0.0.0.0".parse().unwrap()
+    } else {
+        let ip = CStr::from_ptr(ip);
+
+        if let Ok(ip) = ip.to_str().unwrap().parse() {
+            ip
+        } else {
+            return false;
+        }
+    };
+
+    forward_socket_internal(ip, guest_port, host_port);
+
+    true
 }
 
 #[channel_recv]
@@ -57,7 +126,7 @@ fn init(_: &mut PluginHandle) {
     if forward_port != 0 {
         Lazy::force(&MAIN_CHANNEL);
 
-        forward_socket(
+        forward_socket_internal(
             "0.0.0.0".parse().unwrap(),
             forward_port,
             match host_port {
--- panda/plugins/tcp_passthrough/try_it.py
+++ panda/plugins/tcp_passthrough/try_it.py
@@ -2,6 +2,56 @@ from pandare import Panda
 
 panda = Panda(generic="x86_64")
 
+# =====================================================================
+
+def extend_class(cls):
+    return lambda f: (setattr(cls, f.__name__, f) or f)
+
+# =====================================================================
+
+from dataclasses import dataclass
+from ipaddress import IPv4Address
+from typing import Callable
+
+@dataclass
+class SocketInfo:
+    ip: IPv4Address
+    pid: int
+    port: int
+    is_server: bool
+
+    def from_ffi(raw):
+        ip = IPv4Address(bytes([raw.ip[i] for i in range(4)]))
+        pid = raw.pid
+        port = raw.port
+        is_server = raw.server
+
+        return SocketInfo(ip, pid, port, is_server)
+
+@extend_class(Panda)
+def forward_socket(self, guest_port: int, host_port: int):
+    self.plugins["tcp_passthrough"].forward_socket(
+        self.ffi.cast("const char*", self.ffi.NULL),
+        self.ffi.cast("uint16_t", guest_port),
+        self.ffi.cast("uint16_t", host_port)
+    )
+
+@extend_class(Panda)
+def on_get_socket_list(self, callback: Callable[[list], None]):
+    @self.ffi.callback("void(*)(const struct SocketInfo*, uintptr_t)")
+    def cb(ptr, length):
+        callback([SocketInfo.from_ffi(ptr[i]) for i in range(length)])
+
+    self.socket_list = (self.__dict__.get("socket_list") or []) + [cb]
+
+    self.plugins["tcp_passthrough"].on_get_socket_list(cb)
+
+@extend_class(Panda)
+def print_socket_info(self):
+    self.plugins["tcp_passthrough"].print_socket_info()
+
+# =====================================================================
+
 @panda.queue_blocking
 def run_cmd():
     panda.revert_sync("root")
@@ -13,15 +63,21 @@ def run_cmd():
 
     run("sleep 2")
 
-    panda.load_plugin("tcp_passthrough", {
-        "print_sockets": False,
-        "forward_port": 8000,
-        "host_port": 4343
-    })
+    panda.load_plugin("tcp_passthrough")
 
-    print("Before")
-    panda.plugins["tcp_passthrough"].print_socket_info()
-    print("After")
+    # Print a table of the TCP servers in the guest
+    panda.print_socket_info()
+
+    # Forward a socket from localhost:8000 in the guest to localhost:4343 on the host
+    panda.forward_socket(8000, 4343)
+
+    # print out the socket list ourselves too
+    @panda.on_get_socket_list
+    def callback(socket_info: list):
+        print(" Ip Address\tPID\tIs Server?")
+        print("====================================")
+        for socket in socket_info:
+            print(f" {socket.ip}:{socket.port}\t{socket.pid}\t{socket.is_server}")
 
     run("cat")
     panda.end_analysis()
