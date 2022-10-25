169239f73008754f9b1a39ba8b8ec427eaea1934
Luke Craig <lacraig3@gmail.com>
2022-10-25 13:01:29 -0400

    Revert "guest plugins (#1145)" (#1240)
    This reverts commit aee5f52bb20e402a30d69a26a6ca8cdfc6866e47.

--- panda/plugins/guest_shell/guest_shell_pty.sh
+++
@@ -1,1 +0,0 @@
-socat UNIX-CONNECT:/tmp/guest_shell.sock PTY,link=/tmp/guest_shell_pty & screen /tmp/guest_shell_pty
--- panda/plugins/rust_skeleton/src/lib.rs
+++ panda/plugins/rust_skeleton/src/lib.rs
@@ -1,10 +1,16 @@
 use panda::prelude::*;
 
 #[panda::init]
-fn init(_: &mut PluginHandle) {
+fn init(_: &mut PluginHandle) -> bool {
     println!("Initialized!");
+    true
 }
 
-//#[panda::before_block_exec]
-//fn bbe(_cpu: &mut CPUState, _tb: &mut TranslationBlock){
-//}
+#[panda::uninit]
+fn exit(_: &mut PluginHandle) {
+    println!("Exiting");
+}
+
+#[panda::before_block_exec]
+fn bbe(_cpu: &mut CPUState, _tb: &mut TranslationBlock){
+}
