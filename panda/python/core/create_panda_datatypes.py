d3a8cf88101b8f7cef60582696bd199b61c48ef7
Andrew Fasano <fasano@mit.edu>
2022-11-03 14:13:41 -0400

    CI: detect failures pypanda setup multi-processing

--- panda/python/core/create_panda_datatypes.py
+++ panda/python/core/create_panda_datatypes.py
@@ -594,7 +594,33 @@ def get_cbs(ffi):
             ("mips64",64),
         ]
 
-        from multiprocessing import Process
+        from __future__ import print_function
+        import multiprocessing as mp
+        import traceback
+
+        # https://stackoverflow.com/a/33599967
+        class Process(mp.Process):
+            def __init__(self, *args, **kwargs):
+                mp.Process.__init__(self, *args, **kwargs)
+                self._pconn, self._cconn = mp.Pipe()
+                self._exception = None
+
+            def run(self):
+                try:
+                    mp.Process.run(self)
+                    self._cconn.send(None)
+                except Exception as e:
+                    tb = traceback.format_exc()
+                    self._cconn.send((e, tb))
+                    # raise e  # You can still rise this exception if you need to
+
+            @property
+            def exception(self):
+                if self._pconn.poll():
+                    self._exception = self._pconn.recv()
+                return self._exception
+
+
         ps = (
             Process(target=compile, args=(arch[0], arch[1], pypanda_headers, install, INCLUDE_DIR_PYP))
             for arch in arches
