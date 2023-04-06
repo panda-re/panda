dafc04e76aa0d0675b81f492a2ca793fbeba2b26
ryandmaggio <ryandmaggio@gmail.com>
2023-04-06 16:40:13 +0000

    no real changes here, but it works offline with correct profile

--- panda/plugins/pmemaccess/test_pmem.py
+++ panda/plugins/pmemaccess/test_pmem.py
@@ -4,26 +4,46 @@ from sys import argv
 from pandare import Panda
 panda = Panda(generic="x86_64")
 
-#panda = Panda(arch='i386', qcow="/home/rdm/ubuntu.qcow2", mem="1G",
-#        extra_args=f"-boot c \
-#                     -nographic \
-#                     -no-reboot \
-#                     -serial\
-#                     ")
+# Grabbed from malrec's listwins.py
+import volatility.conf as conf
+import volatility.registry as registry
+registry.PluginImporter()
+config = conf.ConfObject()
+import volatility.commands as commands
+import volatility.addrspace as addrspace
+registry.register_global_options(config, commands.Command)
+registry.register_global_options(config, addrspace.BaseAddressSpace)
+config.parse_options()
 
-#panda = Panda(arch='x86_64', qcow="/home/rdm/.panda/bionic-server-cloudimg-amd64-noaslr-nokaslr.qcow2", mem="1G",
-#        extra_args=f"-nographic")
+# Adapted from the `volatility.plugins.gui.windows` import
+import volatility.plugins.linux.pslist as pslist
+
+def try_pslist():
+    global config
+    print("Doing pslist maybe?")
+    psl_plug = pslist.linux_pslist(config)
+    for task in psl_plug.calculate():
+        print(task)
+
+def set_up(prof, sock):
+    global config
+    config.PROFILE = prof
+    config.LOCATION = sock
+    try_pslist()
 
 @panda.queue_blocking
 def run_cmd():
-    global name
+    global name, profile, path
     panda.revert_sync("root")
-    #print(panda.run_serial_cmd("cat /proc/kallsyms | grep access"))
-    panda.run_serial_cmd("sleep 50")
+    # hacky because we need to sleep for a while but doing it as one sleep made panda sad (dead)
+    panda.run_serial_cmd("sleep 10")
     #panda.run_serial_cmd("sleep 10")
     #panda.run_serial_cmd("sleep 10")
     #panda.run_serial_cmd("sleep 10")
     #panda.run_serial_cmd("sleep 10")
+    print("[PANDA] " + panda.run_serial_cmd("lsb_release -a") + " [PANDA]\n")
+    print("[PANDA] " + panda.run_serial_cmd("uname -a") + " [PANDA]\n")
+    set_up(profile, path)
     print("Done sleep, ending")
     panda.end_analysis()
 
@@ -35,10 +55,13 @@ def asidchange(cpu, old_asid, new_asid):
     #panda.disable_callback("asidchange")
     return 0
 
-print("Loading pmemaccess")
-#panda.load_plugin("pmemaccess", args = {"path":"/home/rdm/pmem_sock2", "mode": 2})
+path = "/home/rdm/pmem_sock3"
+profile = "LinuxUbuntu_4_15_0-208-generic_profilex64"
 
-panda.load_plugin("pmemaccess", args = {"path":"/home/rdm/pmem_sock2", "mode": 1, "profile":"LinuxUbuntu18043x64", "command":"linux_pslist"})
+#print("Loading pmemaccess")
+#panda.load_plugin("pmemaccess", args = {"path":path, "mode": 2})
+
+#panda.load_plugin("pmemaccess", args = {"path":path, "mode": 1, "profile":profile, "command":"linux_pslist"})
 
 panda.enable_precise_pc()
 panda.disable_tb_chaining()
