fb18797ccaec6cbda770bb04fd0cc5dc5d3ae9c5
Jacques Becker <jacques.becker20@gmail.com>
2023-05-10 12:00:38 -0400

    removed unnecessary dependencies and unused code

--- panda/plugins/keyfind/keychecker.py
+++ panda/plugins/keyfind/keychecker.py
@@ -9,12 +9,14 @@ import pyshark
 client_random = None
 
 
-SERVER_HANDSHAKE_SECRET = 0
-SERVER_TRAFFIC_SECRET_0 = 0
-CLIENT_HANDSHAKE_SECRET = 0
-CLIENT_TRAFFIC_SECRET_0 = 0
+SERVER_HANDSHAKE_SECRET = None
+SERVER_TRAFFIC_SECRET_0 = None
+CLIENT_HANDSHAKE_SECRET = None
+CLIENT_TRAFFIC_SECRET_0 = None
 
 pcap_filename = sys.argv[1]
+key_candidates_filename = sys.argv[2]
+
 key_candidates = []
 names = ["SERVER_HANDSHAKE_TRAFFIC_SECRET", "SERVER_TRAFFIC_SECRET_0", "CLIENT_HANDSHAKE_TRAFFIC_SECRET", "CLIENT_TRAFFIC_SECRET_0"]
 
@@ -75,13 +77,15 @@ def pyshark_find_server_traffic_secret():
 
     count = 0
     for packet in capture:
-        if 'tls' in packet:# and packet['tls'].has_field("record"):
-            pass
-            #print(packet)
 
-        if 'tls' in packet and packet.highest_layer != "TLS" and packet['tcp'].srcport.hex_value == 443:
-            capture.close()
-            return True
+        #packets sent from the server have a high dest port and 443 as source port
+        #all encrypted packets have an opaque content type "23", the decrypted packets also have a content type, also "23" when the content is application data
+        #see RFC 8446 pg. 80-81, 122
+        if 'tls' in packet and packet['tcp'].srcport.hex_value == 443:
+            if packet['tls'].has_field("record_opaque_type") and packet['tls'].has_field("record_content_type"):
+                if packet['tls'].get_field_value("record_opaque_type") == "23" and packet['tls'].get_field_value("record_content_type") == "23":
+                    capture.close()
+                    return True
 
 
     capture.close()
@@ -97,9 +101,13 @@ def pyshark_find_client_traffic_secret():
     for packet in capture:
 
         #packets sent from the client have a high source port and 443 as dest port
-        if 'tls' in packet and packet.highest_layer != "TLS" and packet['tcp'].dstport.hex_value == 443:
-            capture.close()
-            return True
+        #all encrypted packets have an opaque content type "23", the decrypted packets also have a content type, also "23" when the content is application data
+        #see RFC 8446 pg. 80-81, 122
+        if 'tls' in packet and packet['tcp'].dstport.hex_value == 443:
+            if packet['tls'].has_field("record_opaque_type") and packet['tls'].has_field("record_content_type"):
+                if packet['tls'].get_field_value("record_opaque_type") == "23" and packet['tls'].get_field_value("record_content_type") == "23":
+                    capture.close()
+                    return True
 
 
     capture.close()
@@ -125,19 +133,6 @@ def pyshark_find_server_handshake_secret():
 
 
 def find_server_handshake_key():
-    #for each pair in heap_pairs.txt  
-    #set first buf = client_app_traffic_secret, second buf = server_app_traffic_secret
-        #check against every entry in heap_writes.txt, set to server_handshake_traffic_secret
-        #write to a tmp file
-        #get number of decrypted packets
-    
-    #sanity check, there should only be one correct answer
-
-    #names = ["CLIENT_TRAFFIC_SECRET_0", "SERVER_TRAFFIC_SECRET_0", "SERVER_HANDSHAKE_TRAFFIC_SECRET", "CLIENT_HANDSHAKE_TRAFFIC_SECRET"]
-
-#    f = open("/home/becker/panda_ssl/key_candidates.txt", "r")
-#    heap_writes = f.read().strip().split("\n")
-#    f.close()
 
     file_data = None
 
@@ -150,12 +145,7 @@ def find_server_handshake_key():
         f.write(file_data)
         f.close()
 
-        #print(f"trying server_handshake_secret: {server_handshake_secret}")
-        #print(f"trying client_traffic_secret: {client_traffic_secret}")
-        #print(f"trying server_traffic_secret: {server_traffic_secret}")
-
         if pyshark_find_server_handshake_secret():
-            #print(f"found server handshake secret: {server_handshake_secret}")
             return server_handshake_secret
 
         count += 1
@@ -165,22 +155,7 @@ def find_server_handshake_key():
 
 
 def find_client_handshake_key():
-    #names = ["CLIENT_TRAFFIC_SECRET_0", "SERVER_TRAFFIC_SECRET_0", "SERVER_HANDSHAKE_TRAFFIC_SECRET", "CLIENT_HANDSHAKE_TRAFFIC_SECRET"]
-
-#    f = open("/home/becker/panda_ssl/heap_pairs.txt", "r")
-#    all_pairs = f.read().strip().split("\n")
-#    f.close()
-#
-#    f = open("/home/becker/panda_ssl/heap_writes.txt", "r")
-#    heap_writes = f.read().strip().split("\n")
-#    f.close()
-
     file_data = None
-
-#    pair = all_pairs[0]
-#    p = pair.split(":")
-#    client_traffic_secret = p[0]
-#    server_traffic_secret = p[1]
 
 
     print(f"\nsearching for client handshake key...")
@@ -191,10 +166,6 @@ def find_client_handshake_key():
         f = open("test_keyfile.txt", "w")
         f.write(file_data)
         f.close()
-
-        #print(f"trying server_handshake_secret: {client_handshake_secret}")
-        #print(f"trying client_traffic_secret: {client_traffic_secret}")
-        #print(f"trying server_traffic_secret: {server_traffic_secret}")
 
         if pyshark_find_client_handshake_secret():
             return client_handshake_secret
@@ -207,19 +178,9 @@ def find_client_handshake_key():
 
 
 def find_client_traffic_key():
-    #names = ["CLIENT_TRAFFIC_SECRET_0", "SERVER_TRAFFIC_SECRET_0", "SERVER_HANDSHAKE_TRAFFIC_SECRET", "CLIENT_HANDSHAKE_TRAFFIC_SECRET"]
-
-#    f = open("/home/becker/panda_ssl/heap_pairs.txt", "r")
-#    all_pairs = f.read().strip().split("\n")
-#    f.close()
-
-#    f = open("/home/becker/panda_ssl/heap_writes.txt", "r")
-#    heap_writes = f.read().strip().split("\n")
-#    f.close()
-
     file_data = None
-    ctk = ""
-    stk = ""
+    ctk = None
+    stk = None
 
     print("\nsearching for client traffic key...")
     count = 0
@@ -229,10 +190,6 @@ def find_client_traffic_key():
         f = open("test_keyfile.txt", "w")
         f.write(file_data)
         f.close()
-
-        #print(f"trying server_handshake_secret: {SERVER_HANDSHAKE_SECRET}")
-        #print(f"trying client_handshake_secret: {CLIENT_HANDSHAKE_SECRET}")
-        #print(f"trying client_traffic_secret: {client_traffic_key}")
 
         if pyshark_find_client_traffic_secret():
             print(f"found client traffic secret: {client_traffic_key}")
@@ -253,10 +210,6 @@ def find_client_traffic_key():
         f.write(file_data)
         f.close()
 
-        #print(f"trying server_handshake_secret: {SERVER_HANDSHAKE_SECRET}")
-        #print(f"trying client_handshake_secret: {CLIENT_HANDSHAKE_SECRET}")
-        #print(f"trying server_traffic_secret: {server_traffic_key}")
-
         if pyshark_find_server_traffic_secret():
             print(f"found server traffic secret: {server_traffic_key}")
             stk = server_traffic_key
@@ -271,7 +224,7 @@ def find_client_traffic_key():
 
 
 
-f = open("/home/becker/panda_ssl/key_candidates.txt", "r")
+f = open(key_candidates_filename, "r")
 key_candidates = f.read().strip().split("\n")
 f.close()
 
@@ -282,21 +235,21 @@ if not get_client_random():
     exit(1)
 
 SERVER_HANDSHAKE_SECRET = find_server_handshake_key()
-if SERVER_HANDSHAKE_SECRET == 0:
+if SERVER_HANDSHAKE_SECRET == None:
     print("failed to find server handshake secret")
     exit(1)
 print(f"found server handshake secret: {SERVER_HANDSHAKE_SECRET}")
 
 
 CLIENT_HANDSHAKE_SECRET = find_client_handshake_key()
-if CLIENT_HANDSHAKE_SECRET == 0:
+if CLIENT_HANDSHAKE_SECRET == None: 
     print("failed to find client handshake secret")
     exit(1)
 print(f"found client handshake secret: {CLIENT_HANDSHAKE_SECRET}")
 
 
 CLIENT_TRAFFIC_SECRET_0, SERVER_TRAFFIC_SECRET_0 = find_client_traffic_key()
-if CLIENT_TRAFFIC_SECRET_0 == 0 or SERVER_TRAFFIC_SECRET_0 == 0:
+if CLIENT_TRAFFIC_SECRET_0 == None or SERVER_TRAFFIC_SECRET_0 == None:
     print(f"failed to find client/server traffic secrets")
     exit(1)
 
--- panda/plugins/keyfind/keyfind.cpp
+++ panda/plugins/keyfind/keyfind.cpp
@@ -17,24 +17,9 @@ PANDAENDCOMMENT */
 
 #include "panda/plugin.h"
 
-#include "callstack_instr/callstack_instr.h"
-#include "callstack_instr/callstack_instr_ext.h"
-
-#include "keyfind.h"
-
-
-#include "osi/osi_types.h"
-#include "osi/osi_ext.h"
-
 #include <iostream>
-#include <unordered_set>
 #include <vector>
-#include <set>
-#include <map>
-#include <utility>
 #include <algorithm>
-#include <time.h>
-#include <glib.h>
 #include <math.h>
 #include <stdio.h>
 
@@ -49,7 +34,6 @@ PANDAENDCOMMENT */
 // QEMU/PANDA, which is written in C
 extern "C" {
 
-//int count = 0;
 //time_t start, current;
 
 bool init_plugin(void *);
@@ -58,10 +42,6 @@ int mem_write_callback(CPUState *env, target_ulong pc, target_ulong addr, target
 
 }
 
-//bool in_openssl;
-
-
-//int keysize = 48;
 
 typedef struct Memchunk {
     target_ptr_t start;
@@ -71,64 +51,36 @@ typedef struct Memchunk {
 } Memchunk;
 
 
-std::vector<std::pair<Memchunk, double> > heap_segments;
 
-std::vector<std::pair<Memchunk, double> > deduplicated_heap;
-
-Memchunk last_write;
-//typedef struct cand_prog_point_struct {
-
-
-// Utility functions
-//unsigned char hexchar_to_int(int c)
-//{
-//    if (c >= 0x30 && c < 0x40) return c - 0x30;
-//    else if (c >= 0x41 && c < 0x5B) return c - 0x37;
-//    else return 0;
-//}
-
-//void read_hex_string(std::string in, unsigned char *out)
-//{
-//    unsigned char *ptr = out;
-//    for(unsigned int i = 0; i < in.length(); i += 2) {
-//        int high = toupper(in[i]);
-//        int low = toupper(in[i+1]);
-//        *ptr++ = (hexchar_to_int(high) << 4) | hexchar_to_int(low);
-//    }
-//}
 
 // Globals
-StringInfo g_keydata;
-StringInfo g_master_secret;
-StringInfo g_out;
-StringInfo g_client_random;
-StringInfo g_server_random;
-StringInfo g_version;
-StringInfo g_content_type;
-StringInfo g_enc_msg;
-const EVP_CIPHER *g_ciph = NULL;
-const EVP_MD *g_md = NULL;
 
-bool have_candidates = true;
-bool seen_first_byte = false;
-int count = 0;
-int writes_interval = 0;
+Memchunk last_write;
+
+std::vector<std::pair<Memchunk, double> > memory_segments;
 
 uint16_t ciphersuite_id = 0;
 uint8_t keysize = 0;
 double entropy_threshold = 0.0;
 
-typedef std::tuple<target_ulong, target_ulong, target_ulong> candidate_prog_point;
-std::set <candidate_prog_point> candidates;
-
-// Optimization
-//std::unordered_set <target_ulong> asids;
-//std::vector <target_ulong> eips;
 
 
-typedef std::tuple <target_ulong, target_ulong, std::string> match;
-std::set<match> matches;
-//std::map<prog_point,key_buf> key_tracker;
+//helper function for sorting by buffer value
+bool buffer_compare(std::pair<Memchunk, double> &a, std::pair<Memchunk, double> &b) {
+    return (memcmp(a.first.buf, b.first.buf, keysize) < 0);
+}
+
+//helper function for sorting by highest entropy
+bool entropy_compare(std::pair<Memchunk, double> &a, std::pair<Memchunk, double> &b) {
+    return a.second > b.second;
+}
+
+//heper function for checking equality of memchunks by buffer value
+bool memchunk_compare(std::pair<Memchunk, double> &a, std::pair<Memchunk, double> &b) {
+    return (memcmp(a.first.buf, b.first.buf, keysize) == 0);
+}
+
+
 
 int handle_IP (u_char *args,const struct pcap_pkthdr* pkthdr,const u_char* packet) {
     const struct iphdr* ip;
@@ -172,6 +124,7 @@ int handle_IP (u_char *args,const struct pcap_pkthdr* pkthdr,const u_char* packe
     if (packet[tls_start_idx] == 22) {
         int record_data_idx = tls_start_idx + 5;
 
+        // 2 is server handshake
         if (packet[record_data_idx] == 2) {
 
             int ciphersuite_idx = record_data_idx + 71;
@@ -240,20 +193,6 @@ uint16_t get_ciphersuite_id(const char* pcap_file) {
     return 0;
 }
 
-//helper function for sorting by buffer value
-bool buffer_compare(std::pair<Memchunk, double> &a, std::pair<Memchunk, double> &b) {
-    return (memcmp(a.first.buf, b.first.buf, keysize) < 0);
-}
-
-//helper function for sorting by highest entropy
-bool entropy_compare(std::pair<Memchunk, double> &a, std::pair<Memchunk, double> &b) {
-    return a.second > b.second;
-}
-
-//heper function for checking equality of memchunks by buffer value
-bool memchunk_compare(std::pair<Memchunk, double> &a, std::pair<Memchunk, double> &b) {
-    return (memcmp(a.first.buf, b.first.buf, keysize) == 0);
-}
 
 double shannon_entropy(uint8_t* buf, size_t size) {
     uint8_t frequencies[256] = {0};
@@ -272,52 +211,12 @@ double shannon_entropy(uint8_t* buf, size_t size) {
     return entropy;
 }
 
-//void print_memchunk(Memchunk* m) {
-//    printf("\tstart: " TARGET_PTR_FMT "\n", m->start);
-//    printf("\tend  : " TARGET_PTR_FMT "\n", m->end);
-//    printf("\tsize : " TARGET_FMT_ld "\n", m->size);
-//    printf("\tbuf  : ");
-//
-//    for(int i = 0; i < keysize; i++) {
-//        printf("%02x", m->buf[i]);
-//    }
-//    printf("\n");
-//}
-
-
-void get_heap_bounds(CPUState* env, target_ptr_t* start, target_ptr_t* end) {
-    OsiProc *current;
-    current = get_current_process(env);
-
-    GArray *ms = NULL;
-    ms = get_mappings(env, current);
-    if (ms != NULL) {
-        for (uint32_t j = 0; j < ms->len; j++) {
-            OsiModule *m = &g_array_index(ms, OsiModule, j);
-            if(strcmp(m->name, "[heap]") == 0) {
-                *start = m->base;
-                *end = m->base + m->size;
-                //printf("\t" TARGET_PTR_FMT " " TARGET_PTR_FMT "  %s:%s\n", m->base, m->base + m->size, m->name, m->file);
-            }
-        }
-        g_array_free(ms, true);
-    }
-
-   
-}
 
 
 // after mem write, that is
 void mem_write_callback(CPUState *env, target_ulong pc, target_ulong addr,
                        size_t size, uint8_t *buf) {
 
-
-//    OsiProc *current;
-//    current = get_current_process(env);
-
-//    if(strcmp(current->name, "openssl") != 0) {
-//        return;
-//    }
 
     //check if the write is happening right after the end of the last write
     //if it's not, reset the last_write
@@ -327,18 +226,10 @@ void mem_write_callback(CPUState *env, target_ulong pc, target_ulong addr,
         if(last_write.size == keysize) {
             //store in vec, reset last_write
             double e = shannon_entropy(last_write.buf, keysize);
-            target_ptr_t heap_start;
-            target_ptr_t heap_end;
-            get_heap_bounds(env, &heap_start, &heap_end);
 
-            //if(e >= ENTROPY_THRESHOLD_48 && last_write.start < 0x0000000000c22000 && last_write.start >= 0x0000000000b95000) {
-            if(e >= entropy_threshold /*&& last_write.start < heap_end && last_write.start >= heap_start*/) {
-            //if(e >= 5.0 && last_write.start < (target_ptr_t) 0x00007ffffffff000 && last_write.start >= (target_ptr_t) 0x00007ffffffde000) {
-                heap_segments.push_back(std::make_pair(last_write, e));
-            } else if (e >= entropy_threshold) {
-                //non_heap_segments.push_back(std::make_pair(last_write, e));
-            }
-
+            if(e >= entropy_threshold) {
+                memory_segments.push_back(std::make_pair(last_write, e));
+            } 
         }
 
 
@@ -349,7 +240,7 @@ void mem_write_callback(CPUState *env, target_ulong pc, target_ulong addr,
         last_write.size = size;
 
     //if it is, add it to the last_write chunk
-    } else if (addr == last_write.end /*&& last_write.size < keysize*/) { 
+    } else if (addr == last_write.end) { 
         last_write.end += size;
 
         //no need to keep track of anything written if the amount of data exceeds keysize bytes
@@ -358,14 +249,8 @@ void mem_write_callback(CPUState *env, target_ulong pc, target_ulong addr,
         }
 
         last_write.size += size;
-
     }
-
-
 }
-
-
-
 
 
 
@@ -374,29 +259,16 @@ bool init_plugin(void *self) {
     // General PANDA stuff
     panda_cb pcb;
 
-    panda_require("osi");
-
-    // this sets up OS introspection API
-    assert(init_osi_api());
-
-
-
     printf("Initializing plugin keyfind\n");
-
-    //in_openssl = true;
 
     // Enable our callbacks
     panda_enable_memcb();
     panda_enable_precise_pc();
-    //enabled_memcb = true;
 
     printf("enabling mem write callback\n");
     pcb.virt_mem_after_write = mem_write_callback;
     panda_register_callback(self, PANDA_CB_VIRT_MEM_AFTER_WRITE, pcb);
 
-
-//    pcb.asid_changed = asid_changed_cb;
-//    panda_register_callback(self, PANDA_CB_ASID_CHANGED, pcb);
 
     //get the pcap name
     panda_arg_list *args = panda_get_args("keyfind");
@@ -409,12 +281,12 @@ bool init_plugin(void *self) {
         exit(1);
     }
 
-    //if it's AES_256_GCM_SHA384
+    //if it's AES_256_GCM_SHA384 (1302)
     if (ciphersuite_id == 0x1302) {                     
         keysize = 48;
         entropy_threshold = 5.0044;
 
-    //if it's CHACHA20_POLY1305_SHA256 or AES_128_GCM_SHA256
+    //if it's AES_128_GCM_SHA256 (1301) or CHACHA20_POLY1305_SHA256 (1303)
     } else if (ciphersuite_id == 0x1301 || ciphersuite_id == 0x1303) {
         keysize = 32;
         entropy_threshold = 4.394;
@@ -426,24 +298,26 @@ bool init_plugin(void *self) {
 
 void uninit_plugin(void *self) {
 
-    printf("collected %ld heap_segments:\n", heap_segments.size());
+    printf("collected %ld memory_segments:\n", memory_segments.size());
 
-    printf("deduplicating the heap segments...\n");
-    std::sort(heap_segments.begin(), heap_segments.end(), buffer_compare);
-    heap_segments.erase(std::unique(heap_segments.begin(), heap_segments.end(), memchunk_compare), heap_segments.end());
-    printf("there are %ld heap segments after deduplication\n", heap_segments.size());
+    printf("deduplicating the memory segments...\n");
+
+    //use of std::unique requires that the segments are sorted by value
+    std::sort(memory_segments.begin(), memory_segments.end(), buffer_compare);
+    memory_segments.erase(std::unique(memory_segments.begin(), memory_segments.end(), memchunk_compare), memory_segments.end());
+    printf("there are %ld memory segments after deduplication\n", memory_segments.size());
 
 
-    printf("sorting heap_segments by entropy...\n");
-    std::sort(heap_segments.begin(), heap_segments.end(), entropy_compare);
+    printf("sorting memory segments by entropy...\n");
+    std::sort(memory_segments.begin(), memory_segments.end(), entropy_compare);
     
 
-    printf("writing heap writes to key_candidates.txt\n");
+    printf("writing memory segments to key_candidates.txt\n");
     FILE *fptr;
     fptr = fopen("key_candidates.txt", "w");
-    for(int i = 0; i < heap_segments.size(); i++) {
+    for(int i = 0; i < memory_segments.size(); i++) {
         for(int j = 0; j < keysize; j++) {
-            fprintf(fptr, "%02x", heap_segments[i].first.buf[j]);
+            fprintf(fptr, "%02x", memory_segments[i].first.buf[j]);
         }
         fprintf(fptr, "\n");
     }
