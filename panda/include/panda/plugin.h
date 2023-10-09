b8a9bf3d4166d2ea571d6f0afd927676b2596288
Mark Mankins <Mark.Mankins@ngc.com>
2023-10-09 11:20:52 -0400

    Export symbols to subsequently opened plugins...
    if magic symbol PANDA_EXPORT_SYMBOLS_plugin_name is present in
    the plugin being loaded.

--- panda/include/panda/plugin.h
+++ panda/include/panda/plugin.h
@@ -67,9 +67,10 @@ void panda_disable_plugin(void *plugin);
 
 // Structure to store metadata about a plugin
 typedef struct panda_plugin {
-    char name[256];     // Currently basename(filename)
-    void *plugin;       // Handle to the plugin (for use with dlsym())
-    bool unload;        // When true, unload plugin when safe
+    char name[256];        // Currently basename(filename)
+    void *plugin;          // Handle to the plugin (for use with dlsym())
+    bool unload;           // When true, unload plugin when safe
+    bool exported_symbols; // True if plugin dlopened with RTLD_GLOBAL
 } panda_plugin;
 
 
