a82d1a217f0e0c2d827f42a409ef334e00a23f9d
Andrew Fasano <fasano@mit.edu>
2023-10-14 16:20:45 -0400

    vhost-vsock: add vhost-vsock-common abstraction
    Manual re-implementation of upstream
    003613dd36a7c1bc8fcd16ebf84a2b55299b81f6. I think it should be
    identical, but I actually went through all the changes and made sure
    they made sense this time.
    
    Could probably be replaced with the cherry-picked upstream commit.
    
    Builds

--- include/hw/virtio/vhost-vsock.h
+++ include/hw/virtio/vhost-vsock.h
@@ -14,8 +14,8 @@
 #ifndef _QEMU_VHOST_VSOCK_H
 #define _QEMU_VHOST_VSOCK_H
 
-#include "hw/virtio/virtio.h"
-#include "hw/virtio/vhost.h"
+#include "hw/virtio/vhost-vsock-common.h"
+
 
 #define TYPE_VHOST_VSOCK "vhost-vsock-device"
 #define VHOST_VSOCK(obj) \
@@ -28,14 +28,8 @@ typedef struct {
 
 typedef struct {
     /*< private >*/
-    VirtIODevice parent;
+    VHostVSockCommon parent;
     VHostVSockConf conf;
-    struct vhost_virtqueue vhost_vqs[2];
-    struct vhost_dev vhost_dev;
-    VirtQueue *event_vq;
-    VirtQueue *recv_vq;
-    VirtQueue *trans_vq;
-    QEMUTimer *post_load_timer;
 
     /*< public >*/
 } VHostVSock;
