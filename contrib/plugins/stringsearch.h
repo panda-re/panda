a19e94e334a82581a7a669c8e825461ba3ffa385
Andrew Fasano <fasano@mit.edu>
2023-10-19 11:27:45 -0400

    Add missing stringsearch.h

---
+++ contrib/plugins/stringsearch.h
@@ -0,0 +1,8 @@
+#ifndef STRINGSEARCH_H
+#define STRINGSEARCH_H
+
+QPP_FUN_PROTOTYPE(stringsearch, bool, stringsearch_remove_string, const char* arg_str);
+QPP_FUN_PROTOTYPE(stringsearch, void, stringsearch_reset_strings, void);
+QPP_FUN_PROTOTYPE(stringsearch, bool, stringsearch_add_string, const char* arg_str);
+
+#endif /* STRINGSEARCH_H */
