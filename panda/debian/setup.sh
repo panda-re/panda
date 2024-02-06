51659bd89e509dff65cfff69e92e9194bd8b933b
Andrew <afq2101@columbia.edu>
2024-02-06 14:27:04 -0500

    Creating a pypanda wheel file (#1435)
    * Adding Brendan's first patch, and correcting the default path on build.sh and updating requirements.txt to align with setup.py
    
    * Can successfully get the pandare package. Although I should note, apparently using setup.py is outdated, but we can worry about this another time
    
    * Might as well have both README and requirements.txt match setup.py exactly too to avoid confusion
    
    * Missed a quote, but confirmed on my Linux VM, that running setup.sh will give me both debian and pip package
    
    * Seems there is a reason why the prefix is like this.

--- panda/debian/setup.sh
+++ panda/debian/setup.sh
@@ -43,10 +43,16 @@ if [[ ! -f "../dependencies/ubuntu_${version}_base.txt" ]]; then
 fi
 
 # First build main panda container for the target ubuntu version
-DOCKER_BUILDKIT=1 docker build --target panda -t panda --build-arg BASE_IMAGE="ubuntu:${version}" ../.. 
+DOCKER_BUILDKIT=1 docker build --target panda -t panda --build-arg BASE_IMAGE="ubuntu:${version}" ../..
+
+# Also build the installer, since that's where the whl file is built
+DOCKER_BUILDKIT=1 docker build --target installer -t panda_installer --build-arg BASE_IMAGE="ubuntu:${version}" ../..
 
 # Now build the packager container from that
 docker build -t packager .
 
 # Copy deb file out of container to host
 docker run --rm -v $(pwd):/out packager bash -c "cp /pandare.deb /out"
+
+# Copy whl file out of container to host, this also preserves wheel name, which is important as pip install WILL fail if you arbitarily change the generated wheel file name
+docker run --rm -v $(pwd):/out panda_installer bash -c "cp /panda/panda/python/core/dist/*.whl /out"
