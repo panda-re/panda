92c588d2b976bab1cdabb29b977b86731a050b35
hu <huzh@nyu.edu>
2024-02-19 03:38:42 +0000

    update authors list

--- panda/plugins/dwarf2/dwarf2.cpp
+++ panda/plugins/dwarf2/dwarf2.cpp
@@ -1,11 +1,6 @@
 /* PANDABEGINCOMMENT
  *
  * Authors:
- *  Tim Leek               tleek@ll.mit.edu
- *  Ryan Whelan            rwhelan@ll.mit.edu
- *  Joshua Hodosh          josh.hodosh@ll.mit.edu
- *  Michael Zhivich        mzhivich@ll.mit.edu
- *  Brendan Dolan-Gavitt   brendandg@gatech.edu
  *  Zhenghao Hu            huzh@nyu.edu
  *
  * This work is licensed under the terms of the GNU GPL, version 2.
