e6ed2321f11bf75e33ec12980326480c2b587fc6
Luke Craig <lacraig3@gmail.com>
2024-03-15 14:57:30 -0400

    Revert "Attempt 2 on publishing wheel (#1467)"
    This reverts commit 4d0ba999e98faf39866247a7ccc2e55c9bdd120f.

--- panda/python/core/setup.py
+++ panda/python/core/setup.py
@@ -8,20 +8,17 @@ from subprocess import check_output
 import shutil
 
 import os
-
 arches = ['arm', 'aarch64', 'i386', 'x86_64', 'ppc', 'mips', 'mipsel', 'mips64']
 
 # Check for PANDA binaries in /usr/local/bin/ or in our build directory
-# panda_binaries = ['/usr/local/bin/panda-system-{arch}' for arch in arches]
+#panda_binaries = ['/usr/local/bin/panda-system-{arch}' for arch in arches]
 # Do we actually care at this point?
 
-root_dir = os.path.join(*[os.path.dirname(__file__), "..", "..", ".."])  # panda-git/ root dir
+root_dir = os.path.join(*[os.path.dirname(__file__), "..", "..", ".."]) # panda-git/ root dir
 
-pypi_build = False  # Set to true if trying to minimize size for pypi package upload. Note this disables some architectures
+pypi_build = False # Set to true if trying to minimize size for pypi package upload. Note this disables some architectures
 
 lib_dir = os.path.join("pandare", "data")
-
-
 def copy_objs():
     '''
     Run to copy objects into a (local and temporary) python module before installing to the system.
@@ -68,28 +65,18 @@ def copy_objs():
             shutil.copy(    llvm2,      os.path.join(lib_dir, softmmu))
     '''
 
-
-def parse_requirements(filename):
-    with open(filename, 'r') as f:
-        return [line.strip() for line in f if line.strip() and not line.strip().startswith('#')]
-
-
 from setuptools.command.install import install as install_orig
 from setuptools.command.develop import develop as develop_orig
-
-
 class custom_develop(develop_orig):
     '''
     Install as a local module (not to system) by
         1) Creating datatype files for local-use
         2) Running regular setup tools logic
     '''
-
     def run(self):
         from create_panda_datatypes import main as create_datatypes
         create_datatypes(install=False)
         super().run()
-
 
 class custom_install(install_orig):
     '''
@@ -97,22 +84,20 @@ class custom_install(install_orig):
     1) Running from within the panda repo with panda built - need to create_datatypes
     2) Running from a python sdist where all the files are already prepared
     '''
-
     def run(self):
         try:
             from create_panda_datatypes import main as create_datatypes
             # If we can do the import, we're in the panda repo
             create_datatypes(install=True)
             copy_objs()
-
+            
         except ImportError:
             # Import failed, we're either in a python sdist or something has gone very wrong
-            assert (os.path.isfile("pandare/include/panda_datatypes.h")), \
-                "panda_datatypes.h missing and can't be generated"
-            assert (os.path.isfile("pandare/autogen/panda_datatypes.py")), \
-                "panda_datatypes.py missing and can't be generated"
+            assert(os.path.isfile("pandare/include/panda_datatypes.h")), \
+                            "panda_datatypes.h missing and can't be generated"
+            assert(os.path.isfile("pandare/autogen/panda_datatypes.py")), \
+                            "panda_datatypes.py missing and can't be generated"
         super().run()
-
 
 # To build a package for pip:
 # python setup.py install
@@ -130,13 +115,13 @@ setup(name='pandare',
       author_email='fasano@mit.edu',
       url='https://github.com/panda-re/panda/',
       packages=find_packages(),
-      package_data={'pandare': [
-          'data/*-softmmu/llvm-helpers*.bc*',  # LLVM Helpers
-          'data/pypanda/include/*.h',  # Includes files
-          'data/pypanda/include/*.h',  # Includes files
-          'qcows.json'  # Generic Images
-      ]},
-      install_requires=parse_requirements("requirements.txt"),
+      package_data = { 'pandare': [ \
+          'data/*-softmmu/llvm-helpers*.bc*', # LLVM Helpers
+          'data/pypanda/include/*.h',         # Includes files
+          'data/pypanda/include/*.h',         # Includes files
+          'qcows.json'                        # Generic Images
+          ]},
+      install_requires=[ 'cffi>=1.14.3', 'colorama', 'protobuf>=4.25.1'],
       python_requires='>=3.6',
       cmdclass={'install': custom_install, 'develop': custom_develop},
-      )
+     )
