8de9a08cce4b15ac1cacc3f438d54098433c95e1
marco.cendejas <marco.cendejas-cardenas@ngc.com>
2024-10-30 11:27:01 -0400

    Added new api call to callstack_instr plugin

--- panda/plugins/callstack_instr/callstack_instr_int_fns.h
+++ panda/plugins/callstack_instr/callstack_instr_int_fns.h
@@ -18,6 +18,10 @@ uint32_t get_callers(target_ulong *callers, uint32_t n, CPUState *cpu);
 // Functions are returned in functions[], most recent first
 uint32_t get_functions(target_ulong *functions, uint32_t n, CPUState *cpu);
 
+// Get up to n binaries from the given stack in use at this moment
+// Binaries are returned in libs[], most recent first
+uint32_t get_binaries(char **libs, uint32_t n, CPUState *cpu);
+
 // END_PYPANDA_NEEDS_THIS -- do not delete this comment!
 
 // NB: prog_point is c++, so beware
