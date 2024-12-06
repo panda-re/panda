9a26b7eb4925d0f0f36a5c0fddd98f0f35837a38
AndrewQuijano <andrewquijano92@gmail.com>
2024-12-06 05:06:21 -0500

    First wheel release, then create the GitHub release, and remove code duplication

--- panda/python/core/setup.py
+++ panda/python/core/setup.py
@@ -9,9 +9,12 @@ if "PRETEND_VERSION" in os.environ:
 else:
     from setuptools_scm import get_version
     version = get_version(root='../../..',
-                          fallback_version="0.0.0.1",
+                          fallback_version="0.0.1",
                           version_scheme="guess-next-dev",
                           local_scheme="no-local-version")
+    # python3 -m setuptools_scm -r .. --strip-dev is implemented here
+    # https://github.com/pypa/setuptools-scm/blob/62ae6400205533fb4b355170a11295e49b366d23/src/setuptools_scm/_cli.py#L42-L43
+    version = version.partition(".dev")[0]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
