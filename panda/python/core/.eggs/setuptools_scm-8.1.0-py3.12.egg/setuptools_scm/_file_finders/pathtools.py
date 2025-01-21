fe4c4c9f2232de94b7e940f8ed4913dfdb26083f
SneakyFoxy <d.puiko@yandex.ru>
2025-01-21 18:41:22 +0300

    Remove python .eggs folder

--- panda/python/core/.eggs/setuptools_scm-8.1.0-py3.12.egg/setuptools_scm/_file_finders/pathtools.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-import os
-
-from setuptools_scm import _types as _t
-
-
-def norm_real(path: _t.PathT) -> str:
-    return os.path.normcase(os.path.realpath(path))
